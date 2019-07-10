6346b30b9d71a22742695185df0d0d82ef941a53
Jason Cobb <jason.e.cobb@gmail.com>
2019-07-10 08:13:08 -0500

    Add some common comments

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/comments.kt
@@ -0,0 +1,7 @@
+package org.agoranomic.assessor.decisions
+
+const val NO_VETO = "Conditional: no Notice of Veto was published"
+const val YES_VETO = "Conditional: a Notice of Veto was published"
+const val INEXTRICABLE = "Inextricable conditional"
+const val PM = "PM"
+const val BLOTS = "Blots"
