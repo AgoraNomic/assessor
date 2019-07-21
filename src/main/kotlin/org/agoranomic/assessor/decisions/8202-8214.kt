0dbe8b0b56ae0360a9927927ab6592ddd6a47ba1
Jason Cobb <jason.e.cobb@gmail.com>
2019-07-21 17:59:59 -0400

    Falsifian votes FOR on 8202
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2019-July/040971.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/8202-8214.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8202-8214.kt
@@ -452,7 +452,7 @@ following:
             Telnaior matches G
 
             votes(Falsifian) {
-                /* NO VOTE on 8202 */
+                FOR on 8202
                 AGAINST on 8203
                 AGAINST on 8204
                 endorses(G) on 8205
