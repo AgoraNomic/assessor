77e91718e9128c17fdc3b791c56cbf645f6004a9
Jason Cobb <jason.e.cobb@gmail.com>
2019-09-17 20:17:08 -0400

    Proposals 8248-8249 (#8)
    * Import distribution
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2019-September/013144.html
    
    * Add voting strengths
    
    Latest ADoP report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2019-September/013142.html
    
    Latest Referee report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2019-September/013128.html
    
    * Initial votes by Jason Cobb
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2019-September/041400.html
    
    * Jason Cobb causes Bernie to endorse em on all
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2019-September/041419.html
    
    * Initial votes by Trigon
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2019-September/013151.html
    
    * Initial votes by Murphy, also causing Gaelan to vote
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2019-September/041424.html
    
    * Initial votes by twg, also causing Jacob Arduino to vote
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2019-September/041428.html
    
    * Initial votes by Falsifian, also causing ATMunn to vote
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2019-September/041431.html
    
    * Initial votes by Aris
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2019-September/041432.html
    
    * Inital votes by G., also causing Rance to vote
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2019-September/041436.html
    
    * Trigon causes o to vote
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2019-September/041439.html
    
    * Resolve conditional votes

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8248-8249.kt
@@ -0,0 +1,121 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.lib.AssessmentData
+import org.agoranomic.assessor.lib.VoteKind
+import org.agoranomic.assessor.lib.VoteKind.FOR
+import org.agoranomic.assessor.lib.VoteKind.PRESENT
+import org.agoranomic.assessor.lib.assessment
+
+@UseAssessment
+fun `assessment 8248 to 8249`(): AssessmentData {
+    return assessment {
+        name("8248-8249")
+        quorum(5)
+
+        strengths {
+            default(3)
+
+            G strength 4 comment PM
+        }
+
+        proposals {
+            proposal(8248) {
+                title("Publishing definition")
+                ai(3.0)
+                author(JasonCobb)
+                coauthors(Aris)
+
+                text(
+                    """
+Amend Rule 478 ("Fora") by replacing the text
+
+  A person "publishes" or "announces" something by sending a public
+  message.
+
+with the text
+
+  To "publish" or "announce" something is to send a public message
+  whose body contains that thing."""
+                )
+            }
+
+            proposal(8249) {
+                title("No Harm No Foul")
+                ai(1.0)
+                author(JacobArduino)
+                coauthors(twg)
+
+                text(
+                    """
+Amend rule 2143, "Official Reports and Duties", by removing
+the paragraph starting "A person SHALL NOT publish information
+that is inaccurate or misleading".
+
+[This clause is duplicated by No Faking, except that it also
+illegalises _accidentally_ publishing an inaccurate report,
+or deliberately and obviously publishing an inaccurate report
+for the public good, both of which I think ought to be
+allowed.]"""
+                )
+            }
+        }
+
+        voting {
+            votes(JasonCobb) {
+                FOR on 8248
+                FOR on 8249
+            }
+
+            votes(Bernie) {
+                endorse(JasonCobb) on all
+            }
+
+            votes(Trigon) {
+                FOR on 8248
+                FOR on 8249
+            }
+
+            votes(Murphy) {
+                FOR on 8248
+                FOR on 8249
+            }
+
+            votes(Gaelan) {
+                endorse(Murphy) on all
+            }
+
+            votes(twg) {
+                endorse(Murphy) on all
+            }
+
+            votes(JacobArduino) {
+                endorse(twg) on all
+            }
+
+            votes(Falsifian) {
+                endorse(JasonCobb) on 8248
+                FOR on 8249 comment conditional("No veto, twg not AGAINST")
+            }
+
+            votes(ATMunn) {
+                endorse(Falsifian) on all
+            }
+
+            votes(Aris) {
+                FOR on 8248
+                PRESENT on 8249 comment NO_VETO
+            }
+
+            votes(G) {
+                FOR on 8248
+                FOR on 8249
+            }
+
+            Rance matches G
+
+            votes(o) {
+                endorse(Trigon) on all
+            }
+        }
+    }
+}
