7c25a6e9af9fadecc6033bf93e1cad140d3af6f5
Jason Cobb <jason.e.cobb@gmail.com>
2019-09-26 18:31:39 -0400

    Import distribution

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8250.kt
@@ -0,0 +1,44 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.lib.assessment
+
+@UseAssessment
+fun `assessment 8250`() = assessment {
+    name("8250")
+    quorum(9)
+
+    proposals {
+        proposal(8250) {
+            title("Finger bending")
+            ai(1.7)
+            author(Murphy)
+            coauthors(twg)
+
+            text(
+                """
+Amend Rule 2478 (Vigilante Justice) by replacing this text:
+
+  The Referee is by default the investigator for all Finger
+  Pointing. When a Finger, other than the Arbitor's, is Pointed over
+  an allegation related to the official duties or powers of the
+  Referee, then the Arbitor CAN, by announcement, take over the
+  investigation and thereby become the investigator.
+
+  The Referee CANNOT Point eir Finger. The Arbitor CANNOT Point eir
+  Finger at the Referee.
+
+with this text:
+
+  The above notwithstanding, the investigator CANNOT resolve a
+  Finger Pointing for which e is the perp.
+
+  The Referee is by default the investigator for all Finger
+  Pointing. If the Referee is the perp, then the Arbitor CAN
+  become the investigator of that Finger Pointing by announcement.
+
+[Limiting resolution is more important than limiting initiation, as
+offices may change hands during the process.]"""
+            )
+        }
+    }
+}
