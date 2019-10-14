f9cbf48729e777cbd55fb6ffd79bd73a0dd90294
Jason Cobb <jason.e.cobb@gmail.com>
2019-10-13 23:14:43 -0400

    Proposals 8251-8252 (#10)
    * Import distribution
    
    * Add voting strengths
    
    Latest Referee's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2019-October/013198.html
    
    Latest ADoP's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2019-September/013142.html
    
    * Initial votes by Jason Cobb
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2019-October/041476.html
    
    * Initial votes by twg, also causing Jacob Arduino to vote
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2019-October/041477.html
    
    * Jason Cobb causes Bernie to vote
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2019-October/041478.html
    
    * Initial votes by Falsifian, also causing ATMunn to vote
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2019-October/041481.html
    
    * Initial votes by Gaelan
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2019-October/041488.html
    
    * Initial votes by Murphy
    
    There was also an INEFFECTIVE attempt to cause Gaelan to vote.
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2019-October/041503.html
    
    * Initial votes by G., also causing Rance to vote
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2019-October/041511.html
    
    * Resolve conditional votes

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8251-8252.kt
@@ -0,0 +1,100 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.lib.VoteKind
+import org.agoranomic.assessor.lib.VoteKind.FOR
+import org.agoranomic.assessor.lib.VoteKind.PRESENT
+import org.agoranomic.assessor.lib.assessment
+
+@UseAssessment
+fun `assessment 8251 to 8252`() = assessment {
+    name("8251-8252")
+    quorum(6)
+
+    strengths {
+        default(3)
+        G strength 4 comment PM
+    }
+
+    proposals {
+        proposal(8251) {
+            title("Ruleset definition")
+            ai(3.0)
+            author(G)
+
+            text(
+                """
+Amend Rule 2141 (Role and Attributes of Rules) by appending the following
+text to its first paragraph:
+  The ruleset is the set of all currently-existing rules.
+
+[and while we're at it...]
+Amend Rule 1030 (Precedence between Rules) by replacing all instances
+of "Ruleset" with "ruleset".
+
+[I mean, I think that definition matches what would be the "common"
+definition anyway, but may be useful to have it there]."""
+            )
+        }
+
+        proposal(8252) {
+            title("Blasphemy")
+            ai(1.0)
+            author(JasonCobb)
+
+            text(
+                """
+Destroy the contract that is known as the Reformed Church of the Ritual.
+
+[It's useless now.]"""
+            )
+        }
+    }
+
+    voting {
+        votes(JasonCobb) {
+            FOR on 8251
+            FOR on 8252
+        }
+
+        votes(twg) {
+            endorse(author) on all
+        }
+
+        votes(JacobArduino) {
+            endorse(twg) on all
+        }
+
+        votes(Bernie) {
+            endorse(JasonCobb) on all
+        }
+
+        votes(Falsifian) {
+            FOR on 8251
+            PRESENT on 8252
+        }
+
+        votes(ATMunn) {
+            endorse(Falsifian) on all
+        }
+
+        votes(Gaelan) {
+            endorse(G) on 8251
+            endorse(JasonCobb) on 8252
+        }
+
+        votes(Murphy) {
+            FOR on 8251
+            FOR on 8252
+        }
+
+        votes(G) {
+            FOR on 8251
+            // NO VOTE on 8252
+        }
+
+        votes(Rance) {
+            FOR on 8251
+            // NO VOTE on 8252
+        }
+    }
+}
