7338d9c71beafc9f9eba62ede80c483b21d37946
Jason Cobb <jason.e.cobb@gmail.com>
2019-11-03 21:03:43 -0500

    Move _VotesReceiver to own file

--- src/main/kotlin/org/agoranomic/assessor/lib/dsl.kt
+++ src/main/kotlin/org/agoranomic/assessor/lib/dsl.kt
@@ -1,9 +1,6 @@
 package org.agoranomic.assessor.lib
 
-import org.agoranomic.assessor.lib.dsl_detail.PendingVote
-import org.agoranomic.assessor.lib.dsl_detail._ProposalReceiver
-import org.agoranomic.assessor.lib.dsl_detail._ProposalsReceiver
-import org.agoranomic.assessor.lib.dsl_detail._VotingStrengthReceiver
+import org.agoranomic.assessor.lib.dsl_detail.*
 import java.math.BigDecimal
 
 typealias VotingStrengthValue = Int
@@ -102,46 +99,6 @@ class _AssessmentReceiver {
     class _VotingReciever(private val m_proposals: List<Proposal>) {
         private val m_totalEndorsements = mutableMapOf<Player, Player>()
         private val m_votes = mutableMapOf<Player, Map<ProposalNumber, PendingVote>>()
-
-        @AssessmentDSL
-        class _VotesReceiver(private val m_proposals: List<Proposal>, private val player: Player) {
-            private val m_votes = mutableMapOf<ProposalNumber, _MutableVote>()
-
-            public object _All
-
-            public val all = _All
-
-            data class _MutableVote(val vote: VoteFunc, var comment: String? = null) {
-                fun compile() = PendingVote(vote, comment)
-            }
-
-            private fun addVote(proposal: ProposalNumber, vote: _MutableVote): _MutableVote {
-                require(m_proposals.map { it.number }.contains(proposal)) { "No such proposal $proposal" }
-                require(!m_votes.containsKey(proposal)) { "Vote already specified for proposal $proposal" }
-
-                m_votes[proposal] = vote
-                return vote
-            }
-
-            infix fun HalfFunctionVote.on(proposal: ProposalNumber) = addVote(proposal, _MutableVote(this.func))
-
-            infix fun HalfFunctionVote.on(all: _All) {
-                m_proposals.forEach { addVote(it.number, _MutableVote(this.func)) }
-            }
-
-            private fun simpleVoteFunction(vote: VoteKind) = function { _, _ -> SimpleVote(vote, comment = null) }
-
-            infix fun VoteKind.on(proposal: ProposalNumber) = simpleVoteFunction(this) on proposal
-            infix fun VoteKind.on(all: _All) = simpleVoteFunction(this) on all
-
-            infix fun _MutableVote.comment(value: String) {
-                this.comment = value
-            }
-
-            fun compile(): Map<ProposalNumber, PendingVote> {
-                return m_votes.mapValues { (k, v) -> v.compile() }
-            }
-        }
 
         infix fun Player.matches(other: Player) {
             m_totalEndorsements[this] = other
