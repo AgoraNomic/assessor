465ba3ec592613a19a057696cbd43a23004d2a49
Jason Cobb <jason.e.cobb@gmail.com>
2019-11-07 16:09:44 -0500

    Initial votes by Falsifian, also causing ATMunn to vote
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2019-November/041656.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/8266-8274.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8266-8274.kt
@@ -328,5 +328,21 @@ with this text:
         votes(Bernie) {
             endorse(JasonCobb) on all
         }
+
+        votes(Falsifian) {
+            // TODO: resolve conditional vote on 8266: AGAINST IF VETO ELSE FOR
+            FOR on 8267
+            FOR on 8268
+            PRESENT on 8269
+            PRESENT on 8270
+            FOR on 8271
+            AGAINST on 8272
+            // TODO: resolve conditional vote on 8273: AGAINST IF VETO ELSE FOR
+            // TODO: resolve conditional vote on 8274: AGAINST IF VETO ELSE FOR
+        }
+
+        votes(ATMunn) {
+            endorse(Falsifian) on all
+        }
     }
 }
