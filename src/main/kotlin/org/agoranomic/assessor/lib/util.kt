30d4fe1692cfccfba23ee9e6c3107874680a4694
Jason Cobb <jason.e.cobb@gmail.com>
2019-12-09 09:24:12 -0500

    Add BigInteger/Int utility methods

--- src/main/kotlin/org/agoranomic/assessor/lib/util.kt
+++ src/main/kotlin/org/agoranomic/assessor/lib/util.kt
@@ -1,6 +1,7 @@
 package org.agoranomic.assessor.lib
 
 import java.math.BigDecimal
+import java.math.BigInteger
 
 fun <K, V> Map<K, V>.getOrFail(key: K): V {
     if (containsKey(key)) {
@@ -15,3 +16,21 @@ operator fun Int.times(other: BigDecimal) = other * this
 
 operator fun BigDecimal.compareTo(other: Int) = this.compareTo(other.toBigDecimal())
 operator fun Int.compareTo(other: BigDecimal) = (this.toBigDecimal()).compareTo(other)
+
+operator fun BigInteger.plus(other: Int) = this.plus(other.toBigInteger())
+operator fun Int.plus(other: BigInteger) = (this.toBigInteger()).plus(other)
+
+operator fun BigInteger.minus(other: Int) = this.minus(other.toBigInteger())
+operator fun Int.minus(other: BigInteger) = (this.toBigInteger()).minus(other)
+
+operator fun BigInteger.times(other: Int) = this.times(other.toBigInteger())
+operator fun Int.times(other: BigInteger) = (this.toBigInteger()).times(other)
+
+operator fun BigInteger.div(other: Int) = this.div(other.toBigInteger())
+operator fun Int.div(other: BigInteger) = (this.toBigInteger()).div(other)
+
+operator fun BigInteger.rem(other: Int) = this.rem(other.toBigInteger())
+operator fun Int.rem(other: BigInteger) = (this.toBigInteger()).rem(other)
+
+operator fun BigInteger.compareTo(other: Int) = this.compareTo(other.toBigInteger())
+operator fun Int.compareTo(other: BigInteger) = (this.toBigInteger()).compareTo(other)
