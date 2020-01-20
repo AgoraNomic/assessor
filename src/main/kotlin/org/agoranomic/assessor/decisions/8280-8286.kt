4eea5f685f7843102c7abc3ebaf7757bcc76f637
Jason Cobb <jason.e.cobb@gmail.com>
2020-01-19 21:49:03 -0500

    Jason Cobb changes votes on Proposals 8283, 8284, and 8286
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-January/041908.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/8280-8286.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8280-8286.kt
@@ -259,10 +259,10 @@ Repeal Rule 2597, "Line-item Veto"."""
             AGAINST on 8280
             FOR on 8281
             AGAINST on 8282
-            AGAINST on 8283
-            FOR on 8284
+            PRESENT on 8283
+            AGAINST on 8284
             AGAINST on 8285
-            PRESENT on 8286
+            AGAINST on 8286
         }
 
         votes(Falsifian) {
