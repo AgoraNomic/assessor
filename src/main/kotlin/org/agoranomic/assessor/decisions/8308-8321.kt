83f21f0cf2c026911c5b7b6dfc1b09cbaac55e0e
Jason Cobb <jason.e.cobb@gmail.com>
2020-02-09 16:27:30 -0500

    Initial votes by omd
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-February/042076.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/8308-8321.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8308-8321.kt
@@ -560,5 +560,22 @@ least one proposal).]"""
             FOR on 8320
             FOR on 8321
         }
+
+        votes(omd) {
+            AGAINST on 8308
+            AGAINST on 8309
+            PRESENT on 8310
+            FOR on 8311
+            AGAINST on 8312
+            PRESENT on 8313
+            FOR on 8314
+            AGAINST on 8315
+            PRESENT on 8316
+            FOR on 8317
+            PRESENT on 8318
+            FOR on 8319
+            FOR on 8320
+            FOR on 8321
+        }
     }
 }
