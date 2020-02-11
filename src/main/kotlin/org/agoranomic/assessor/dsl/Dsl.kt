4dd248a273305faf863e1281bef095d3a8f8ebcd
Jason Cobb <jason.e.cobb@gmail.com>
2020-02-10 21:44:26 -0500

    Add DslValue class

--- src/main/kotlin/org/agoranomic/assessor/dsl/Dsl.kt
+++ src/main/kotlin/org/agoranomic/assessor/dsl/Dsl.kt
@@ -4,6 +4,38 @@ import org.agoranomic.assessor.dsl.receivers.AssessmentReceiver
 import org.agoranomic.assessor.dsl.receivers.AssessmentReceiverImpl
 import org.agoranomic.assessor.lib.AssessmentData
 
+/**
+ * Represents a value that must be set exactly once.
+ */
+class DslValue<T> {
+    private var currentValue: T? = null
+    private var isInitialized: Boolean = false
+
+    /**
+     * Sets the value. Fails if the value has already been set.
+     */
+    fun set(value: T) {
+        check(!isInitialized)
+        currentValue = value
+        isInitialized = true
+    }
+
+    /**
+     * Gets the value. Fails if the value has not been set.
+     */
+    fun get(): T {
+        check(isInitialized)
+        return currentValue as T
+    }
+
+    /**
+     * Gets the value, or a default if the value has not been set. Does not fail.
+     */
+    fun getOrElse(default: T): T {
+        return if (isInitialized) currentValue as T else default
+    }
+}
+
 @DslMarker
 @Target(AnnotationTarget.CLASS)
 annotation class AssessmentDSL
