d47fb6ce3924e0e1cc0d876d051f4b08d70353ba
Jason Cobb <jason.e.cobb@gmail.com>
2020-02-11 17:27:16 -0500

    Add DslValueMap class

--- src/main/kotlin/org/agoranomic/assessor/dsl/Dsl.kt
+++ src/main/kotlin/org/agoranomic/assessor/dsl/Dsl.kt
@@ -3,6 +3,7 @@ package org.agoranomic.assessor.dsl
 import org.agoranomic.assessor.dsl.receivers.AssessmentReceiver
 import org.agoranomic.assessor.dsl.receivers.AssessmentReceiverImpl
 import org.agoranomic.assessor.lib.AssessmentData
+import org.agoranomic.assessor.lib.getOrFail
 
 /**
  * Represents a value that must be set exactly once.
@@ -33,6 +34,44 @@ class DslValue<T> {
      */
     fun getOrElse(default: T): T {
         return if (isInitialized) currentValue as T else default
+    }
+}
+
+/**
+ * A map that contains values that may only be set exactly once per key.
+ */
+class DslValueMap<K, V> {
+    private val map = mutableMapOf<K, DslValue<V>>()
+
+    /**
+     * Sets the value for [key]. Fails if the value has already been set for [key].
+     */
+    operator fun set(key: K, value: V) {
+        val dslValue = map.computeIfAbsent(key) { DslValue() }
+        dslValue.set(value)
+    }
+
+    /**
+     * Gets the value for [key]. Fails if the value has not already been set for [key].
+     */
+    operator fun get(key: K): V {
+        val dslValue = map.getOrFail(key)
+        return dslValue.get()
+    }
+
+    /**
+     * Gets the value for [key], or [default] if it has not been set. Does not fail.
+     */
+    fun getOrElse(key: K, default: V): V {
+        val dslValue = map[key] ?: return default
+        return dslValue.getOrElse(default)
+    }
+
+    /**
+     * Compiles all set keys and values into a [Map].]
+     */
+    fun compile(): Map<K, V> {
+        return map.mapValues { (_, v) -> v.get() }
     }
 }
 
