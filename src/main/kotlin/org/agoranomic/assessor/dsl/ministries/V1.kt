acbe9baa9d7be17a53a2d50379ad64554be6d38c
Jason Cobb <jason.e.cobb@gmail.com>
2020-02-13 12:45:09 -0500

    Rename ministries to put version numbers at end

--- src/main/kotlin/org/agoranomic/assessor/dsl/ministries/V1.kt
+++ src/main/kotlin/org/agoranomic/assessor/dsl/ministries/V1.kt
@@ -8,21 +8,21 @@ import org.agoranomic.assessor.lib.Person
 import org.agoranomic.assessor.lib.VotingStrength
 import org.agoranomic.assessor.lib.proposal_set.ProposalSet
 
-typealias V1Office = V0Office
+typealias OfficeV1 = OfficeV0
 
 // These interests come from V0 and P8318 (Notorial Economy) which set the Notary's interest to [Economy]
-val V1_INTERESTS_MAP: Map<V1Office, List<Ministry>> = V0_INTERESTS_MAP.toPersistentMap().mutate { interests ->
-    interests[V1Office.Notary] = listOf(Ministry.Economy)
+val INTERESTS_MAP_V1: Map<OfficeV1, List<Ministry>> = INTERESTS_MAP_V0.toPersistentMap().mutate { interests ->
+    interests[OfficeV1.Notary] = listOf(Ministry.Economy)
 }
 
-private val V1_MINISTRY_BONUS = VotingStrength(2)
+private val MINISTRIES_V1_BONUS = VotingStrength(2)
 
-fun VotingStrengthReceiver.v1Ministries(
-    officeMap: Map<V0Office, Person?>,
+fun VotingStrengthReceiver.ministriesV1(
+    officeMap: Map<OfficeV0, Person?>,
     proposals: ProposalSet
 ) = ministries(
     officeMap,
-    V1_INTERESTS_MAP,
-    V1_MINISTRY_BONUS,
+    INTERESTS_MAP_V1,
+    MINISTRIES_V1_BONUS,
     proposals
 )
