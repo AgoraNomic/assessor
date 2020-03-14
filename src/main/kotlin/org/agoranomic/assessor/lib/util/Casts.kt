1e8a7aa2b8ab657295420ac40f24d200d3afeb74
Jason Cobb <jason.e.cobb@gmail.com>
2020-03-14 19:07:57 -0400

    Document asNullable

--- src/main/kotlin/org/agoranomic/assessor/lib/util/Casts.kt
+++ src/main/kotlin/org/agoranomic/assessor/lib/util/Casts.kt
@@ -1,5 +1,8 @@
 package org.agoranomic.assessor.lib.util
 
+/**
+ * Returns this object, except with a nullable type (if it wasn't already).
+ */
 // Inlined because this is a small utility method and function call overhead may be significant
 @Suppress("NOTHING_TO_INLINE")
 inline fun <T> T.asNullable(): T? = this
