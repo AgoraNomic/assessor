afce777a4084c1fd31365c3e7c8c2310a290b731
Jason Cobb <jason.e.cobb@gmail.com>
2020-03-14 21:14:06 -0400

    Document functions in Enums.kt

--- src/main/kotlin/org/agoranomic/assessor/lib/util/Enums.kt
+++ src/main/kotlin/org/agoranomic/assessor/lib/util/Enums.kt
@@ -2,6 +2,12 @@ package org.agoranomic.assessor.lib.util
 
 import kotlin.reflect.KClass
 
+/**
+ * Returns `true` if this collection contains all enum constants of [E], and false otherwise.
+ *
+ * @param E the enum type
+ * @param enumClass the reified [Class] of [E]
+ */
 fun <E : Enum<E>> Collection<E>.isExhaustive(enumClass: KClass<E>): Boolean {
     val enumConstants = enumClass.java.enumConstants?.toList()?.requireNoNulls()
     check(enumConstants != null)
@@ -10,8 +16,19 @@ fun <E : Enum<E>> Collection<E>.isExhaustive(enumClass: KClass<E>): Boolean {
     return collection.containsAll(enumConstants)
 }
 
+/**
+ * Returns `true` if this collection contains all enum constants of [E], and false otherwise.
+ *
+ * @param E the enum type
+ */
 inline fun <reified E : Enum<E>> Collection<E>.isExhaustive(): Boolean = isExhaustive(E::class)
 
+/**
+ * Throws [IllegalArgumentException] if this [Collection] does not contain all enum constants of [E].
+ *
+ * @param E the enum type
+ * @param enumClass the reified [Class] of [E]
+ */
 fun <E : Enum<E>> Collection<E>.requireExhaustive(enumClass: KClass<E>) {
     val enumConstants = enumClass.java.enumConstants?.toList()?.requireNoNulls()
     check(enumConstants != null)
@@ -21,4 +38,9 @@ fun <E : Enum<E>> Collection<E>.requireExhaustive(enumClass: KClass<E>) {
     }
 }
 
+/**
+ * Throws [IllegalArgumentException] if this [Collection] does not contain all enum constants of [E].
+ *
+ * @param E the enum type
+ */
 inline fun <reified E : Enum<E>> Collection<E>.requireExhaustive() = requireExhaustive(E::class)
