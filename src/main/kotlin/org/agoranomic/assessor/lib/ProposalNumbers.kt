1f4841cc67f72dfcac45878e7632cc0a086a2c01
Jason Cobb <jason.e.cobb@gmail.com>
2020-03-15 11:23:19 -0400

    Document ProposalNumbers helper functions

--- src/main/kotlin/org/agoranomic/assessor/lib/ProposalNumbers.kt
+++ src/main/kotlin/org/agoranomic/assessor/lib/ProposalNumbers.kt
@@ -4,12 +4,22 @@ import kotlinx.collections.immutable.ImmutableSet
 import kotlinx.collections.immutable.toImmutableSet
 import org.agoranomic.assessor.lib.util.toSetCheckingDistinct
 
+/**
+ * A [Collection] that contains a set of distinct ProposalNumbers.
+ */
 data class ProposalNumbers(private val data: ImmutableSet<ProposalNumber>) : Collection<ProposalNumber> by data {
     constructor(data: Set<ProposalNumber>) : this(data.toImmutableSet())
 }
 
+/**
+ * Returns an empty [ProposalNumbers].
+ */
 fun emptyProposalNumbers() = ProposalNumbers(emptySet())
 
+/**
+ * Returns a [ProposalNumbers] that contains the [ProposalNumber]s in [numbers]. Throws [IllegalArgumentException] if
+ * [numbers] contains any [ProposalNumber] more than once.
+ */
 // Kotlin doesn't let us explicitly use a vararg of an inline class, so we use a type parameter that is
 // constrained to be derived from ProposalNumber. Since inline classes are final, it can only be deduced as
 // ProposalNumber, which is what we want.
