409e33a4798f3e5bfd764a7e808c8e89115546f7
Jason Cobb <jason.e.cobb@gmail.com>
2020-03-14 21:21:03 -0400

    Add enumValuesOf(KClass) function

--- src/main/kotlin/org/agoranomic/assessor/lib/util/Enums.kt
+++ src/main/kotlin/org/agoranomic/assessor/lib/util/Enums.kt
@@ -3,14 +3,28 @@ package org.agoranomic.assessor.lib.util
 import kotlin.reflect.KClass
 
 /**
+ * Returns an [Array] containing the enum constants of [E].
+ *
+ * @param E the enum for which to retrieve the constants
+ * @param enumClass the reified [Class] of [E]
+ */
+fun <E : Enum<E>> enumValuesOf(enumClass: KClass<E>): Array<E> {
+    val result = enumClass.java.enumConstants ?: null
+
+    check(result != null)
+    check(result.none { it == null })
+
+    return result
+}
+
+/**
  * Returns `true` if this collection contains all enum constants of [E], and false otherwise.
  *
  * @param E the enum type
  * @param enumClass the reified [Class] of [E]
  */
 fun <E : Enum<E>> Collection<E>.isExhaustive(enumClass: KClass<E>): Boolean {
-    val enumConstants = enumClass.java.enumConstants?.toList()?.requireNoNulls()
-    check(enumConstants != null)
+    val enumConstants = enumValuesOf(enumClass).toList()
 
     val collection = this
     return collection.containsAll(enumConstants)
@@ -30,8 +44,7 @@ inline fun <reified E : Enum<E>> Collection<E>.isExhaustive(): Boolean = isExhau
  * @param enumClass the reified [Class] of [E]
  */
 fun <E : Enum<E>> Collection<E>.requireExhaustive(enumClass: KClass<E>) {
-    val enumConstants = enumClass.java.enumConstants?.toList()?.requireNoNulls()
-    check(enumConstants != null)
+    val enumConstants = enumValuesOf(enumClass)
 
     for (value in enumConstants) {
         require(this.contains(value)) { "Collection was required to be exhaustive, but did not contain $value" }
