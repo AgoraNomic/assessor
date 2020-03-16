c85f8d9b6d0de9998d6c2b0091d511beeaa1b3f2
Jason Cobb <jason.e.cobb@gmail.com>
2020-03-16 11:17:59 -0400

    Document HalfFunctionVote and functionVote

--- src/main/kotlin/org/agoranomic/assessor/lib/FunctionVote.kt
+++ src/main/kotlin/org/agoranomic/assessor/lib/FunctionVote.kt
@@ -1,5 +1,12 @@
 package org.agoranomic.assessor.lib
 
+/**
+ * A function vote without a comment. This is "half" of a vote because the other half is the comment of the vote.
+ * @param func the [VoteFunc] for the function vote
+ */
 data class HalfFunctionVote(val func: VoteFunc)
 
+/**
+ * Returns a [HalfFunctionVote] containing [vote].
+ */
 fun functionVote(vote: VoteFunc) = HalfFunctionVote(vote)
