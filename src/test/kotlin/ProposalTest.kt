8637cf02822a8008479f00a4de56287324ce5e73
Jason Cobb <jason.e.cobb@gmail.com>
2020-03-16 12:42:16 -0400

    Add tests for checkMismatch

---
+++ src/test/kotlin/ProposalTest.kt
@@ -0,0 +1,52 @@
+import org.agoranomic.assessor.lib.ProposalDataMismatchException
+import org.agoranomic.assessor.lib.checkMismatch
+import test_objects.firstTestProposal
+import test_objects.secondTestProposal
+import test_util.assertSucceeds
+import kotlin.test.Test
+import kotlin.test.assertFailsWith
+import kotlin.test.assertSame
+
+class `checkMismatch tests` {
+    @Test
+    fun `checkMismatch does not throw for equivalent proposals`() {
+        val firstProposal = firstTestProposal()
+        val firstProposalCopy = firstProposal.copy()
+
+        assertSucceeds {
+            checkMismatch(original = firstProposal, next = firstProposalCopy)
+        }
+    }
+
+    @Test
+    fun `checkMismatch throws IllegalArgumentException if numbers are different`() {
+        val firstProposal = firstTestProposal()
+        val secondProposal = secondTestProposal()
+
+        check(firstProposal.number != secondProposal.number)
+
+        assertFailsWith<IllegalArgumentException> {
+            checkMismatch(original = firstProposal, next = secondProposal)
+        }
+
+        assertFailsWith<IllegalArgumentException> {
+            checkMismatch(original = secondProposal, next = firstProposal)
+        }
+    }
+
+    @Test
+    fun `checkMismatch throws IllegalArgumentException if numbers are same and data is different`() {
+        val originalProposal = firstTestProposal()
+        val nextProposal = secondTestProposal().copy(number = originalProposal.number)
+
+        check(originalProposal.number == nextProposal.number)
+        check(originalProposal != nextProposal)
+
+        val exception = assertFailsWith<ProposalDataMismatchException> {
+            checkMismatch(original = originalProposal, next = nextProposal)
+        }
+
+        assertSame(originalProposal, exception.original)
+        assertSame(nextProposal, exception.next)
+    }
+}
