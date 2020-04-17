67e8f50c0beb81df343cf05c1ac3d44aa24b8f62
Jason Cobb <jason.e.cobb@gmail.com>
2020-04-17 14:20:20 -0400

    Proposals 8366-8367 (#25)
    * Fix implementation of rewards report
    
    * Import distribution
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-April/013578.html
    
    * Add offices
    
    Latest ADoP's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-March/013556.html
    
    Changes since then: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-April/042584.html
    
    * Add voting strengths
    
    * Add voting block
    
    * Initial votes by R. Lee
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-April/042538.html
    
    * Initial votes by Alexis
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-April/042539.html
    
    * Initial votes by PSS
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-April/042540.html
    
    * Initial votes by Warrigal
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-April/042541.html
    
    * Initial votes by Trigon
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-April/042542.html
    
    * Initial votes by Jason, also causing Rance to vote
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-April/042543.html
    
    * Initial votes by Falsifian
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-April/042565.html
    
    * Resolve conditional votes

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8366-8367.kt
@@ -0,0 +1,122 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.ministries.OfficeV2
+import org.agoranomic.assessor.dsl.ministries.OfficeV2.*
+import org.agoranomic.assessor.dsl.ministries.ministriesV2
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.coauthors
+import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.votes.endorse
+import org.agoranomic.assessor.lib.VoteKind
+import org.agoranomic.assessor.lib.VoteKind.FOR
+
+@UseAssessment
+fun `assesssment 8366 to 8367`() = assessment {
+    name("8366-8367")
+    quorum(7)
+
+    val offices = mapOf(
+        ADoP to Murphy,
+        Arbitor to G,
+        Assessor to Jason,
+        Comptrollor to null,
+        Distributor to omd,
+        Herald to Alexis,
+        Notary to Gaelan,
+        PrimeMinister to Aris,
+        Promotor to Aris,
+        Referee to PSS,
+        Registrar to Falsifian,
+        Rulekeepor to Jason,
+        Speaker to twg,
+        Tailor to twg,
+        Treasuror to twg
+    )
+
+    strengths {
+        default(3)
+
+        offices[Speaker]!! add 1
+        Murphy subtract 1 // 4 Blots
+        twg subtract 2 // 6 Blots
+
+        ministriesV2(offices, allProposals)
+    }
+
+    proposals(v1) {
+        proposal(8366) {
+            title("Asset Determinacy")
+            ai("3.0")
+            author(Aris)
+            coauthors(twg)
+            democratic()
+
+            text(
+                """
+Amend Rule 2576, "Ownership", by changing the text
+  "If an asset would otherwise lack an owner, it is owned by the Lost
+   and Found Department"
+to read
+   "If an asset would otherwise lack an owner, or if its ownership
+    would otherwise be indeterminate, it is owned by the Lost and Found
+    Department.
+
+If there is a rule titled "A Coin Award", repeal it."""
+            )
+        }
+
+        proposal(8367) {
+            title("Slow burn")
+            ai("3.0")
+            author(Murphy)
+            coauthors(Falsifian)
+            democratic()
+
+            text(
+                """
+Amend Rule 1789 (Cantus Cygneus) by replacing "Registrar's Report" with
+"Registrar's monthly report"."""
+            )
+        }
+    }
+
+    voting {
+        votes(RLee) {
+            FOR on all
+        }
+
+        votes(Alexis) {
+            endorse(RLee) on all
+        }
+
+        votes(PSS) {
+            FOR on 8367
+            FOR on 8366
+        }
+
+        votes(Warrigal) {
+            FOR on 8366
+            FOR on 8367
+        }
+
+        votes(Trigon) {
+            FOR on 8366
+            FOR on 8367
+        }
+
+        votes(Jason) {
+            FOR on 8366
+            FOR on 8367
+        }
+
+        votes(Rance) {
+            endorse(Jason) on all
+        }
+
+        votes(Falsifian) {
+            endorse(Aris) on 8366
+            FOR on 8367
+        }
+    }
+}
