50916ce3de14492d4086e16e8835f2641e4888cb
Jason Cobb <jason.e.cobb@gmail.com>
2020-05-06 10:59:07 -0400

    Add tests for OfficeMap

--- src/test/kotlin/OfficeMapTest.kt
+++ src/test/kotlin/OfficeMapTest.kt
@@ -1,11 +1,10 @@
 import org.agoranomic.assessor.dsl.ministries.OfficeState
 import org.agoranomic.assessor.dsl.ministries.isHeld
 import org.agoranomic.assessor.dsl.ministries.isVacant
+import org.agoranomic.assessor.dsl.ministries.officeMapOf
 import test_objects.firstTestPlayer
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertFalse
-import kotlin.test.assertTrue
+import test_util.assertEqualsAndHashCode
+import kotlin.test.*
 
 class `OfficeState tests` {
     @Test
@@ -37,5 +36,53 @@ class `OfficeState tests` {
 }
 
 class `OfficeMap tests` {
+    private enum class UnaryOffice { Value }
 
+    @Test
+    fun `officeMapOf returns held for non-null holder`() {
+        val testPerson = firstTestPlayer()
+
+        val map = officeMapOf(UnaryOffice.Value to testPerson)
+        assertEquals(OfficeState.heldBy(testPerson), map[UnaryOffice.Value])
+    }
+
+    @Test
+    fun `officeMapOf returns vacant for null holder`() {
+        val map = officeMapOf(UnaryOffice.Value to null)
+        assertEquals(OfficeState.vacant(), map[UnaryOffice.Value])
+    }
+
+    @Test
+    fun `officeMapOf throws when non-exhaustive`() {
+        assertFailsWith<IllegalArgumentException> {
+            officeMapOf<UnaryOffice>()
+        }
+    }
+
+    @Test
+    fun `officeMapOf throws when keys repeated`() {
+        assertFailsWith<IllegalArgumentException> {
+            officeMapOf(UnaryOffice.Value to null, UnaryOffice.Value to null)
+        }
+    }
+
+    @Test
+    fun `OfficeMap compares equal to self`() {
+        val firstMap = officeMapOf(UnaryOffice.Value to null)
+        assertEqualsAndHashCode(firstMap, firstMap)
+
+        val testPerson = firstTestPlayer()
+        val secondMap = officeMapOf(UnaryOffice.Value to testPerson)
+        assertEqualsAndHashCode(secondMap, secondMap)
+    }
+
+    @Test
+    fun `OfficeMap compares unequal to different map`() {
+        val firstMap = officeMapOf(UnaryOffice.Value to null)
+
+        val testPerson = firstTestPlayer()
+        val secondMap = officeMapOf(UnaryOffice.Value to testPerson)
+
+        assertNotEquals(firstMap, secondMap)
+    }
 }
