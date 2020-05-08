ec587e769277ffa34a39d9867605ca8d43eed624
Jason Cobb <jason.e.cobb@gmail.com>
2020-05-08 12:54:36 -0400

    Proposals 8373-8376 (#30)
    * Import distribution
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-April/013632.html
    
    * Add officeholders
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-April/013638.html
    
    Alexis deregisters, forfeiting office: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-May/042693.html
    
    * Add voting strengths
    
    Latest Referee's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-May/013642.html
    
    * Add voting block
    
    * Initial votes by PSS
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-April/042648.html
    
    * Initial votes by R. Lee
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-April/042649.html
    
    * Initial votes by Alexis
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-April/042651.html
    
    * Initial votes by Trigon
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-April/042650.html
    
    * Initial votes by Aris
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-April/042656.html
    
    * Initial votes by Jason
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-April/042657.html
    
    * R. Lee changes votes
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-April/042658.html
    
    * Initial votes by Murphy
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-April/042667.html
    
    * Initial votes by Falsifian
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-April/042673.html
    
    * Initial votes by G.
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-May/042687.html
    
    * Aris changes votes on 8374 and 8375
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-May/042688.html
    
    * Jason changes votes on 8374 and 8375
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-May/042690.html
    
    * PSS changes vote on 8375
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-May/042691.html
    
    * Alexis changes votes on all but 8375
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-May/042693.html
    
    * G. changes vote on 8375
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-May/042694.html
    
    * Trigon changes vote on 8375
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-May/042695.html
    
    * Falsifian changes vote on 8374
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-May/042696.html
    
    * Resolve conditional votes

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8373-8376.kt
@@ -0,0 +1,203 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.ministries.OfficeV2
+import org.agoranomic.assessor.dsl.ministries.OfficeV2.*
+import org.agoranomic.assessor.dsl.ministries.endorseOfficer
+import org.agoranomic.assessor.dsl.ministries.ministriesV2
+import org.agoranomic.assessor.dsl.ministries.officeMapOf
+import org.agoranomic.assessor.dsl.receivers.addToHolder
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.coauthors
+import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.votes.endorse
+import org.agoranomic.assessor.lib.Ministry
+import org.agoranomic.assessor.lib.Ministry.Economy
+import org.agoranomic.assessor.lib.Ministry.Participation
+import org.agoranomic.assessor.lib.VoteKind
+import org.agoranomic.assessor.lib.VoteKind.*
+
+@UseAssessment
+fun `assessment 8373 to 8376`() = assessment {
+    name("8373-8376")
+    quorum(7)
+
+    val offices = officeMapOf(
+        ADoP to Murphy,
+        Arbitor to G,
+        Assessor to Jason,
+        Comptrollor to omd,
+        Distributor to omd,
+        Herald to null,
+        Notary to RLee,
+        PrimeMinister to Aris,
+        Promotor to Aris,
+        Referee to PSS,
+        Registrar to Falsifian,
+        Rulekeepor to Jason,
+        Speaker to Trigon,
+        Tailor to PSS,
+        Treasuror to PSS
+    )
+
+    strengths {
+        default(3)
+
+        addToHolder(offices, Speaker, 1)
+        twg subtract 1 // BLOTS
+
+        ministriesV2(offices, allProposals)
+    }
+
+    proposals(v1) {
+        proposal(8373) {
+            title("Auction resolution fixes")
+            ai("1.0")
+            author(Jason)
+            coauthors(Aris)
+            chamber(Economy)
+
+            text(
+                """
+Amend Rule 2551 by replacing the text from "A person's priority on an
+Auction is their" to "the excess lots are not won by any person."
+(including both ends) with the following paragraphs:
+
+  The bidders in an Auction are the players who have a non-withdrawn
+  bid in that Auction. Priority is a strict total ordering over the
+  bidders in an Auction. For two bidders with different bid amounts on
+  their non-withdrawn bids, the bidder with the higher bid has higher
+  priority than the other bidder; otherwise, the bidder whose
+  non-withdrawn bid was placed earlier has higher priority than the
+  other bidder.
+
+  Upon the end of an Auction, for each lot, in the order they are to
+  be awarded, the bidder with the highest priority in the Auction who
+  is not yet the winner of a lot becomes the winner of the lot. If
+  there are more lots than there are persons with non-withdrawn bids,
+  the excess lots are not won by any person.
+
+
+See [0] for an explanation on what this does.
+
+[0]:
+https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-discussion/2020-April/057218.html"""
+            )
+        }
+
+        proposal(8374) {
+            title("Re-Officialization")
+            ai("1.0")
+            author(Alexis)
+            chamber(Participation)
+
+            text(
+                """
+Append the following to the list of official Patent Titles in Rule 2581
+(Official Patent Titles):
+{
+  - Left in a Huff, awardable by the Registrar to any player deregistered by
+  a Writ of FAGE, where the Cantus Cygneus that gave rise to the Writ was due
+  to a genuine, serious grievance.
+
+  - {Three, Six, Nine, Twelve} Months Long Service, each awardable by that
+  ADoP to a player who has served the appropriate number of months in a
+  single office without significant dereliction of duty.
+}"""
+            )
+        }
+
+        proposal(8375) {
+            title("Patent Title Unswap")
+            ai("2.0")
+            author(Alexis)
+            chamber(Participation)
+
+            text(
+                """
+Revoke the Patent titles "Prince of Agora" and "Princess of Andorra" from
+Alexis, and award em the Patent titles "Prince of Andorra" and "Princess of
+Agora".
+
+[At some point, these appear to have been swapped in Herald's reports.
+Since the report was ratified since the mistake was made, a proposal is
+necessary to fix them.]"""
+            )
+        }
+
+        proposal(8376) {
+            title("Additional fire retardant")
+            ai("3.0")
+            author(Falsifian)
+            democratic()
+
+            text(
+                """
+Amend Rule 1789 by replacing "subsequent Registrar Reports" with
+"subsequent Registrar monthly Reports"."""
+            )
+        }
+    }
+
+    voting {
+        votes(PSS) {
+            FOR on 8373
+            FOR on 8374
+            endorse(G) on 8375
+            FOR on 8376
+        }
+
+        votes(RLee) {
+            PRESENT on 8373
+            FOR on 8375
+            AGAINST on others
+        }
+
+        votes(Alexis) {
+            AGAINST on 8373
+            AGAINST on 8374
+            FOR on 8375
+            AGAINST on 8376
+        }
+
+        votes(Trigon) {
+            PRESENT on 8373
+            FOR on 8374
+            FOR on 8375
+            FOR on 8376
+        }
+
+        votes(Aris) {
+            FOR on 8373
+            endorse(G) on 8374
+            endorse(G) on 8375
+            FOR on 8376
+        }
+
+        votes(Jason) {
+            FOR on 8373
+            endorse(G) on 8374
+            endorse(G) on 8375
+            FOR on 8376
+        }
+
+        votes(Murphy) {
+            endorseOfficer(offices, Registrar) on 8373
+            FOR on 8374
+            endorseOfficer(offices, Herald) on 8375
+            FOR on 8376
+        }
+
+        votes(Falsifian) {
+            FOR on 8373 comment NO_VETO
+            FOR on 8374 comment NO_VETO
+            endorse(Alexis) on 8375
+            FOR on 8376
+        }
+
+        votes(G) {
+            AGAINST on 8374
+            PRESENT on 8375
+        }
+    }
+}
