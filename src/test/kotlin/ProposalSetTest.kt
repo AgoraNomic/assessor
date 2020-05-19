d58c88797fc2e273276592f4b492a87ae7a3255d
Jason Cobb <jason.e.cobb@gmail.com>
2020-05-19 19:34:14 -0400

    Add tests for MutableProposalSets

--- src/test/kotlin/ProposalSetTest.kt
+++ src/test/kotlin/ProposalSetTest.kt
@@ -6,10 +6,12 @@ import test_objects.firstTestProposal
 import test_objects.secondTestProposal
 import test_objects.thirdTestProposal
 import test_util.assertEqualsAndHashCode
+import test_util.assertSucceeds
 import test_util.copyWithNumber
 import kotlin.test.*
 
 private typealias CreateProposalSetFunc = (proposals: Array<out Proposal>) -> ProposalSet
+private typealias CreateMutableProposalSetFunc = (proposals: Array<out Proposal>) -> MutableProposalSet
 
 class `ProposalSet tests` {
     private operator fun CreateProposalSetFunc.invoke(vararg proposals: Proposal) = this(proposals)
@@ -201,5 +203,145 @@ class `ProposalSet tests` {
             proposalNumbersOf(firstProposal.number, secondProposal.number, thirdProposal.number),
             createProposalSet(firstProposal, secondProposal, thirdProposal).numbers()
         )
+    }
+}
+
+class `MutableProposalSet tests` {
+    private operator fun CreateMutableProposalSetFunc.invoke(vararg proposals: Proposal) = this(proposals)
+
+    companion object {
+        @JvmStatic
+        private fun createProposalSetFuncs(): List<CreateMutableProposalSetFunc> {
+            return listOf(::mutableProposalSetOf)
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("createProposalSetFuncs")
+    fun `add works`(createProposalSet: CreateMutableProposalSetFunc) {
+        val firstProposal = firstTestProposal()
+        val secondProposal = secondTestProposal()
+
+        val mutableProposalSet = createProposalSet()
+
+        mutableProposalSet.add(firstProposal)
+        assertEqualsAndHashCode(createProposalSet(firstProposal), mutableProposalSet)
+
+        mutableProposalSet.add(secondProposal)
+        assertEqualsAndHashCode(createProposalSet(secondProposal, firstProposal), mutableProposalSet)
+
+        assertEquals(setOf(firstProposal, secondProposal), mutableProposalSet.toSet())
+    }
+
+    @ParameterizedTest
+    @MethodSource("createProposalSetFuncs")
+    fun `add is idempotent`(createProposalSet: CreateMutableProposalSetFunc) {
+        val firstProposal = firstTestProposal()
+        val secondProposal = secondTestProposal()
+
+        val mutableProposalSet = createProposalSet(firstProposal)
+        mutableProposalSet.add(secondProposal)
+        mutableProposalSet.add(secondProposal)
+
+        assertEquals(setOf(firstProposal, secondProposal), mutableProposalSet.toSet())
+
+        // Duplicate should have no effect
+        assertEqualsAndHashCode(createProposalSet(firstProposal, secondProposal), mutableProposalSet)
+    }
+
+    @ParameterizedTest
+    @MethodSource("createProposalSetFuncs")
+    fun `add of mismatched proposal throws`(createProposalSet: CreateMutableProposalSetFunc) {
+        val commonNumber = ProposalNumber(12)
+
+        val original = firstTestProposal().copyWithNumber(commonNumber)
+        val next = secondTestProposal().copyWithNumber(commonNumber)
+
+        val mutableProposalSet = createProposalSet(original)
+
+        val thrown = assertFailsWith<ProposalDataMismatchException> {
+            mutableProposalSet.add(next)
+        }
+
+        assertEquals(original, thrown.original)
+        assertEquals(next, thrown.next)
+    }
+
+    @ParameterizedTest
+    @MethodSource("createProposalSetFuncs")
+    fun `remove(ProposalNumber) works`(createProposalSet: CreateMutableProposalSetFunc) {
+        val firstProposal = firstTestProposal()
+        val secondProposal = secondTestProposal()
+
+        val mutableProposalSet = createProposalSet(firstProposal, secondProposal)
+
+        mutableProposalSet.remove(firstProposal.number)
+        assertEqualsAndHashCode(createProposalSet(secondProposal), mutableProposalSet)
+
+        mutableProposalSet.remove(secondProposal.number)
+        assertEqualsAndHashCode(createProposalSet(), mutableProposalSet)
+
+        assertTrue(mutableProposalSet.isEmpty())
+    }
+
+    @ParameterizedTest
+    @MethodSource("createProposalSetFuncs")
+    fun `remove(Proposal) works`(createProposalSet: CreateMutableProposalSetFunc) {
+        val firstProposal = firstTestProposal()
+        val secondProposal = secondTestProposal()
+
+        val mutableProposalSet = createProposalSet(firstProposal, secondProposal)
+
+        mutableProposalSet.remove(firstProposal)
+        assertEqualsAndHashCode(createProposalSet(secondProposal), mutableProposalSet)
+
+        mutableProposalSet.remove(secondProposal)
+        assertEqualsAndHashCode(createProposalSet(), mutableProposalSet)
+
+        assertTrue(mutableProposalSet.isEmpty())
+    }
+
+    @ParameterizedTest
+    @MethodSource("createProposalSetFuncs")
+    fun `remove of un-contained ProposalNumber works`(createProposalSet: CreateMutableProposalSetFunc) {
+        val mutableProposalSet = createProposalSet()
+
+        assertSucceeds {
+            mutableProposalSet.remove(firstTestProposal().number)
+            mutableProposalSet.remove(secondTestProposal().number)
+        }
+
+        assertEquals(createProposalSet(), mutableProposalSet)
+    }
+
+    @ParameterizedTest
+    @MethodSource("createProposalSetFuncs")
+    fun `remove of un-contained Proposal works`(createProposalSet: CreateMutableProposalSetFunc) {
+        val mutableProposalSet = createProposalSet()
+
+        assertSucceeds {
+            mutableProposalSet.remove(firstTestProposal())
+            mutableProposalSet.remove(secondTestProposal())
+        }
+
+        assertEquals(createProposalSet(), mutableProposalSet)
+    }
+
+    @ParameterizedTest
+    @MethodSource("createProposalSetFuncs")
+    fun `remove of mismatched Proposal throws`(createProposalSet: CreateMutableProposalSetFunc) {
+        val commonNumber = ProposalNumber(12)
+
+        val original = firstTestProposal().copyWithNumber(commonNumber)
+        val next = secondTestProposal().copyWithNumber(commonNumber)
+
+        val mutableProposalSet = createProposalSet(original)
+
+        val thrown = assertFailsWith<ProposalDataMismatchException> {
+            mutableProposalSet.remove(next)
+        }
+
+        assertEquals(original, thrown.original)
+        assertEquals(next, thrown.next)
     }
 }
