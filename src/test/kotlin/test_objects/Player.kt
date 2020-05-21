a175e73e0173810d5e48d67c86c94e1abc9b942f
Jason Cobb <jason.e.cobb@gmail.com>
2020-05-20 22:54:32 -0400

    Add testPlayer(int) overloads

--- src/test/kotlin/test_objects/Player.kt
+++ src/test/kotlin/test_objects/Player.kt
@@ -4,5 +4,8 @@ import org.agoranomic.assessor.lib.Person
 
 fun testPlayer(num: TestNumber, type: String = "Player") = Person(testString(subReqNum(num), type))
 
+fun testPlayer(num: Int) = testPlayer(testNumber(num))
+fun testPlayer(num: Int, type: String) = testPlayer(testNumber(num), type)
+
 fun firstTestPlayer(type: String = "Player") = testFirst { testPlayer(it, type) }
 fun secondTestPlayer(type: String = "Player") = testSecond { testPlayer(it, type) }
