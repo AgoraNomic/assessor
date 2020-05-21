1f79cb140438493eb77affd1b4805a5dd780c053
Jason Cobb <jason.e.cobb@gmail.com>
2020-05-20 23:15:12 -0400

    Document CappedVotingStrengthMap

--- src/main/kotlin/org/agoranomic/assessor/lib/CappedVotingStrengthMap.kt
+++ src/main/kotlin/org/agoranomic/assessor/lib/CappedVotingStrengthMap.kt
@@ -1,5 +1,12 @@
 package org.agoranomic.assessor.lib
 
+/**
+ * A [VotingStrengthMap] that (optionally) places caps on its voting strengths. [min] and [max] may both be null, in
+ * which case on lower- or upper-bound (respectively) is placed on the voting strengths.
+ *
+ * @throws IllegalArgumentException if `min` and `max` are both non-null, and `max >= min`
+ * @throws IllegalArgumentException if `base.defaultStrength` is less than `min` or more than `max` (if either is non-null).
+ */
 data class CappedVotingStrengthMap(
     private val base: VotingStrengthMap,
     private val min: VotingStrength?,
@@ -15,12 +22,27 @@ data class CappedVotingStrengthMap(
 
     private fun VotingStrength.isValid() = !(isBelowMin() || isAboveMax())
 
+    /**
+     * Evaluates to `base.defaultStrenth`.
+     */
     override val defaultStrength
         get() = base.defaultStrength
 
+    /**
+     * Evaluates to `base.specialPeople`.
+     */
     override val specialPeople
         get() = base.specialPeople
 
+    /**
+     * Let B be the result of `base.getOrNull(person)`.
+     * If B is null, returns null.
+     * Otherwise, if min is not null, and B.value < min, returns a result with a strength of min.
+     * Otherwise, if max is not null, and B.value > max, returns a result with a strength of min.
+     * Otherwise, returns B.
+     *
+     * Note: in the cases where min/max is returned, the comment is not specified.
+     */
     override fun getOrNull(person: Person): VotingStrengthWithComment? {
         val baseValue = base.getOrNull(person) ?: return null
 
