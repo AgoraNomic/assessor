c8d5ad3d6ae86db9c3cb988bb5beb1b1018fa6a6
Jason Cobb <jason.e.cobb@gmail.com>
2020-06-02 22:46:09 -0400

    Use stdlib implementations of ProposalSet operators where possible

--- src/main/kotlin/org/agoranomic/assessor/lib/proposal_set/Operators.kt
+++ src/main/kotlin/org/agoranomic/assessor/lib/proposal_set/Operators.kt
@@ -2,40 +2,17 @@ package org.agoranomic.assessor.lib.proposal_set
 
 import org.agoranomic.assessor.lib.Proposal
 
-/**
- * Equivalent to converting the list containing the [Proposals][Proposal] in this, along with those in `other` into a
- * [ProposalSet].
- */
-operator fun ProposalSet.plus(other: ProposalSet): ProposalSet {
-    return (this.toList() + other.toList()).toProposalSet()
-}
+operator fun ProposalSet.plus(other: ProposalSet) =
+    ((this as Iterable<Proposal>) + (other as Iterable<Proposal>)).toProposalSet()
 
-/**
- * Equivalent to `this + proposalSetOf(other)`.
- * @see [ProposalSet.plus]
- */
-operator fun ProposalSet.plus(other: Proposal) = this + proposalSetOf(other)
+operator fun ProposalSet.plus(other: Proposal) = ((this as Iterable<Proposal>) + other).toProposalSet()
 
-/**
- * Equivalent to `proposalSetOf(this) + other`.
- * @see [ProposalSet.plus]
- */
-operator fun Proposal.plus(other: ProposalSet) = proposalSetOf(this) + other
+operator fun Proposal.plus(other: ProposalSet) = (listOf(this) + (other as Iterable<Proposal>)).toProposalSet()
 
-/**
- * Equivalent to creating a (mutable) copy of this [ProposalSet], then calling `removeAll(other)`, and returning that
- * copy.
- */
-operator fun ProposalSet.minus(other: ProposalSet): ProposalSet {
-    val copy = this.toMutableProposalSet()
-    copy.removeAll(other)
-    return copy
-}
+operator fun ProposalSet.minus(other: ProposalSet) =
+    ((this as Iterable<Proposal>) - (other as Iterable<Proposal>)).toProposalSet()
 
-/**
- * Equivalent to `this - proposalSetOf(other)`.
- */
-operator fun ProposalSet.minus(other: Proposal) = this - proposalSetOf(other)
+operator fun ProposalSet.minus(other: Proposal) = ((this as Iterable<Proposal>) - other).toProposalSet()
 
 /**
  * Equivalent to calling `add(other)`.
