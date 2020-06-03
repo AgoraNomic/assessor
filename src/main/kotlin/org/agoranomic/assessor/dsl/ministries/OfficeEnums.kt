c177f2efc4069c3cf770ae309a052614dbe38dcf
Jason Cobb <jason.e.cobb@gmail.com>
2020-06-03 18:03:25 -0400

    Proposals 8388-8404 (#36)
    * Import distribution
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-May/013695.html
    
    * Add offices
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-May/013692.html
    
    * Add OfficeJune3
    
    The P8388 will be adopted by unanimous vote
    
    * Add ministries V3
    
    * Add voting strengths
    
    * Add voting block
    
    * Initial votes by PSS
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-May/042917.html
    
    * Initial votes by Trigon
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-May/013696.html
    
    * Initial votes by nch
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-May/042918.html
    
    * Initial votes by R. Lee
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-May/042921.html
    
    * Initial votes by Jason
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-May/042929.html
    
    * Initial votes by G.
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-May/042938.html
    
    * Initial votes by Falsifian
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-May/042946.html
    
    * Initial votes by ATMunn
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-May/042943.html
    
    * Initial votes by Aris
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-May/042944.html
    
    * Initial votes by Murphy
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-May/042959.html
    
    * Resolve conditional votes

---
+++ src/main/kotlin/org/agoranomic/assessor/dsl/ministries/OfficeEnums.kt
@@ -0,0 +1,49 @@
+package org.agoranomic.assessor.dsl.ministries
+
+enum class OfficeInitial {
+    ADoP,
+    Arbitor,
+    Assessor,
+    Comptrollor,
+    Distributor,
+    Herald,
+    Notary,
+    PrimeMinister,
+    Promotor,
+    Referee,
+    Registrar,
+    Rulekeepor,
+    Speaker,
+    Tailor,
+    Treasuror,
+    ;
+}
+
+// Reflects addition of Webmastor created by P8388, adopted ~June 3, 2020
+// See https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-May/013695.html
+enum class OfficeJune3 {
+    ADoP,
+    Arbitor,
+    Assessor,
+    Comptrollor,
+    Distributor,
+    Herald,
+    Notary,
+    PrimeMinister,
+    Promotor,
+    Referee,
+    Registrar,
+    Rulekeepor,
+    Speaker,
+    Tailor,
+    Treasuror,
+    Webmastor,
+    ;
+
+    companion object {
+        // Relies on enumerators having the same names as OfficeInitial
+        fun fromInitial(other: OfficeInitial): OfficeJune3 {
+            return OfficeJune3.valueOf(other.name)
+        }
+    }
+}
