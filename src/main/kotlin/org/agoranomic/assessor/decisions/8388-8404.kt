4be8af4951a0fe331f94a7d9a2af52a8cb4690e5
Jason Cobb <jason.e.cobb@gmail.com>
2020-06-02 23:01:34 -0400

    Initial votes by G.
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-May/042938.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/8388-8404.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8388-8404.kt
@@ -657,5 +657,25 @@ Create a new AI-1.7 rule, "Indictment", with the following text:
             AGAINST on 8403
             FOR on 8404
         }
+
+        votes(G) {
+            FOR on 8388
+            FOR on 8389
+            AGAINST on 8390
+            AGAINST on 8391
+            FOR on 8392
+            AGAINST on 8393
+            FOR on 8394
+            FOR on 8395
+            FOR on 8396
+            FOR on 8397
+            AGAINST on 8398
+            AGAINST on 8399
+            AGAINST on 8400
+            AGAINST on 8401
+            PRESENT on 8402
+            PRESENT on 8403
+            PRESENT on 8404
+        }
     }
 }
