dc9635b1ede3dd89beced0a6c3ef8295a78ca6a0
Jason Cobb <jason.e.cobb@gmail.com>
2020-06-12 16:21:14 -0400

    Initial votes by Jason
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-June/043034.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/8405-8408.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8405-8408.kt
@@ -370,5 +370,12 @@ Flip the Pended switch of every proposal in the proposal pool to True."""
             FOR on 8407
             FOR on 8408
         }
+
+        votes(Jason) {
+            FOR on 8405
+            AGAINST on 8406
+            FOR on 8407
+            FOR on 8408
+        }
     }
 }
