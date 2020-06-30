40a29110c78e311dec33d24678dd474e1e4e0bdd
Jason Cobb <jason.e.cobb@gmail.com>
2020-06-30 17:07:18 -0400

    Initial votes by G., also causing twg to vote
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-June/043507.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/8442-8457.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8442-8457.kt
@@ -482,5 +482,28 @@ Create a power 2 rule called "Excess Proposals" with the text'
             FOR on 8456
             endorseOfficer(offices, Promotor) on 8457
         }
+
+        votes(G) {
+            FOR on 8442
+            PRESENT on 8443
+            AGAINST on 8444
+            FOR on 8445
+            FOR on 8446
+            AGAINST on 8447
+            AGAINST on 8448
+            AGAINST on 8449
+            FOR on 8450
+            PRESENT on 8451
+            FOR on 8452
+            FOR on 8453
+            FOR on 8454
+            FOR on 8455
+            PRESENT on 8456
+            AGAINST on 8457
+        }
+
+        votes(twg) {
+            endorse(G) on all
+        }
     }
 }
