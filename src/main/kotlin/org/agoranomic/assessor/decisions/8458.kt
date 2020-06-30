46ae44e57ab039cb52b1bd6008f0493e8b700bf6
Jason Cobb <jason.e.cobb@gmail.com>
2020-06-29 22:18:33 -0400

    Proposal 8458 (#40)
    * Import distribution
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-June/013848.html
    
    * Add voting strengths
    
    Latest ADoP's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-June/013830.html
    
    Latest Referee's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-June/013834.html
    
    * Add voting block
    
    * Initial votes by Falsifian
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-June/043593.html
    
    * Initial votes by Jason
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-June/043594.html
    
    * Initial votes by R. Lee, also causing Tcbapo to vote
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-June/043588.html
    
    * Initial votes by nch, also purchasing strength
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-June/043600.html
    
    * R. Lee purchases strength
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-June/043601.html
    
    * Initial votes by PSS
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-June/043615.html
    
    * Initial votes by ATMunn
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-June/013850.html
    
    * Initial votes by Aris
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-June/043684.html
    
    * Initial votes by Trigon
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-June/013855.html
    
    * nch changes vote on 8458 to FOR
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-June/043698.html
    
    * R. Lee changes vote on 8458 to FOR
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-June/043700.html
    
    * Initial votes by Murphy
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-June/043714.html

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8458.kt
@@ -0,0 +1,109 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.ministries.OfficeJune3.*
+import org.agoranomic.assessor.dsl.ministries.ministriesJun15
+import org.agoranomic.assessor.dsl.ministries.officeMapOf
+import org.agoranomic.assessor.dsl.receivers.addToHolder
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.lib.Ministry
+import org.agoranomic.assessor.lib.ProposalNumber
+import org.agoranomic.assessor.lib.VoteKind.*
+
+@UseAssessment
+fun `assessment 8458`() = assessment {
+    name("8458")
+    quorum(8)
+
+    val offices = officeMapOf(
+        ADoP to RLee,
+        Arbitor to G,
+        Assessor to Jason,
+        Distributor to omd,
+        Herald to PSS,
+        Notary to ATMunn,
+        PrimeMinister to nch,
+        Promotor to Aris,
+        Referee to PSS,
+        Registrar to Falsifian,
+        Rulekeepor to Jason,
+        Speaker to G,
+        Tailor to PSS,
+        Treasuror to Trigon,
+        Webmastor to nch
+    )
+
+    strengths {
+        default(3)
+        min(0)
+        max(15)
+
+        ministriesJun15(offices, allProposals)
+        addToHolder(offices, Speaker, 1)
+
+        proposal(ProposalNumber(8458)) {
+            nch add 3 // Extra votes
+            RLee add 3 // Extra votes
+        }
+    }
+
+    proposals(v1) {
+        proposal(8458) {
+            title("Welcome Package Patch")
+            ai("1.0")
+            author(Aris)
+            chamber(Ministry.Economy)
+
+            text(
+                """
+Amend Rule 2499, "Welcome Packages", by appending to the first paragraph:
+
+  A player CANNOT receive a Welcome Package via this method if e was
+  deregistered on account of having excessive blots."""
+            )
+        }
+    }
+
+    voting {
+        votes(RLee) {
+            FOR on 8458
+        }
+
+        votes(Tcbapo) {
+            AGAINST on 8458
+        }
+
+        votes(Falsifian) {
+            FOR on 8458
+        }
+
+        votes(Jason) {
+            FOR on 8458
+        }
+
+        votes(nch) {
+            FOR on 8458
+        }
+
+        votes(PSS) {
+            FOR on 8458
+        }
+
+        votes(ATMunn) {
+            FOR on 8458
+        }
+
+        votes(Aris) {
+            FOR on 8458
+        }
+
+        votes(Trigon) {
+            FOR on 8458
+        }
+
+        votes(Murphy) {
+            PRESENT on 8458
+        }
+    }
+}
