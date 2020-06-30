e821034040b79a618f1e7dc6f6d276f8d1d96f01
Jason Cobb <jason.e.cobb@gmail.com>
2020-06-30 09:37:06 -0400

    Add historical url to P8458 resolution

--- src/main/kotlin/org/agoranomic/assessor/decisions/8458.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8458.kt
@@ -7,6 +7,7 @@ import org.agoranomic.assessor.dsl.ministries.officeMapOf
 import org.agoranomic.assessor.dsl.receivers.ai
 import org.agoranomic.assessor.dsl.receivers.proposal
 import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.receivers.url
 import org.agoranomic.assessor.dsl.votes.addToHolder
 import org.agoranomic.assessor.dsl.votes.extraVotes
 import org.agoranomic.assessor.lib.Ministry
@@ -15,6 +16,7 @@ import org.agoranomic.assessor.lib.VoteKind.*
 @UseAssessment
 fun `assessment 8458`() = assessment {
     name("8458")
+    url("https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-June/013874.html")
     quorum(8)
 
     val offices = officeMapOf(
