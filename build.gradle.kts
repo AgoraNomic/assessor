acd7c6870175bd301a44513b9259f5be1edb0f04
Jason Cobb <jason.e.cobb@gmail.com>
2020-06-30 19:03:44 -0400

    Clean up versioning in build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,6 @@ plugins {
     kotlin("jvm") version "1.3.71"
 }
 
-val kotlinVersion = "1.3.71"
-
 group = "org.agoranomic"
 version = "1.0-SNAPSHOT"
 
@@ -17,16 +15,16 @@ dependencies {
     fun kotlinx(module: String, version: String? = null): Any =
         "org.jetbrains.kotlinx:kotlinx-$module${version?.let { ":$version" } ?: ""}"
 
-    implementation(kotlin("stdlib-jdk8", kotlinVersion))
-    implementation(kotlin("reflect", kotlinVersion))
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
     implementation(kotlinx("collections-immutable-jvm", "0.3"))
     implementation(kotlinx("serialization-runtime", "0.13.0"))
     implementation("commons-cli:commons-cli:1.4")
     implementation("io.github.classgraph:classgraph:4.8.46")
     implementation("io.github.random-internet-cat:kotlin-utils:1.1.0")
 
-    testImplementation(kotlin("test", kotlinVersion))
-    testImplementation(kotlin("test-junit5", kotlinVersion))
+    testImplementation(kotlin("test"))
+    testImplementation(kotlin("test-junit5"))
     testImplementation(group = "org.junit.jupiter", name = "junit-jupiter", version = "5.5.2")
 }
 
