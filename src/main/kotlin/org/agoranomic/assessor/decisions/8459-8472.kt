82f9f82320d95c85d563e068b9e8466dfee510c7
Jason Cobb <jason.e.cobb@gmail.com>
2020-07-07 21:37:33 -0400

    Add URL

--- src/main/kotlin/org/agoranomic/assessor/decisions/8459-8472.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8459-8472.kt
@@ -7,6 +7,7 @@ import org.agoranomic.assessor.dsl.ministries.officeMapOf
 import org.agoranomic.assessor.dsl.receivers.ai
 import org.agoranomic.assessor.dsl.receivers.coauthors
 import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.receivers.url
 import org.agoranomic.assessor.dsl.votes.addToHolder
 import org.agoranomic.assessor.dsl.votes.blotPenalty
 import org.agoranomic.assessor.dsl.votes.endorse
@@ -17,6 +18,7 @@ import org.agoranomic.assessor.lib.VoteKind.*
 fun `assessment 8459 to 8472`() = assessment {
     name("8459-8472")
     quorum(8)
+    url("https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-July/013921.html")
 
     val offices = officeMapOf(
         ADoP to Murphy,
