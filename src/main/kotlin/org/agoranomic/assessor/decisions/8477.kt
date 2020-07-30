7ade610b5ee65074ca591d11fb090a537db599e7
Jason Cobb <jason.e.cobb@gmail.com>
2020-07-30 15:30:52 -0400

    Proposal 8477 (#46)
    * Import distribution
    
    * Add voting strengths
    
    Latest ADoP's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-July/014022.html
    
    Latest Referee's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-July/014020.html
    
    * Add voting block
    
    * Initial votes by omd
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-July/044200.html
    
    * Initial votes by PSS
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-July/044201.html
    
    * Initial votes by nix
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-July/044210.html
    
    * Initial votes by Jason
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-July/044211.html
    
    * Initial votes by ATMunn
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-July/044212.html
    
    * Initial votes by Falsifian
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-July/044212.html
    
    * Initial votes by G., also causing twg to vote
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-July/044212.html
    
    * Initial votes by Murphy
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-July/044292.html

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8477.kt
@@ -0,0 +1,120 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.ministries.OfficeJune30.*
+import org.agoranomic.assessor.dsl.ministries.ministries_2020_07_26
+import org.agoranomic.assessor.dsl.ministries.officeMapOf
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.votes.addToHolder
+import org.agoranomic.assessor.dsl.votes.blotPenalty
+import org.agoranomic.assessor.lib.proposal.Ministry.Economy
+import org.agoranomic.assessor.lib.vote.VoteKind.*
+
+@UseAssessment
+fun `assessment 8477`() = assessment {
+    name("8477")
+    quorum(8)
+
+    val offices = officeMapOf(
+        ADoP to Murphy,
+        Arbitor to G,
+        Assessor to Jason,
+        Coopor to RLee,
+        Distributor to omd,
+        Herald to PSS,
+        Notary to ATMunn,
+        PrimeMinister to nix,
+        Promotor to Aris,
+        Referee to PSS,
+        Registrar to Falsifian,
+        Rulekeepor to Jason,
+        Speaker to Jason,
+        Tailor to PSS,
+        Treasuror to Trigon,
+        Webmastor to nix
+    )
+
+    strengths {
+        default(3)
+        min(0)
+        max(15)
+
+        blotPenalty(Gaelan, 4 / 3)
+        blotPenalty(RLee, 80 / 3)
+
+        addToHolder(offices, Speaker, 1)
+        ministries_2020_07_26(offices, allProposals)
+    }
+
+    proposals(v2) {
+        proposal(8477) {
+            title("More officer cards")
+            ai("1.0")
+            author(Jason)
+            chamber(Economy)
+            sponsored()
+
+            text(
+                """
+Amend Rule 2624 (Card Administration) by replacing the final paragraph
+(including the list) with the following:
+
+{
+
+  The officeholder of an office CAN by announcement grant another player a
+  specified type of card, specifying that office, under the following
+  conditions:
+
+  * e has not done so in reference to that office in the current Agoran month,
+
+  * that office's interests includes the card's associated ministry,
+
+  * the player receiving the card does not hold an office with the card's
+  associated ministry in its interests, and
+
+  * the player receiving the card is not a zombie.
+
+}"""
+            )
+        }
+    }
+
+    voting {
+        votes(omd) {
+            PRESENT on 8477
+        }
+
+        votes(PSS) {
+            FOR on 8477
+        }
+
+        votes(nix) {
+            FOR on 8477
+        }
+
+        votes(Jason) {
+            FOR on 8477
+        }
+
+        votes(ATMunn) {
+            FOR on 8477
+        }
+
+        votes(Falsifian) {
+            AGAINST on 8477
+        }
+
+        votes(G) {
+            FOR on 8477
+        }
+
+        votes(twg) {
+            FOR on 8477
+        }
+
+        votes(Murphy) {
+            FOR on 8477
+        }
+    }
+}
