8b9173e7a2d1fbd1c9d481e6784544a81b5d51a6
Jason Cobb <jason.e.cobb@gmail.com>
2020-08-09 19:50:38 -0400

    Aris changes vote on 8483
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-August/044460.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/8478-8487.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8478-8487.kt
@@ -380,7 +380,7 @@ Ribbon Ownership to True."""
             FOR on 8480
             AGAINST on 8481
             FOR on 8482
-            AGAINST on 8483
+            FOR on 8483
             FOR on 8484
             FOR on 8485
             FOR on 8486
