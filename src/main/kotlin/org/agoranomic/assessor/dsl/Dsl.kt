9d9d0897e67073b498e3d0730e30642f0c8b0fdd
Jason Cobb <jason.e.cobb@gmail.com>
2020-08-16 14:15:52 -0400

    Add proposal compile helpers

--- src/main/kotlin/org/agoranomic/assessor/dsl/Dsl.kt
+++ src/main/kotlin/org/agoranomic/assessor/dsl/Dsl.kt
@@ -1,7 +1,7 @@
 package org.agoranomic.assessor.dsl
 
-import org.agoranomic.assessor.dsl.receivers.AssessmentReceiverInit
-import org.agoranomic.assessor.dsl.receivers.DefaultAssessmentCompiler
+import org.agoranomic.assessor.dsl.receivers.*
+import org.agoranomic.assessor.lib.proposal.proposal_set.ImmutableProposalSet
 import org.agoranomic.assessor.lib.resolve.AssessmentData
 
 @DslMarker
@@ -11,3 +11,21 @@ annotation class AssessmentDsl
 fun assessment(block: AssessmentReceiverInit): AssessmentData {
     return DefaultAssessmentCompiler().compile(block)
 }
+
+/**
+ * Returns the proposals compiled from [init] using [DefaultProposalsCompilerV0].
+ */
+fun compileProposals(v0: Version0, init: ProposalsReceiverV0Init): ImmutableProposalSet =
+    DefaultProposalsCompilerV0().compile(init)
+
+/**
+ * Returns the proposals compiled from [init] using [DefaultProposalsCompilerV1].
+ */
+fun compileProposals(v1: Version1, init: ProposalsReceiverV1Init): ImmutableProposalSet =
+    DefaultProposalsCompilerV1().compile(init)
+
+/**
+ * Returns the proposals compiled from [init] using [DefaultProposalsCompilerV2].
+ */
+fun compileProposals(v2: Version2, init: ProposalsReceiverV2Init): ImmutableProposalSet =
+    DefaultProposalsCompilerV2().compile(init)
