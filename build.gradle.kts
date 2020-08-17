50b75b9954f43189804a1f2ba1a91958cf98ba62
Jason Cobb <jason.e.cobb@gmail.com>
2020-08-17 19:38:48 -0400

    Clean up dependencies for Kotlin 1.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,9 +15,8 @@ dependencies {
     fun kotlinx(module: String, version: String? = null): Any =
         "org.jetbrains.kotlinx:kotlinx-$module${version?.let { ":$version" } ?: ""}"
 
-    implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
-    implementation(kotlinx("collections-immutable-jvm", "0.3"))
+    implementation(kotlinx("collections-immutable", "0.3"))
     implementation(kotlinx("serialization-runtime", "0.13.0"))
     implementation("commons-cli:commons-cli:1.4")
     implementation("io.github.classgraph:classgraph:4.8.46")
