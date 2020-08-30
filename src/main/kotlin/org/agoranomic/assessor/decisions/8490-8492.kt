2cd5642453182af4c5aaf82af505395dfcfa7505
Jason Cobb <jason.e.cobb@gmail.com>
2020-08-30 15:06:05 -0400

    Import distribution
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-August/014131.html

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8490-8492.kt
@@ -0,0 +1,69 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.lib.proposal.Ministry.Efficiency
+
+@UseAssessment
+fun assessment8490to8492() = assessment {
+    name("8490-8492")
+    quorum(7)
+
+    proposals(v2) {
+        proposal(8490) {
+            title("Functional Emergency Regulations")
+            ai("3.0")
+            author(Jason)
+            democratic()
+
+            text("""
+Amend Rule 2614 by appending the following sentence to the paragraph
+beginning "The Prime Minister CAN, in an emergency message and with 3
+Agoran consent": "To the extent explicitly permitted by this rule,
+Emergency Regulations are always taking effect."
+
+[Right now, Emergency Regulations CAN do some stuff, but they never
+actually take effect to do it.]""")
+        }
+
+        proposal(8491) {
+            title("Minor Speaker fix")
+            ai("2.0")
+            author(ATMunn)
+            chamber(Efficiency)
+            sponsored()
+
+            text("""
+Amend Rule 103 "The Speaker" by appending the following sentence to
+the paragraph beginning "If the office of Speaker has been held
+continuously":
+  Rules to the contrary notwithstanding, the Speaker CANNOT object to
+  an intent by any player to do so.
+
+  [Explanation: Currently, if the Speaker has been Speaker for at least
+  90 days, e can be replaced with support. However, the Speaker has
+  the power to stop any intent, even one that is with support, by
+  objecting to it. This would mean that the Speaker cannot be removed by
+  this method. The office can still be reappointed by the Prime Minister
+  if another player wins the game, but as is, the 90+ days method is
+  relatively obsolete. The fix is to either remove the feature
+  altogether or fix it, and I think it's a nice feature, so I proposed
+  this fix. I've also just managed to make an explanation about 3 times
+  as long as the actual proposal. Oops.]""")
+        }
+
+        proposal(8492) {
+            title("Unscoped RWO")
+            ai("3.0")
+            author(Jason)
+            democratic()
+            sponsored()
+
+            text("""
+Amend Rule 2202 by replacing the paragraph beginning "A player CAN" with
+the following: "A player CAN, without objection, ratify a specified
+public document."""")
+        }
+    }
+}
