47a06b75c20c47ce082f859b92f48d92b861c3d8
Jason Cobb <jason.e.cobb@gmail.com>
2020-09-04 15:45:43 -0400

    Remove unused comments

--- src/main/kotlin/org/agoranomic/assessor/decisions/Comments.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/Comments.kt
@@ -3,8 +3,3 @@ package org.agoranomic.assessor.decisions
 fun conditional(message: String) = "Conditional resolved: $message"
 
 val NO_VETO = conditional("no Notice of Veto was published")
-val YES_VETO = conditional("a Notice of Veto was published")
-const val INEXTRICABLE = "Inextricable conditional"
-const val PM = "PM"
-const val SPEAKER = "Speaker"
-const val BLOTS = "Blots"
