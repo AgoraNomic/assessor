57cbcf3791b76f57b3fce6ee0d56a3b286d2cb31
Jason Cobb <jason.e.cobb@gmail.com>
2020-09-07 18:08:33 -0400

    Import distribution
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-August/014176.html

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8488A-8489A.kt
@@ -0,0 +1,46 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.coauthors
+import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.lib.proposal.Ministry.Economy
+
+@UseAssessment
+fun assessment8488Ato8489A() = assessment {
+    suffix("A")
+    quorum(5)
+
+    proposals(v2) {
+        proposal(8488) {
+            title("Empty the escalator v1")
+            ai("3.0")
+            author(Falsifian)
+            coauthors(Jason, omd)
+            democratic()
+            sponsored()
+
+            text("""
+Amend Rule 2577 by adding the sentence "Attempts to destroy no assets
+are successful." before the sentence that begins "An indestructible asset".
+
+Amend Rule 2577 by adding the sentence "Attempts to transfer no assets
+are successful." before the sentence that begins "A fixed asset".
+
+Amend Rule 2579 by deleting the paragraph that ends with "0 or empty fee".""")
+        }
+
+        proposal(8489) {
+            title("Interesting economics")
+            ai("1.0")
+            author(Jason)
+            chamber(Economy)
+            sponsored()
+
+            text("""
+Amend Rule 2624 by appending the following sentence to the first
+paragraph: "The type of card with the fewest existing instances is
+associated with the Ministry of Economy."""")
+        }
+    }
+}
