3835778e6d0d0dcbc4084bed2cc3b180b58fb929
Jason Cobb <jason.e.cobb@gmail.com>
2020-09-25 15:49:00 -0400

    Proposals 8495 to 8497 (#53)
    * Import distribution
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-August/014176.html
    
    * Add voting strengths
    
    Latest ADoP's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-September/014199.html
    
    Latest Referee's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-September/014186.html
    
    * Add voting block
    
    * Decision for P8496 was never initiated
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-September/044761.html
    
    * Initial votes by Aris, also causing Telnaior to vote
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-August/044751.html
    
    * Initial votes by Jason
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-September/044762.html
    
    * Jason causes Baron von Vanderham to vote
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-September/044763.html
    
    * Initial votes by Trigon, also causing Sukil to vote
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-September/044765.html
    
    * Initial votes by Falsifian
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-September/044816.html

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8495-8497.kt
@@ -0,0 +1,121 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.ministries.OfficeJune30.*
+import org.agoranomic.assessor.dsl.ministries.endorseOfficer
+import org.agoranomic.assessor.dsl.ministries.ministries_2020_08_02
+import org.agoranomic.assessor.dsl.ministries.officeMapOf
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.votes.addToHolder
+import org.agoranomic.assessor.dsl.votes.blotPenalty
+import org.agoranomic.assessor.dsl.votes.endorse
+import org.agoranomic.assessor.dsl.votes.match
+import org.agoranomic.assessor.lib.proposal.Ministry.Participation
+import org.agoranomic.assessor.lib.vote.VoteKind.*
+
+@UseAssessment
+fun assessment8495to8497() = assessment {
+    quorum(5)
+
+    val offices = officeMapOf(
+        ADoP to Murphy,
+        Arbitor to G,
+        Assessor to Jason,
+        Coopor to RLee,
+        Distributor to omd,
+        Herald to PSS,
+        Notary to ATMunn,
+        PrimeMinister to nix,
+        Promotor to Aris,
+        Referee to PSS,
+        Registrar to Falsifian,
+        Rulekeepor to Jason,
+        Speaker to grok,
+        Tailor to PSS,
+        Treasuror to Trigon,
+        Webmastor to nix,
+    )
+
+    strengths {
+        default(3)
+        min(0)
+        max(15)
+
+        blotPenalty(Gaelan, 4 / 3)
+        blotPenalty(RLee, 81 / 3)
+
+        addToHolder(offices, Speaker, 1)
+        ministries_2020_08_02(offices, allProposals)
+    }
+
+    proposals(v2) {
+        proposal(8495) {
+            title("Please stand by")
+            ai("1.0")
+            author(G)
+            chamber(Participation)
+            sponsored()
+
+            text("""
+Create a rule, "Please Stand By", with the following text:
+
+  Normal service will resume shortly.""")
+        }
+
+        // Decision on 8496 was never initiated.
+        // See https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-September/044761.html
+
+        proposal(8497) {
+            title("Withdrawal Quick-Fix")
+            ai("3.0")
+            author(nix)
+            democratic()
+            sponsored()
+
+            text("""
+Amend R2350 "Proposals" by adding, to the last paragraph:
+
+"When a player withdraws a pending proposal, e CAN, by announcement,
+grant a Pendant to the player that pended the proposal."""")
+        }
+    }
+
+    voting {
+        votes(Aris) {
+            FOR on 8495
+            // FOR on 8496
+            AGAINST on 8497
+        }
+
+        votes(Telnaior) {
+            endorse(Aris) on all
+        }
+
+        votes(Jason) {
+            PRESENT on 8495
+            // FOR on 8496
+            FOR on 8497
+        }
+
+        votes(BaronVV) {
+            endorse(Jason) on all
+        }
+
+        votes(Trigon) {
+            AGAINST on 8495
+            // FOR on 8496
+            AGAINST on 8497
+        }
+
+        votes(sukil) {
+            match(Trigon) on all
+        }
+
+        votes(Falsifian) {
+            AGAINST on 8495
+            // FOR on 8496
+            endorseOfficer(offices, Treasuror) on 8497
+        }
+    }
+}
