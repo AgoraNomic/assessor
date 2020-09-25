430dfb3d14af0822c07479536ac5fcd7330d7787
Jason Cobb <jason.e.cobb@gmail.com>
2020-09-25 15:43:51 -0400

    Proposals 8494 to 8495 (#51)
    * Import distribution
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-August/014150.html
    
    * Add voting strengths
    
    Latest ADoP's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-August/014155.html
    
    Latest Referee's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-September/014186.html
    
    * Add voting block
    
    * Initial votes by Shelvacu
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-August/044725.html
    
    * Initial votes by Murphy
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-August/044737.html
    
    * Initial votes by Jason
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-August/044740.html
    
    * Initial votes by Gaelan
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-August/044741.html
    
    * Initial votes by G., also causing D. Margaux to vote
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-August/044755.html
    
    * Initial votes by Nathan
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-August/044757.html
    
    * G. withdraws AGAINST votes on 8494 and 8495 via Rulebending Form
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-September/044802.html
    
    * Add url
    
    * Fix that Speaker is grok
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-September/014183.html

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8494-8495.kt
@@ -0,0 +1,82 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.ministries.OfficeJune30.*
+import org.agoranomic.assessor.dsl.ministries.ministries_2020_08_02
+import org.agoranomic.assessor.dsl.ministries.officeMapOf
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.receivers.url
+import org.agoranomic.assessor.dsl.votes.addToHolder
+import org.agoranomic.assessor.dsl.votes.blotPenalty
+import org.agoranomic.assessor.lib.proposal.Ministry.Legislation
+import org.agoranomic.assessor.lib.vote.VoteKind.AGAINST
+
+@UseAssessment
+fun assessment8494to8495() = assessment {
+    url("https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-September/014195.html and https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-September/014196.html")
+    quorum(3)
+
+    val offices = officeMapOf(
+        ADoP to Murphy,
+        Arbitor to G,
+        Assessor to Jason,
+        Coopor to RLee,
+        Distributor to omd,
+        Herald to PSS,
+        Notary to ATMunn,
+        PrimeMinister to nix,
+        Promotor to Aris,
+        Referee to PSS,
+        Registrar to Falsifian,
+        Rulekeepor to Jason,
+        Speaker to grok,
+        Tailor to PSS,
+        Treasuror to Trigon,
+        Webmastor to nix,
+    )
+
+    strengths {
+        default(3)
+        min(0)
+        max(15)
+
+        blotPenalty(Gaelan, 4 / 3)
+        blotPenalty(RLee, 81 / 3)
+
+        addToHolder(offices, Speaker, 1)
+        ministries_2020_08_02(offices, allProposals)
+    }
+
+    proposals(v2) {
+        proposal(8494) {
+            title("nope")
+            ai("1.0")
+            author(Gaelan)
+            chamber(Legislation)
+            sponsored()
+
+            text("Repeal rule 2633.")
+        }
+
+        proposal(8495) {
+            title("nopenopenope")
+            ai("3.0")
+            author(Gaelan)
+            democratic()
+            sponsored()
+
+            text("Repeal rule 2633.")
+        }
+    }
+
+    voting {
+        votes(G) {
+            AGAINST on all
+        }
+
+        votes(DMargaux) {
+            AGAINST on all
+        }
+    }
+}
