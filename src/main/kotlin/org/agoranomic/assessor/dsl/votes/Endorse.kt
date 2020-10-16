321ccc1c25f4a9f8472fcac87c350269a6d7b9ec
Jason Cobb <jason.e.cobb@gmail.com>
2020-10-16 16:02:01 -0400

    Add endorseOrElse

--- src/main/kotlin/org/agoranomic/assessor/dsl/votes/Endorse.kt
+++ src/main/kotlin/org/agoranomic/assessor/dsl/votes/Endorse.kt
@@ -1,10 +1,7 @@
 package org.agoranomic.assessor.dsl.votes
 
 import org.agoranomic.assessor.lib.Person
-import org.agoranomic.assessor.lib.vote.FunctionVote
-import org.agoranomic.assessor.lib.vote.InextricableVote
-import org.agoranomic.assessor.lib.vote.Vote
-import org.agoranomic.assessor.lib.vote.VoteFunc
+import org.agoranomic.assessor.lib.vote.*
 
 fun makeEndorsementFor(endorsee: Person, endorseeVote: Vote?): Vote {
     return when (endorseeVote) {
@@ -31,3 +28,12 @@ fun endorse(person: Person) = endorsementVote(person)
 // author parameter exists for overloading, so it is kept
 @Suppress("UNUSED_PARAMETER")
 fun endorse(author: AuthorMarker) = authorEndorsementVote()
+
+fun endorseOrElse(endorsee: Person, default: FunctionVote) = FunctionVote { prop, context ->
+    checkNotNull(endorsementFunc(endorsee)(prop, context)).takeUnless { it is InextricableVote }
+        ?: default.func(prop, context)
+}
+
+fun endorseOrElse(endorsee: Person, default: VoteKind): FunctionVote = endorseOrElse(endorsee, FunctionVote { _, _ ->
+    SimpleVote(default, comment = "${endorsee.name} would have been endorsed, but eir vote was inextricable")
+})
