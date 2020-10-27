3b1b4f9f24a3a173e748f8921a39c6a2594b4ac9
Jason Cobb <jason.e.cobb@gmail.com>
2020-10-27 18:05:13 -0400

    Add voting strengths
    Latest ADoP's report: https://listserver.tue.nl/pipermail/agora/2020-October/000550.html
    
    Latest Referee's report: https://listserver.tue.nl/pipermail/agora/2020-October/000532.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/8507-8515.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8507-8515.kt
@@ -1,15 +1,51 @@
 package org.agoranomic.assessor.decisions
 
 import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.ministries.OfficeJune30.*
+import org.agoranomic.assessor.dsl.ministries.ministries_2020_08_02
+import org.agoranomic.assessor.dsl.ministries.officeMapOf
 import org.agoranomic.assessor.dsl.receivers.ai
 import org.agoranomic.assessor.dsl.receivers.coauthors
 import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.votes.addToHolder
+import org.agoranomic.assessor.dsl.votes.blotPenalty
 import org.agoranomic.assessor.lib.proposal.Ministry.*
 
 @UseAssessment
 fun assessment8507to8515() = assessment {
     name("8507-8515")
     quorum(5)
+
+    val offices = officeMapOf(
+        ADoP to Murphy,
+        Arbitor to G,
+        Assessor to Jason,
+        Coopor to G,
+        Distributor to omd,
+        Herald to PSS,
+        Notary to ATMunn,
+        PrimeMinister to ATMunn,
+        Promotor to Aris,
+        Referee to PSS,
+        Registrar to Falsifian,
+        Rulekeepor to Jason,
+        Speaker to G,
+        Tailor to PSS,
+        Treasuror to Trigon,
+        Webmastor to nix,
+    )
+
+    strengths {
+        default(3)
+        min(0)
+        max(15)
+
+        blotPenalty(Gaelan, 4 / 3)
+        blotPenalty(RLee, 81 / 3)
+
+        addToHolder(offices, Speaker, 1)
+        ministries_2020_08_02(offices, allProposals)
+    }
 
     proposals(v2) {
         proposal(8507) {
