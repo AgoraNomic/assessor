d8f927289618a99a6b3c71815666a5505e9213cb
Jason Cobb <jason.e.cobb@gmail.com>
2020-11-10 22:11:45 -0500

    Add voting strengths
    Latest ADoP's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-November/014380.html
    
    Latest Referee's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-November/014377.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/8516-8521.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8516-8521.kt
@@ -4,6 +4,8 @@ import org.agoranomic.assessor.dsl.assessment
 import org.agoranomic.assessor.dsl.receivers.ai
 import org.agoranomic.assessor.dsl.receivers.coauthors
 import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.votes.blotPenalty
+import org.agoranomic.assessor.dsl.votes.speakerBonus
 import org.agoranomic.assessor.lib.proposal.MinistryV2.Compliance
 import org.agoranomic.assessor.lib.proposal.MinistryV2.Economy
 
@@ -11,6 +13,21 @@ import org.agoranomic.assessor.lib.proposal.MinistryV2.Economy
 fun assessment8516to8521() = assessment {
     name("8516-8521")
     quorum(9)
+
+    strengths {
+        default(3)
+        min(0)
+        max(15)
+
+        speakerBonus(G)
+        blotPenalty(RLee, 81 / 3)
+
+        // Ministries are broken as of this resolution due to a bug in P8515:
+        //    For each item of each office's Ministry Interest, that office's
+        //    holder's voting strength is increased by 2 on proposals whose
+        //    Ministry Impact is set to that Ministry.
+        // "Ministry Impact" is not defined.
+    }
 
     proposals(v3) {
         proposal(8516) {
