6423c09afe50967666b89631af7eb45e97168864
Jason Cobb <jason.e.cobb@gmail.com>
2020-11-30 17:35:13 -0500

    Proposal 8523 (#59)
    * Import distribution
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-November/014386.html
    
    * Add voting strengths
    
    Latest ADoP's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-November/014415.html
    
    Latest Referee's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-November/014411.html
    
    * Add voting block
    
    * Initial votes by Shy Owl
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-November/045270.html
    
    * Initial votes by ATMunn
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-November/045272.html
    
    * Initial votes by Lucidiot
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-November/045241.html
    
    * Initial votes by Jason, also causing Baron von Vanderham to vote
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-November/045275.html
    
    * Initial votes by Falsifian, also causing twg to vote
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-November/045276.html
    
    * Initial votes by Murphy
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-November/045288.html
    
    * Add url

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8523.kt
@@ -0,0 +1,111 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.ministries.OfficeOct28.*
+import org.agoranomic.assessor.dsl.ministries.ministries_2020_11_07
+import org.agoranomic.assessor.dsl.ministries.officeMapOf
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.coauthors
+import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.receivers.url
+import org.agoranomic.assessor.dsl.votes.addToHolder
+import org.agoranomic.assessor.dsl.votes.blotPenalty
+import org.agoranomic.assessor.dsl.votes.endorse
+import org.agoranomic.assessor.lib.proposal.MinistryV2.Economy
+import org.agoranomic.assessor.lib.vote.VoteKind.FOR
+
+@UseAssessment
+fun assessment8523() = assessment {
+    name("8523")
+    url("https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-November/014417.html")
+    quorum(7)
+
+    val offices = officeMapOf(
+        ADoP to Murphy,
+        Arbitor to G,
+        Assessor to Jason,
+        Coopor to G,
+        Distributor to omd,
+        Herald to PSS,
+        Ministor to nix,
+        Notary to ATMunn,
+        PrimeMinister to ATMunn,
+        Promotor to Aris,
+        Referee to PSS,
+        Registrar to Falsifian,
+        Rulekeepor to Jason,
+        Speaker to G,
+        Tailor to PSS,
+        Treasuror to Trigon,
+        Webmastor to nix,
+    )
+
+    strengths {
+        default(3)
+        min(0)
+        max(15)
+
+        blotPenalty(RLee, 81 / 3)
+
+        addToHolder(offices, Speaker, 1)
+        ministries_2020_11_07(offices, allProposals)
+    }
+
+    proposals(v3) {
+        proposal(8523) {
+            title("Emergency Economy Unbreaking")
+            ai("2.0")
+            author(Aris)
+            coauthors(nix, Jason)
+            chamber(Economy)
+            sponsored()
+
+            text("""
+[Currently, the concentration stone plus the economy focus would
+ create over 1000 coins. That represents inflation of more than 10%.
+ Obviously, that wasn't what we had in mind. The plan is to delay
+ termination of the auction until this is resolved, so people should
+ be able to adjust their bids.]
+
+Amend the rule entitled "The Stones" by replacing:
+  - Concentration Stone (weekly, 60%)
+with:
+  - Concentration Stone (monthly, 60%)
+""")
+        }
+    }
+
+    voting {
+        votes(ShyOwl) {
+            FOR on 8523
+        }
+
+        votes(ATMunn) {
+            FOR on 8523
+        }
+
+        votes(lucidiot) {
+            FOR on 8523
+        }
+
+        votes(Jason) {
+            FOR on 8523
+        }
+
+        votes(BaronVV) {
+            endorse(Jason) on 8523
+        }
+
+        votes(Falsifian) {
+            FOR on 8523
+        }
+
+        votes(twg) {
+            endorse(Falsifian) on 8523
+        }
+
+        votes(Murphy) {
+            FOR on 8523
+        }
+    }
+}
