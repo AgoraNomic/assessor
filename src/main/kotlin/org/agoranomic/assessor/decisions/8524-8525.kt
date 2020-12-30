fa556203f6d99154e11b4d1a90be0038308774d3
Jason Cobb <jason.e.cobb@gmail.com>
2020-12-29 20:29:46 -0500

    Proposals 8524-8525 (#60)
    * Import distribution
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-November/014416.html
    
    * Add voting strengths
    
    Latest ADoP's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-November/014434.html
    
    Latest Referee's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-November/014432.html
    
    * Add voting block
    
    * Initial votes for ATMunn
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-November/045324.html
    
    * Initial votes by Murphy
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-November/045325.html
    
    * Initial votes by Jason
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-November/045327.html
    
    * Initial votes by lucidot
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-November/045330.html
    
    * Initial votes by Aris
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-November/045331.html
    
    * Initial votes by nix
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-November/045333.html
    
    * Initial votes by Falsifian, also causing twg to vote
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-November/045336.html
    
    * Initial votes by Gaelan
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-November/045338.html
    
    * Resolve conditional votes
    
    * Add url

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8524-8525.kt
@@ -0,0 +1,150 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.ministries.OfficeOct28.*
+import org.agoranomic.assessor.dsl.ministries.ministries_2020_11_07
+import org.agoranomic.assessor.dsl.ministries.officeMapOf
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.receivers.url
+import org.agoranomic.assessor.dsl.votes.addToHolder
+import org.agoranomic.assessor.dsl.votes.blotPenalty
+import org.agoranomic.assessor.dsl.votes.endorse
+import org.agoranomic.assessor.lib.proposal.MinistryV2.Compliance
+import org.agoranomic.assessor.lib.proposal.MinistryV2.Economy
+import org.agoranomic.assessor.lib.vote.VoteKind.FOR
+import org.agoranomic.assessor.lib.vote.VoteKind.PRESENT
+
+@UseAssessment
+fun assessment8524to8525() = assessment {
+    name("8524-8525")
+    url("https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-December/014439.html")
+    quorum(9)
+
+    val offices = officeMapOf(
+        ADoP to Murphy,
+        Arbitor to G,
+        Assessor to Jason,
+        Coopor to G,
+        Distributor to omd,
+        Herald to PSS,
+        Ministor to nix,
+        Notary to ATMunn,
+        PrimeMinister to ATMunn,
+        Promotor to Aris,
+        Referee to PSS,
+        Registrar to Falsifian,
+        Rulekeepor to Jason,
+        Speaker to G,
+        // Stonemason to Jason, // unneeded because I'm lazy and it has no ministries
+        Tailor to PSS,
+        Treasuror to Trigon,
+        Webmastor to nix,
+    )
+
+    strengths {
+        default(3)
+        min(0)
+        max(15)
+
+        blotPenalty(RLee, 81 / 3)
+
+        addToHolder(offices, Speaker, 1)
+        ministries_2020_11_07(offices, allProposals)
+    }
+
+    proposals(v3) {
+        proposal(8524) {
+            title("Agora's Stones Are Immune")
+            ai("2.0")
+            author(nix)
+            chamber(Economy)
+            sponsored()
+
+            text("""
+[Currently, you can steal from Agora with the soul stone, which seems a
+bit goofy and also creates problems with auctions. Also, I cleaned up
+the verbiage just a bit.]
+
+Amend "Collecting Stones" by replacing
+
+  A stone is immune if there has been no collection notice after it was
+  most recently granted immunity (if ever), as defined by other rules.
+
+with:
+
+  A stone is immune if it has been granted immunity, as defined by other
+  rules, since the last collection notice or if it is currently owned by
+  Agora.
+
+and by replacing:
+
+  A collection notice includes, for every non-immune stone not belonging
+  to Agora,
+
+with:
+
+  A collection notice includes, for every non-immune stone,""")
+        }
+
+        proposal(8525) {
+            title("Expungement for R. Lee")
+            ai("1.7")
+            author(Aris)
+            chamber(Compliance)
+            sponsored()
+
+            text("""
+[As of right now, this would lower eir blots to 9, just enough to set eir
+vote to 0 in the absence of other factors. I phrased it like this
+both so e can begin paying it down and so that any blots e earns now
+still count against em.]
+
+Expunge 72 of R. Lee's blots.""")
+        }
+    }
+
+    voting {
+        votes(ATMunn) {
+            FOR on all
+        }
+
+        votes(Murphy) {
+            FOR on 8524
+            FOR on 8525
+        }
+
+        votes(Jason) {
+            FOR on 8524
+            PRESENT on 8525
+        }
+
+        votes(lucidiot) {
+            FOR on all
+        }
+
+        votes(Aris) {
+            PRESENT on 8524
+            FOR on 8525
+        }
+
+        votes(nix) {
+            FOR on 8524
+            PRESENT on 8525
+        }
+
+        votes(Falsifian) {
+            FOR on 8524
+            FOR on 8525 comment conditional("R. Lee has at least 72 blots")
+        }
+
+        votes(twg) {
+            endorse(Falsifian) on all
+        }
+
+        votes(Gaelan) {
+            PRESENT on 8524
+            FOR on 8525
+        }
+    }
+}
