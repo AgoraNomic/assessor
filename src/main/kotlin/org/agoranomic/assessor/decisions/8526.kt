a4e2129e5b27d811ef36cb4161ccdf9e2615e050
Jason Cobb <jason.e.cobb@gmail.com>
2020-12-31 17:52:30 -0500

    Add voting strengths
    Latest ADoP's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-December/014493.html
    
    Change to offices by PSS: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2020-December/045446.html
    
    Latest Referee's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2020-December/014511.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/8526.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8526.kt
@@ -1,14 +1,51 @@
 package org.agoranomic.assessor.decisions
 
 import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.ministries.OfficeOct28.*
+import org.agoranomic.assessor.dsl.ministries.ministries_2020_11_07
+import org.agoranomic.assessor.dsl.ministries.officeMapOf
 import org.agoranomic.assessor.dsl.receivers.ai
 import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.votes.addToHolder
+import org.agoranomic.assessor.dsl.votes.blotPenalty
 import org.agoranomic.assessor.lib.proposal.MinistryV2.Economy
 
 @UseAssessment
 fun assessment8526() = assessment {
     name("8526")
     quorum(6)
+
+    val offices = officeMapOf(
+        ADoP to Murphy,
+        Arbitor to G,
+        Assessor to Jason,
+        Coopor to null,
+        Distributor to omd,
+        Herald to null,
+        Ministor to nix,
+        Notary to ATMunn,
+        PrimeMinister to ATMunn,
+        Promotor to Aris,
+        Referee to null,
+        Registrar to Falsifian,
+        Rulekeepor to Jason,
+        Speaker to G,
+        // Stonemason to Jason,
+        Tailor to null,
+        Treasuror to Trigon,
+        Webmastor to nix,
+    )
+
+    strengths {
+        default(3)
+        min(0)
+        max(15)
+
+        blotPenalty(RLee, 9 / 3)
+
+        addToHolder(offices, Speaker, 1)
+        ministries_2020_11_07(offices, allProposals)
+    }
 
     proposals(v3) {
         proposal(8526) {
