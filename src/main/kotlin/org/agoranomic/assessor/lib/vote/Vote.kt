30a23ed7d7e563682ede92086d39fb2e700b519e
Jason Cobb <jason.e.cobb@gmail.com>
2021-01-07 21:14:05 -0500

    Add new vote interface

--- src/main/kotlin/org/agoranomic/assessor/lib/vote/Vote.kt
+++ src/main/kotlin/org/agoranomic/assessor/lib/vote/Vote.kt
@@ -7,6 +7,54 @@ import org.agoranomic.assessor.lib.proposal.ProposalNumber
 
 enum class VoteKind { PRESENT, AGAINST, FOR }
 
+data class VoteStepDescription(
+    val readable: String,
+    val kind: String,
+    val parameters: ImmutableMap<String, String>,
+) {
+    constructor(
+        readable: String,
+        kind: String,
+        parameters: Map<String, String>,
+    ) : this(
+        readable = readable,
+        kind = kind,
+        parameters = parameters.toImmutableMap(),
+    )
+}
+
+sealed class VoteStepResolution {
+    data class Continue(val nextVote: ResolvingVote) : VoteStepResolution()
+    data class Resolved(val resolution: VoteKind) : VoteStepResolution()
+}
+
+interface ResolvingVote {
+    fun resolveStep(context: VoteContext): VoteStepResolution
+    val currentStepDescription: VoteStepDescription?
+}
+
+data class ResolvedVote(val value: VoteKind) : ResolvingVote {
+    override fun resolveStep(context: VoteContext): VoteStepResolution {
+        return VoteStepResolution.Resolved(value)
+    }
+
+    override val currentStepDescription: VoteStepDescription?
+        get() = null
+}
+
+data class CommentedResolvingVote(val comment: String, val nextVote: ResolvingVote) : ResolvingVote {
+    override fun resolveStep(context: VoteContext): VoteStepResolution {
+        return VoteStepResolution.Continue(nextVote)
+    }
+
+    override val currentStepDescription: VoteStepDescription
+        get() = VoteStepDescription(
+            readable = comment,
+            kind = "commented",
+            parameters = mapOf("comment" to comment),
+        )
+}
+
 sealed class Vote {
     abstract val comment: String?
     abstract fun copyWithComment(newComment: String?): Vote
