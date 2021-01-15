304c21c35c2a78c006ba5eed3ce73040f6854506
Jason Cobb <jason.e.cobb@gmail.com>
2021-01-14 21:35:06 -0500

    Various cleanups

--- src/main/kotlin/org/agoranomic/assessor/cli/Statistics.kt
+++ src/main/kotlin/org/agoranomic/assessor/cli/Statistics.kt
@@ -31,10 +31,10 @@ private fun ProposalSet.coauthorCountMap(): Map<Person, Int> {
 }
 
 @JvmName("writeStatisticBigDecimal")
-private fun writeStatistic(name: String, statistic: Map<Person, BigDecimal>) {
+private fun writeStatistic(name: String, statistic: Map<String, BigDecimal>) {
     Files.writeString(
         Path.of("$name.txt"),
-        statistic.entries.sortedBy { it.key.name }.joinToString("\n") { "${it.key.name}: ${it.value}" },
+        statistic.entries.sortedBy { it.key }.joinToString("\n") { "${it.key}: ${it.value}" },
         FILE_CHARSET,
         StandardOpenOption.CREATE,
         StandardOpenOption.TRUNCATE_EXISTING,
@@ -42,14 +42,14 @@ private fun writeStatistic(name: String, statistic: Map<Person, BigDecimal>) {
 }
 
 @JvmName("writeStatisticBigInteger")
-private fun writeStatistic(name: String, statistic: Map<Person, BigInteger>) {
+private fun writeStatistic(name: String, statistic: Map<String, BigInteger>) {
     // For some reason overload resolution fails if this variable doesn't exist /shrug
     val transformed = statistic.mapValues { (_, v) -> v.toBigDecimal() }
     writeStatistic(name, transformed)
 }
 
 @JvmName("writeStatisticDouble")
-private fun writeStatistic(name: String, statistic: Map<Person, Double>, scale: Int = 2) {
+private fun writeStatistic(name: String, statistic: Map<String, Double>, scale: Int = 2) {
     // For some reason overload resolution fails if this variable doesn't exist /shrug
     val transformed = statistic.mapValues { (_, v) -> v.toBigDecimal().setScale(scale, RoundingMode.HALF_UP) }
     writeStatistic(name, transformed)
@@ -57,7 +57,7 @@ private fun writeStatistic(name: String, statistic: Map<Person, Double>, scale: 
 
 
 @JvmName("writeStatisticInt")
-private fun writeStatistic(name: String, statistic: Map<Person, Int>) {
+private fun writeStatistic(name: String, statistic: Map<String, Int>) {
     // For some reason overload resolution fails if this variable doesn't exist /shrug
     val transformed = statistic.mapValues { (_, v) -> v.toBigInteger() }
     writeStatistic(name, transformed)
@@ -86,17 +86,27 @@ private fun <K, VE> Map<K, Iterable<VE>>.mapValuesToCounts(): Map<K, Int> {
     return mapValues { (_, v) -> v.count() }
 }
 
+private fun <V> Map<Person, V>.mapKeysToNames(): Map<String, V> {
+    return mapKeys { (k, _) -> k.name }
+}
+
 fun main() {
     val resolutionsList = findAssessments().map { resolve(it) }
+
     val proposals = resolutionsList.flatMap { it.proposals }.toProposalSet()
-    val proposalsByAuthor = proposals.groupByAuthor()
-    val proposalsByCoauthor = proposals.groupByCoauthor()
 
     val proposalResolutionsByNumber = proposals.associateWith { proposal ->
         resolutionsList.filter { it.proposals.contains(proposal.number) }.map { it.resolutionOf(proposal.number) }
     }
 
     val proposalResolutions = proposalResolutionsByNumber.values.flatten()
+
+    val proposalsByAuthor = proposals.groupByAuthor().mapKeysToNames()
+    val proposalsByCoauthor = proposals.groupByCoauthor().mapKeysToNames()
+
+    val allAuthors = proposalsByAuthor.keys
+    val allCoauthors = proposalsByCoauthor.keys
+    val allVoters = Persons(proposalResolutions.asSequence().flatMap { it.votes.voters }.toSet())
 
     val adoptedProposals =
         proposalResolutionsByNumber
@@ -105,37 +115,49 @@ fun main() {
             .map { it.key }
             .toProposalSet()
 
-    val adoptedProposalsByAuthor = adoptedProposals.groupByAuthor()
-    val adoptedProposalsByCoauthor = adoptedProposals.groupByCoauthor()
+    val adoptedProposalsByAuthor = adoptedProposals.groupByAuthor().mapKeysToNames()
+    val adoptedProposalsByCoauthor = adoptedProposals.groupByCoauthor().mapKeysToNames()
 
     val writtenCountsByAuthor =
-        proposalsByAuthor.mapValuesToCounts().also { writeStatistic("author_written", it) }
+        proposalsByAuthor
+            .mapValuesToCounts()
+            .also { writeStatistic("author_written", it) }
 
     val writtenCountsByCoauthor =
-        proposalsByCoauthor.mapValuesToCounts().also { writeStatistic("coauthor_written", it) }
-
-    val allAuthors = writtenCountsByAuthor.keys
-    val allCoauthors = writtenCountsByCoauthor.keys
-
-    val allVoters = Persons(proposalResolutions.asSequence().flatMap { it.votes.voters }.toSet())
+        proposalsByCoauthor
+            .mapValuesToCounts()
+            .also { writeStatistic("coauthor_written", it) }
 
     val adoptedCountsByAuthor =
-        adoptedProposalsByAuthor.mapValuesToCounts().also { writeStatistic("author_adopted", it) }
+        adoptedProposalsByAuthor
+            .mapValuesToCounts()
+            .also { writeStatistic("author_adopted", it) }
 
     val adoptedCountsByCoauthor =
-        adoptedProposalsByCoauthor.mapValuesToCounts().also { writeStatistic("coauthor_adopted", it) }
+        adoptedProposalsByCoauthor
+            .mapValuesToCounts()
+            .also { writeStatistic("coauthor_adopted", it) }
 
-    val adoptedRateByAuthor = allAuthors.associateWith {
-        (adoptedCountsByAuthor[it] ?: 0).toDouble() / (writtenCountsByAuthor.getValue(it)).toDouble()
-    }.also { writeStatistic("author_adopted_rate", it) }
+    val adoptedRateByAuthor =
+        allAuthors
+            .associateWith {
+                (adoptedCountsByAuthor[it] ?: 0).toDouble() / (writtenCountsByAuthor.getValue(it)).toDouble()
+            }
+            .also { writeStatistic("author_adopted_rate", it) }
 
-    val adoptedRateByCoauthor = allCoauthors.associateWith {
-        (adoptedCountsByCoauthor[it] ?: 0).toDouble() / (writtenCountsByCoauthor.getValue(it)).toDouble()
-    }.also { writeStatistic("coauthor_adopted_rate", it) }
+    val adoptedRateByCoauthor =
+        allCoauthors
+            .associateWith {
+                (adoptedCountsByCoauthor[it] ?: 0).toDouble() / (writtenCountsByCoauthor.getValue(it)).toDouble()
+            }
+            .also { writeStatistic("coauthor_adopted_rate", it) }
 
-    val adoptedWordsByAuthor = adoptedProposalsByAuthor.mapValues { (_, v) ->
-        v.sumOf { it.textWords().toBigInteger() }
-    }.also { writeStatistic("author_adopted_words", it) }
+    val adoptedWordsByAuthor =
+        adoptedProposalsByAuthor
+            .mapValues { (_, v) ->
+                v.sumOf { it.textWords().toBigInteger() }
+            }
+            .also { writeStatistic("author_adopted_words", it) }
 
     val endorsements =
         proposalResolutions
@@ -145,7 +167,6 @@ fun main() {
             .filterNotNull()
             .filter { it.kind == "endorsement" }
             .groupBy { it.parameters.getValue("endorsee") }
-            .mapKeys { (name, _) -> Person(name = name) }
             .mapValuesToCounts()
             .also { writeStatistic("endorsement_counts", it) }
 }
