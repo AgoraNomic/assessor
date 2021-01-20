bccdfa6fa8dcf7e32fad78ac0b612a64f4d071e2
Jason Cobb <jason.e.cobb@gmail.com>
2021-01-20 10:28:35 -0500

    Add min/max strength stats

--- src/main/kotlin/org/agoranomic/assessor/stats/StrengthStats.kt
+++ src/main/kotlin/org/agoranomic/assessor/stats/StrengthStats.kt
@@ -3,22 +3,52 @@ package org.agoranomic.assessor.stats
 import org.agoranomic.assessor.lib.Person
 import org.agoranomic.assessor.lib.resolve.ResolutionData
 
-fun writeVotingStrengthData(voters: List<Person>, proposalResolutionsByVoter: Map<Person, List<ResolutionData>>) {
-    writeStatistic(
-        "voter_average_strength",
-        voters.map { voter ->
-            val resolutions = proposalResolutionsByVoter.getValue(voter)
+private data class AverageMinMax(
+    val average: Double,
+    val min: Int,
+    val max: Int,
+)
 
-            voter to
-                    resolutions
-                        .sumOf {
-                            it.votingStrengths.trailForPerson(voter).final.raw
-                        }
-                        .intValueExact()
-                        .toDouble() /
-                    resolutions
-                        .count()
-                        .toDouble()
-        }
-    )
+fun writeVotingStrengthData(voters: List<Person>, proposalResolutionsByVoter: Map<Person, List<ResolutionData>>) {
+    val data = voters.associateWith { voter ->
+        val resolutions = proposalResolutionsByVoter.getValue(voter)
+
+        val strengths = resolutions.map { it.votingStrengths.trailForPerson(voter).final.raw.intValueExact() }
+
+        AverageMinMax(
+            average = strengths.average(),
+            min = strengths.minOrNull() ?: error(""),
+            max = strengths.maxOrNull() ?: error(""),
+        )
+    }
+
+
+    // The variables are needed, otherwise overload resolution fails (probably due to type inference).
+
+    run {
+        val averages = data.mapValues { (_, v) -> v.average }
+
+        writeStatistic(
+            "voter_strength_avg",
+            averages,
+        )
+    }
+
+    run {
+        val mins = data.mapValues { (_, v) -> v.min }
+
+        writeStatistic(
+            "voter_strength_min",
+            mins,
+        )
+    }
+
+    run {
+        val maxs = data.mapValues { (_, v) -> v.max }
+
+        writeStatistic(
+            "voter_strength_max",
+            maxs,
+        )
+    }
 }
