f4f274db9f8b9e0fd68578b47545788b7af892eb
Jason Cobb <jason.e.cobb@gmail.com>
2021-01-26 14:11:28 -0500

    Import distribution
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2021-January/014559.html

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8530-8531.kt
@@ -0,0 +1,63 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.coauthors
+import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.lib.proposal.MinistryV2.Legacy
+
+@UseAssessment
+fun assessment8530to8531() = assessment {
+    name("8530-8531")
+    quorum(2)
+
+    proposals(v3) {
+        proposal(8530) {
+            title("No Honour")
+            ai("1.0")
+            author(nix)
+            coauthors(Jason)
+            chamber(Legacy)
+            sponsored()
+
+            text("""
+Repeal R2510, "Such is Karma".
+
+[Karma gets rare, sporadic usage and I'd prefer not to maintain it if
+it's not going to be used. I'm also open to supporting efforts to revive
+it, but I don't have the time/interest to write such proposals myself
+right now.]""")
+        }
+
+        proposal(8531) {
+            title("Patent Title Restoration v2")
+            ai("1.5")
+            author(Jason)
+            chamber(Legacy)
+            sponsored()
+
+            text("""
+Amend Rule 649 by replacing "A Patent Title is a legal title given to a
+person in recognition of the person's distinction" with "A Patent Title
+is a legal title held by an entity in recognition of eir distinction".
+
+[The current version could plausibly be read to strip patent titles when
+an entity ceases to be a person. This makes it so that patent titles can
+be held by all entities, but can only be awarded to persons. The
+requirement on personhood/citizenship is explicit for most awarding
+methods (including the Herald's general method), while the other methods
+require 2 Agoran consent, which should be enough to prevent them going
+to non-persons if we don't want them to as a group. There's not really
+an easy place to put an explicit restriction, because some of the 2
+Agoran consent methods are at Power 3, while the patent titles rule is
+at Power 1.5.]
+
+Award all Patent Titles to eir former holders that were lost solely due
+to the entity not being a person (under any current or former definition
+of "person"), other than Hero of Agora Nomic or Grand Hero of Agora Nomic.
+
+[Don't try to award HAN/GHAN because it seems unlikely they were ever
+stripped and because this proposal doesn't have high enough AI to do so.]""")
+        }
+    }
+}
