f433b36155b94b97722f717d96accd9097f17258
Jason Cobb <jason.e.cobb@gmail.com>
2021-01-26 13:18:26 -0500

    Add voter vs. result graph

--- src/main/kotlin/org/agoranomic/assessor/stats/VoterVsResultStats.kt
+++ src/main/kotlin/org/agoranomic/assessor/stats/VoterVsResultStats.kt
@@ -1,30 +1,32 @@
 package org.agoranomic.assessor.stats
 
+import jetbrains.letsPlot.Stat
+import jetbrains.letsPlot.geom.geom_bar
+import jetbrains.letsPlot.ggsize
+import jetbrains.letsPlot.label.ggtitle
+import jetbrains.letsPlot.lets_plot
+import jetbrains.letsPlot.scale.scale_fill_discrete
+import jetbrains.letsPlot.scale.scale_x_discrete
+import jetbrains.letsPlot.scale.scale_y_continuous
 import org.agoranomic.assessor.lib.Person
 import org.agoranomic.assessor.lib.resolve.ProposalResult
 import org.agoranomic.assessor.lib.resolve.ResolutionData
 import org.agoranomic.assessor.lib.vote.VoteKind
 
-private fun calculateVoteToResultRates(
+private fun calculateVoteToResultCounts(
     voters: List<Person>,
     proposalResolutionsByVoter: Map<Person, List<ResolutionData>>,
     targetResultFor: ProposalResult,
     targetResultAgainst: ProposalResult,
-): Map<Person, Double> {
+): Map<Person, Int> {
     return voters.associateWith { voter ->
-        val resolutions = proposalResolutionsByVoter.getValue(voter)
-
-        val agreementCount = resolutions.count { resolution ->
+        proposalResolutionsByVoter.getValue(voter).count { resolution ->
             val vote = resolution.votes.voteFor(voter)
             val result = resolution.result
 
             (vote == VoteKind.FOR && result == targetResultFor) ||
                     (vote == VoteKind.AGAINST && result == targetResultAgainst)
         }
-
-        val totalCount = resolutions.count()
-
-        agreementCount.toDouble() / totalCount.toDouble()
     }
 }
 
@@ -33,23 +35,59 @@ fun writeVoterResultData(
     voters: List<Person>,
     proposalResolutionsByVoter: Map<Person, List<ResolutionData>>,
 ) {
+    val agreementCountsByVoter = calculateVoteToResultCounts(
+        voters = voters,
+        proposalResolutionsByVoter = proposalResolutionsByVoter,
+        targetResultFor = ProposalResult.ADOPTED,
+        targetResultAgainst = ProposalResult.REJECTED,
+    )
+
     writeStatistic(
         "voter_result_agreement_rate",
-        calculateVoteToResultRates(
-            voters = voters,
-            proposalResolutionsByVoter = proposalResolutionsByVoter,
-            targetResultFor = ProposalResult.ADOPTED,
-            targetResultAgainst = ProposalResult.REJECTED,
-        )
+        agreementCountsByVoter.mapValues { (voter, agreementCount) ->
+            agreementCount.toDouble() / proposalResolutionsByVoter.getValue(voter).count().toDouble()
+        }.also {}
+    )
+
+    val disagreementCountsByVoter = calculateVoteToResultCounts(
+        voters = voters,
+        proposalResolutionsByVoter = proposalResolutionsByVoter,
+        targetResultFor = ProposalResult.REJECTED,
+        targetResultAgainst = ProposalResult.ADOPTED,
     )
 
     writeStatistic(
         "voter_result_disagreement_rate",
-        calculateVoteToResultRates(
-            voters = voters,
-            proposalResolutionsByVoter = proposalResolutionsByVoter,
-            targetResultFor = ProposalResult.REJECTED,
-            targetResultAgainst = ProposalResult.ADOPTED,
-        )
+        disagreementCountsByVoter.mapValues { (voter, disagreementCount) ->
+            disagreementCount.toDouble() / proposalResolutionsByVoter.getValue(voter).count().toDouble()
+        }.also {}
+    )
+
+    writeGraph(
+        "voter_result_agreement",
+        lets_plot(data = mapOf(
+            "voter" to voters.flatMap {
+                listOf(it.name, it.name)
+            },
+            "count" to voters.flatMap {
+                listOf(agreementCountsByVoter.getValue(it), disagreementCountsByVoter.getValue(it))
+            },
+            "kind" to voters.flatMap {
+                listOf("AGREEMENT", "DISAGREEMENT")
+            },
+        )) +
+                geom_bar(stat = Stat.identity) {
+                    x = "voter"
+                    y = "count"
+                    fill = "kind"
+                } +
+                ggtitle("Voter vs. result") +
+                scale_x_discrete(name = "Voter") +
+                scale_y_continuous(name = "Count") +
+                scale_fill_discrete(name = "Kind") +
+                ggsize(
+                    voters.size * 30 + 10,
+                    1000,
+                ),
     )
 }
