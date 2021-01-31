2fb84c4977fe6937768428314f8aebfb8778f09e
Jason Cobb <jason.e.cobb@gmail.com>
2021-01-30 20:00:29 -0500

    Add margin box plots

--- src/main/kotlin/org/agoranomic/assessor/stats/Margin.kt
+++ src/main/kotlin/org/agoranomic/assessor/stats/Margin.kt
@@ -2,6 +2,7 @@ package org.agoranomic.assessor.stats
 
 import jetbrains.letsPlot.Stat
 import jetbrains.letsPlot.geom.geom_bar
+import jetbrains.letsPlot.geom.geom_boxplot
 import jetbrains.letsPlot.ggsize
 import jetbrains.letsPlot.label.ggtitle
 import jetbrains.letsPlot.lets_plot
@@ -11,30 +12,17 @@ import jetbrains.letsPlot.scale.scale_y_continuous
 import org.agoranomic.assessor.lib.Person
 import org.agoranomic.assessor.lib.resolve.ProposalResult
 import org.agoranomic.assessor.lib.resolve.ResolutionData
+import java.math.BigDecimal
 
-private data class MarginStats(
-    val average: Double,
-)
-
-private fun calculateMarginStats(data: List<ResolutionData>): MarginStats {
-    return MarginStats(
-        average = data
-            .map {
-                it.aiStrengths.strengthFor.raw.toBigDecimal() -
-                        it.aiStrengths.strengthAgainst.raw.toBigDecimal() * it.proposal.ai.raw
-            }
-            .map { it.toDouble() }
-            .onEach { check(it.isFinite()) }
-            .average()
-    )
+private fun calculateMarginStats(data: List<ResolutionData>): List<BigDecimal> {
+    return data.map {
+        it.aiStrengths.strengthFor.raw.toBigDecimal() -
+                it.aiStrengths.strengthAgainst.raw.toBigDecimal() * it.proposal.ai.raw
+    }
 }
 
-private fun Map<Person, List<ResolutionData>>.calculateValueMarginStats(): Map<Person, Double> {
-    return this
-        .filterValues { it.isNotEmpty() }
-        .mapValues { (_, v) ->
-            calculateMarginStats(v).average.also { check(it.isFinite()) }
-        }
+private fun Map<Person, List<ResolutionData>>.calculateMargins(): Map<Person, List<BigDecimal>> {
+    return this.filterValues { it.isNotEmpty() }.mapValues { (_, v) -> calculateMarginStats(v) }
 }
 
 fun writeMarginStats(authors: List<Person>, resolutions: List<ResolutionData>) {
@@ -42,77 +30,109 @@ fun writeMarginStats(authors: List<Person>, resolutions: List<ResolutionData>) {
 
     val averageAllMarginByAuthor =
         resolutionsByAuthor
-            .calculateValueMarginStats()
+            .calculateMargins()
 
     val averageAdoptedMarginByAuthor =
         resolutionsByAuthor
             .mapValues { (_, v) -> v.filter { it.result == ProposalResult.ADOPTED } }
-            .calculateValueMarginStats()
+            .calculateMargins()
 
     val averageRejectedMarginByAuthor =
         resolutionsByAuthor
             .mapValues { (_, v) -> v.filter { it.result == ProposalResult.REJECTED } }
-            .calculateValueMarginStats()
+            .calculateMargins()
 
     writeSingleMarginStat(
         authors = authors,
-        averageAdoptedMarginByAuthor = averageAllMarginByAuthor,
-        name = "author_avg_all_strength_margin",
-        graphTitle = "Average voting strength margin by author",
+        marginsByAuthor = averageAllMarginByAuthor,
+        kind = "all",
     )
 
     writeSingleMarginStat(
         authors = authors,
-        averageAdoptedMarginByAuthor = averageAdoptedMarginByAuthor,
-        name = "author_avg_adopted_strength_margin",
-        graphTitle = "Average ADOPTED voting strength margin by author",
+        marginsByAuthor = averageAdoptedMarginByAuthor,
+        kind = "adopted",
     )
 
     writeSingleMarginStat(
         authors = authors,
-        averageAdoptedMarginByAuthor = averageRejectedMarginByAuthor,
-        name = "author_avg_rejected_strength_margin",
-        graphTitle = "Average REJECTED voting strength margin by author",
+        marginsByAuthor = averageRejectedMarginByAuthor,
+        kind = "rejected",
     )
 }
 
 private fun writeSingleMarginStat(
     authors: List<Person>,
-    averageAdoptedMarginByAuthor: Map<Person, Double>,
-    name: String,
-    graphTitle: String,
+    marginsByAuthor: Map<Person, List<BigDecimal>>,
+    kind: String,
 ) {
     val graphedAdoptedMargins =
         authors
-            .filter { it in averageAdoptedMarginByAuthor.keys }
-            .associateWith { averageAdoptedMarginByAuthor.getValue(it) }
+            .filter { it in marginsByAuthor.keys }
+            .associateWith { marginsByAuthor.getValue(it) }
 
-    writeAverageMarginGraph(
+    writeMarginGraphs(
+        authors,
         graphedAdoptedMargins,
-        name,
-        graphTitle,
+        name = "author_avg_${kind}_strength_margin",
+        barGraphTitle = "Average ${kind.toUpperCase()} voting strength margin by author",
+        boxPlotTitle = "${kind.toUpperCase()} strength margins by author",
     )
 
+    val averageMarginByAuthor = marginsByAuthor.mapValues { (_, v) ->
+        v.map { it.toDouble() }.onEach { check(it.isFinite()) }.average()
+    }
+
     writeStatistic(
-        name,
-        averageAdoptedMarginByAuthor,
+        "author_avg_${kind}_strength_margin",
+        averageMarginByAuthor,
     )
 }
 
-private fun writeAverageMarginGraph(margins: Map<Person, Double>, name: String, title: String) {
+private fun writeMarginGraphs(
+    authors: List<Person>,
+    marginsByAuthor: Map<Person, List<BigDecimal>>,
+    name: String,
+    barGraphTitle: String,
+    boxPlotTitle: String,
+) {
     writeGraph(
         name,
         lets_plot(mapOf(
-            "author" to margins.map { it.key.name },
-            "margin" to margins.map { it.value },
+            "author" to marginsByAuthor.map { entry ->
+                entry.key.name
+            },
+            "margin" to marginsByAuthor.map { entry ->
+                entry.value.map { it.toDouble() }.onEach { check(it.isFinite()) }.average()
+            },
         )) +
                 geom_bar(stat = Stat.identity, sampling = sampling_none) {
                     x = "author"
                     y = "margin"
                 } +
-                ggsize(margins.keys.size * 60 + 60, 1000) +
-                scale_x_discrete(name = "Author") +
+                ggsize(marginsByAuthor.keys.size * 60 + 60, 1000) +
+                scale_x_discrete(name = "Author", limits = authors.map { it.name }) +
                 scale_y_continuous(name = "Average margin") +
-                ggtitle(title)
+                ggtitle(barGraphTitle)
+    )
+
+    val boxPlotData: List<Pair<String, Double>> = marginsByAuthor.flatMap { (author, margins) ->
+        margins.map { margin -> author.name to margin.toDouble().also { check(it.isFinite()) } }
+    }
+
+    writeGraph(
+        name + "_box_plot",
+        lets_plot(mapOf(
+            "author" to boxPlotData.map { it.first },
+            "margin" to boxPlotData.map { it.second },
+        )) +
+                geom_boxplot(sampling = sampling_none) {
+                    x = "author"
+                    y = "margin"
+                } +
+                ggsize(marginsByAuthor.keys.size * 60 + 60, 1000) +
+                scale_x_discrete(name = "Author", limits = authors.map { it.name }) +
+                scale_y_continuous(name = "Margin") +
+                ggtitle(boxPlotTitle)
     )
 }
