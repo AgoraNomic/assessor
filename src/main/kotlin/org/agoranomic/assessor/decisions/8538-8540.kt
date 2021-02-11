d04c47276cd4ec26fe3333389b199b156f63fab8
Jason Cobb <jason.e.cobb@gmail.com>
2021-02-11 15:19:22 -0500

    Initial votes by Jason
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-February/045831.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/8538-8540.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8538-8540.kt
@@ -325,5 +325,11 @@ the following text:
             FOR on 8539
             FOR on 8540
         }
+
+        votes(Jason) {
+            FOR on 8538
+            FOR on 8539
+            FOR on 8540
+        }
     }
 }
