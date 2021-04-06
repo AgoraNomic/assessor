5db9ac16bc307d6250baae639acc446650e7ca81
Jason Cobb <jason.e.cobb@gmail.com>
2021-04-06 18:57:01 -0400

    Import distribution
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2021-March/014764.html

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8556.kt
@@ -0,0 +1,34 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.coauthors
+import org.agoranomic.assessor.dsl.receivers.quorum
+
+@UseAssessment
+fun assessment8556() = assessment {
+    name("8556")
+    quorum(3)
+
+    proposals(v4) {
+        proposal(8556) {
+            title("Ministry Normalization")
+            ai("1.0")
+            author(Jason)
+            coauthors(nix)
+            ordinary()
+            sponsored()
+
+            text(
+                """
+Amend Rule 2624 by inserting the following line after the line "Ministry
+of Participation: 1 Voting Card": "Ministry of Legacy: 1 Victory Point"
+and by deleting "and 1 Victory Point to every other player whose
+Ministry Focus is Legacy".
+
+[This allows the Concentration Stone to work (at least partially) for
+players focused on Legacy.]"""
+            )
+        }
+    }
+}
