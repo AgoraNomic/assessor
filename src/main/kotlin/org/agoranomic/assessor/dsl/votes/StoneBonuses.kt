9fb1b554edad9f6fb06d4726a91507207084ce70
Jason Cobb <jason.e.cobb@gmail.com>
2021-04-06 19:00:44 -0400

    Add powerStone strength function

---
+++ src/main/kotlin/org/agoranomic/assessor/dsl/votes/StoneBonuses.kt
@@ -0,0 +1,19 @@
+package org.agoranomic.assessor.dsl.votes
+
+import org.agoranomic.assessor.dsl.receivers.GeneralVotingStrengthReceiver
+import org.agoranomic.assessor.lib.Person
+import org.agoranomic.assessor.lib.voting_strength.VotingStrengthDifference
+import org.agoranomic.assessor.lib.voting_strength.VotingStrengthModificationDescription
+
+fun GeneralVotingStrengthReceiver.powerStone(person: Person, amount: VotingStrengthDifference) {
+    person add amount describedAs VotingStrengthModificationDescription(
+        readable = "Increased by $amount due to Power Stone",
+        kind = "power_stone",
+        parameters = mapOf(
+            "bonus" to amount.toString(),
+        ),
+    )
+}
+
+fun GeneralVotingStrengthReceiver.powerStone(person: Person, amount: Int) =
+    powerStone(person, VotingStrengthDifference(amount))
