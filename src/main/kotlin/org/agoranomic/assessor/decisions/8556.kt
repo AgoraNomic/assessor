f7cc7084cc216039e814b32eea6f822d78c3f750
Jason Cobb <jason.e.cobb@gmail.com>
2021-04-08 23:33:30 -0400

    Proposal 8556 (#70)
    * Import distribution
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2021-March/014764.html
    
    * Add voting strengths
    
    Latest Referee's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2021-April/014781.html
    
    Jason wields Power Stone: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-April/046220.html
    
    * Add voting block
    
    * Initial votes by Jason
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-March/046202.html
    
    * Initial votes by ATMunn
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-March/046205.html
    
    * Initial votes by Trigon
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-March/046211.html
    
    * Initial votes by G.
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-March/046213.html
    
    * G. withdraws votes
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-March/046214.html
    
    * Initial votes by Falsifian
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-March/046216.html
    
    * Initial votes by nix
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-April/046232.html
    
    * Initial votes by Murphy
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-April/046260.html
    
    * Add url

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8556.kt
@@ -0,0 +1,76 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.coauthors
+import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.receivers.url
+import org.agoranomic.assessor.dsl.votes.blotPenalty
+import org.agoranomic.assessor.dsl.votes.endorse
+import org.agoranomic.assessor.dsl.votes.powerStone
+import org.agoranomic.assessor.lib.vote.VoteKind.FOR
+import org.agoranomic.assessor.lib.vote.VoteKind.PRESENT
+
+@UseAssessment
+fun assessment8556() = assessment {
+    name("8556")
+    url("https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2021-April/014790.html")
+    quorum(3)
+
+    strengths {
+        default(3)
+        min(0)
+        max(15)
+
+        blotPenalty(RLee, 7 / 3)
+        powerStone(Jason, 3)
+    }
+
+    proposals(v4) {
+        proposal(8556) {
+            title("Ministry Normalization")
+            ai("1.0")
+            author(Jason)
+            coauthors(nix)
+            ordinary()
+            sponsored()
+
+            text(
+                """
+Amend Rule 2624 by inserting the following line after the line "Ministry
+of Participation: 1 Voting Card": "Ministry of Legacy: 1 Victory Point"
+and by deleting "and 1 Victory Point to every other player whose
+Ministry Focus is Legacy".
+
+[This allows the Concentration Stone to work (at least partially) for
+players focused on Legacy.]"""
+            )
+        }
+    }
+
+    voting {
+        votes(Jason) {
+            endorse(nix) on 8556
+        }
+
+        votes(ATMunn) {
+            FOR on 8556
+        }
+
+        votes(Trigon) {
+            FOR on 8556
+        }
+
+        votes(Falsifian) {
+            endorse(Jason) on 8556
+        }
+
+        votes(nix) {
+            FOR on 8556
+        }
+
+        votes(Murphy) {
+            PRESENT on 8556
+        }
+    }
+}
