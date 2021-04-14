13fbd25f0687cb5bc5b1ed36f4ade3d38d3d40b9
Jason Cobb <jason.e.cobb@gmail.com>
2021-04-14 18:35:33 -0400

    Proposals 8549-8555 (#71)
    * Import "distribution"
    
    Close enough
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2021-March/014743.html
    
    * Add voting strengths
    
    Referee report at end of voting period: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2021-April/014781.html
    
    Jason wields Power Stone: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-March/046183.html
    
    * Add voting block
    
    * Initial votes by Aris
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-March/046168.html
    
    * Initial votes by Jason
    
    Source 1: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-March/046155.html
    
    Source 2: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2021-March/014744.html
    
    * Initial votes by Gaelan
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-March/046169.html
    
    * Initial votes by Murphy
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-March/046177.html
    
    * Initial votes by Cuddlebeam
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-March/046182.html
    
    * Initial votes by Falsifian
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-March/046184.html
    
    * Initial votes by cuddlybanana
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-March/046185.html
    
    * Initial votes by Trigon
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-March/046190.html
    
    * Initial votes by R. Lee
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-March/046192.html
    
    * Initial votes by Aenet
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-March/046193.html
    
    * Initial votes by ATMunn
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-March/046194.html
    
    * Initial votes by G.
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-March/046195.html
    
    * G. wields Sabotage Stone specifying P8552
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-March/046195.html
    
    * Resolve conditional votes

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8549-8555.kt
@@ -0,0 +1,225 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.coauthors
+import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.votes.blotPenalty
+import org.agoranomic.assessor.dsl.votes.endorse
+import org.agoranomic.assessor.dsl.votes.powerStone
+import org.agoranomic.assessor.lib.proposal.DecisionAI
+import org.agoranomic.assessor.lib.proposal.ProposalAI
+import org.agoranomic.assessor.lib.vote.VoteKind.*
+import java.math.BigDecimal
+
+@UseAssessment
+fun assessment8549to8555() = assessment {
+    name("8549-8555")
+    quorum(3)
+
+    strengths {
+        default(3)
+        min(0)
+        max(15)
+
+        blotPenalty(RLee, 7 / 3)
+        powerStone(Jason, 3)
+    }
+
+    proposals(v4) {
+        proposal(8549) {
+            title("You Can Only Stack Turtles So High")
+            ai("2.2")
+            author(Aris)
+            coauthors(Gaelan, G)
+            ordinary()
+            sponsored()
+
+            text(
+                """
+[This fixes G.'s scam, which would otherwise allow indeterminacy to be
+created through infinite promise loops.]
+
+Amend Rule 2618, "Promises", by inserting, after the text:
+
+  A promise's bearer CAN, by announcement, cash the promise,
+  provided that any conditions for cashing it specified by its text
+  are unambiguously met. By doing so, e acts on the creator of the
+  promise's behalf, causing the creator to act as if e published the
+  promise's text, and destroys the promise.
+
+the text:
+
+  However, to limit recursion, no promise can be cashed during the execution
+  of another promise unless it existed before the execution of that promise
+  began."""
+            )
+        }
+
+        proposal(8552) {
+            title("Apathy for all")
+            proposalAI(ProposalAI(BigDecimal("1.0")))
+            decisionAI(DecisionAI(BigDecimal("2.0")))
+            author(Jason)
+            coauthors(ais523)
+            ordinary()
+            sponsored()
+
+            text(
+                """
+Amend Rule 2465 by replacing, as a single amendment, the text "set of
+players" with "set of persons" and the text "specified players" with
+"specified persons"."""
+            )
+        }
+
+        proposal(8553) {
+            title("Slightly less apathetic")
+            ai("1.0")
+            author(Jason)
+            ordinary()
+            sponsored()
+
+            text(
+                """
+Amend Rule 2465, "Victory by Apathy", by replacing the final "."
+character with the following: ", except for those who have won the game
+through this Rule in the past 14 days.""""
+            )
+        }
+
+        proposal(8554) {
+            title("I Want My Promise Back!")
+            ai("2.2")
+            author(Aris)
+            coauthors(nix)
+            ordinary()
+            sponsored()
+
+            text(
+                """
+[This is intended to remove the need to put in a special provision
+to a promise allow the creator to destroy a promise in the possession of
+the Library. Since that's usually what's wanted, this makes it the
+default behavior, while still allowing the creator to override it.]
+
+Amend Rule 2618, "Promises", by inserting, after the text:
+
+  The Library is an entity and CAN own promises. Any player CAN take
+  a specified promise from the Library by announcement, provided e
+  cashes the promise in the same message.
+
+the text:
+
+  The creator of a promise CAN take or revoke it from the Library by
+  announcement, unless the promise's text unambiguously designates
+  it as irrevocable."""
+            )
+        }
+
+        proposal(8555) {
+            title("something to vote on")
+            ai("1.0")
+            author(G)
+            ordinary()
+            sponsored()
+
+            text(
+                """
+The player who voted unambiguously FOR, and had the highest
+unique[*] voting strength (at the end of the voting period) on,
+the decision to adopt this proposal
+
+                      is hereby granted
+
+                  ****  1 Victory Card   *****
+
+[* "Highest unique" means if the top two are 15 and the next
+highest is 14, and no one else is at 14, the 14 wins]."""
+            )
+        }
+    }
+
+    voting {
+        votes(Aris) {
+            FOR on 8549
+            AGAINST on 8552
+            AGAINST on 8553
+            FOR on 8554
+            PRESENT on 8555
+        }
+
+        votes(Jason) {
+            FOR on 8549
+            FOR on 8552
+            FOR on 8553
+            FOR on 8554
+            FOR on 8555
+        }
+
+        votes(Gaelan) {
+            FOR on 8549
+            AGAINST on 8552
+            FOR on 8553
+            FOR on 8554
+            AGAINST on 8555 comment conditional("not highest unique voting strenght")
+        }
+
+        votes(Murphy) {
+            FOR on 8549
+            FOR on 8552
+            FOR on 8553
+            FOR on 8554
+            FOR on 8555
+        }
+
+        votes(CuddleBeam) {
+            FOR on all
+        }
+
+        votes(Falsifian) {
+            endorse(Aris) on 8549
+            AGAINST on 8552
+            PRESENT on 8553
+            endorse(Aris) on 8554
+            AGAINST on 8555
+        }
+
+        votes(cuddlybanana) {
+            FOR on all
+        }
+
+        votes(Trigon) {
+            FOR on 8549
+            PRESENT on 8552
+            AGAINST on 8553
+            FOR on 8554
+            FOR on 8555
+        }
+
+        votes(RLee) {
+            AGAINST on 8549 comment conditional("adds text to the ruleset")
+            PRESENT on 8552 comment conditional("does not add text to the ruleset")
+            AGAINST on 8553 comment conditional("adds text to the ruleset")
+            AGAINST on 8554 comment conditional("adds text to the ruleset")
+            PRESENT on 8555 comment conditional("does not add text to the ruleset")
+        }
+
+        votes(Aenet) {
+            AGAINST on 8552
+            FOR on others
+        }
+
+        votes(ATMunn) {
+            FOR on 8549
+            PRESENT on 8552
+            PRESENT on 8553
+            FOR on 8554
+            AGAINST on 8555
+        }
+
+        votes(G) {
+            AGAINST on all
+        }
+    }
+}
