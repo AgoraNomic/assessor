534d40075a5e0ad09bc5551ff88b89331b48198c
Jason Cobb <jason.e.cobb@gmail.com>
2021-05-17 21:28:05 -0400

    Proposal 8557 (#72)
    * Import distribution
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2021-April/014789.html
    
    * Add strengths
    
    Referee's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2021-April/014818.html
    
    * Add voting block
    
    * Initial votes by Trigon
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-April/046269.html
    
    * Initial votes by Aris
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-April/046270.html
    
    * Initial votes by Falsifian
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-April/046276.html
    
    * Initial votes by Jason
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-April/046277.html
    
    * Initial votes by Murphy
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-April/046282.html

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8557.kt
@@ -0,0 +1,126 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.coauthors
+import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.votes.blotPenalty
+import org.agoranomic.assessor.lib.vote.VoteKind.FOR
+import org.agoranomic.assessor.lib.vote.VoteKind.PRESENT
+
+@UseAssessment
+fun assessment8557() = assessment {
+    name("8557")
+    quorum(3)
+
+    strengths {
+        default(3)
+        min(0)
+        max(15)
+
+        blotPenalty(RLee, 7 / 3)
+    }
+
+    proposals(v4) {
+        proposal(8557) {
+            title("Archimedes' Principle")
+            ai("2.0")
+            author(Aris)
+            coauthors(Trigon, nix)
+            ordinary()
+            sponsored()
+
+            text(
+                """
+[Changes:
+  -The Treasuror sets a target for the Total Buoyancy every week;
+   the most recent target takes effect at the beginning of the month
+  -The Unit of Floatation is now rounded, so people can actually remember
+   it (unlike the draft, I'm rounding up, to deal with cases
+   where it's evenly between two integers)
+  -The Treasuror no longer has a monthly report, and is instead
+   just encouraged to make sure the Total Buoyancy and Unit of Floatation
+   are published
+]
+
+Amend Rule 2634, "Buoyancy Control", by changing it to read in full:
+
+  The Total Buoyancy and Buoyancy Target are singleton
+  integer switches, tracked by the Treasuror in eir weekly report.
+
+  The Treasuror CAN, by announcement, set the Buoyancy Target
+  to a specified value approximately equal to the sum of all coin
+  balances at a specified point within the last week,
+  and SHALL do so each time e publishes eir weekly report.
+
+  The Treasuror may exercise reasonable judgement in calculating the
+  Buoyancy Target. The Buoyancy Target will be deemed set so long as
+  the value chosen by the Treasuror is not obviously and grossly
+  incorrect. However, the Treasuror SHALL NOT deliberately introduce
+  error into the Buoyancy Target.
+
+  At the beginning of each month, the Total Buoyancy is flipped to
+  the Buoyancy Target.
+
+Amend Rule 2635, "Floating Rate Fleet", by changing it to read in full:
+
+  The Unit of Flotation is equal to 1/2500 times the Total Buoyancy,
+  rounded up, and is tracked in the Treasuror's weekly report.
+  A boatload of something is a quantity of that thing equal
+  in count to the Unit of Floatation.
+
+  The Treasuror is ENCOURAGED to arrange for the new Total Buoyancy
+  and Unit of Flotation to be published as close as possible
+  to the beginning of the month.
+
+
+[No clue why this should be secured — granting assets isn't secured.
+The reason I'm amending this is to make the payday happen
+after the boatload value update; I'm open to other phrasings.]
+
+Amend Rule 2559, "Paydays", by replacing:
+
+  The occurrence of Paydays is secured. At the beginning of each
+  month, a Payday occurs.
+
+with:
+
+  At the beginning of each month, after all other events that
+  take place at the beginning of the month, a Payday occurs.
+
+
+  Amend Rule 2559, "Paydays", by replacing:
+
+    The occurrence of Paydays is secured. At the beginning of each
+    month, a Payday occurs.
+
+  with:
+
+    At the beginning of each month, after all other events that
+    take place at the beginning of the month, a Payday occurs."""
+            )
+        }
+    }
+
+    voting {
+        votes(Trigon) {
+            FOR on 8557
+        }
+
+        votes(Aris) {
+            FOR on 8557
+        }
+
+        votes(Falsifian) {
+            FOR on 8557
+        }
+
+        votes(Jason) {
+            PRESENT on 8557
+        }
+
+        votes(Murphy) {
+            FOR on 8557
+        }
+    }
+}
