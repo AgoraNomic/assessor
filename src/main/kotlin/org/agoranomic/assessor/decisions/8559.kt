69b7e0aad241ebca140e809971be4aba02e5239d
Jason Cobb <jason.e.cobb@gmail.com>
2021-06-04 15:37:31 -0400

    Proposal 8559 (#74)
    * Import distribution
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2021-May/014911.html
    
    * Add voting strengths
    
    Latest Referee's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2021-May/014926.html
    
    Jason wields Power Stone: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-May/046456.html
    
    * Add voting block
    
    * Initial votes by Falsifian
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-May/046439.html
    
    * Initial votes by Jason
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-May/046442.html
    
    * Initial votes by ATMunn
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-May/046449.html
    
    * Initial votes by Murphy
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-May/046457.html
    
    * Add url

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8559.kt
@@ -0,0 +1,65 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.coauthors
+import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.receivers.url
+import org.agoranomic.assessor.dsl.votes.blotPenalty
+import org.agoranomic.assessor.dsl.votes.endorse
+import org.agoranomic.assessor.dsl.votes.powerStone
+import org.agoranomic.assessor.lib.vote.VoteKind.FOR
+
+@UseAssessment
+fun assessment8559() = assessment {
+    name("8559")
+    quorum(3)
+    url("https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2021-May/014927.html")
+
+    strengths {
+        default(3)
+        min(0)
+        max(15)
+
+        blotPenalty(RLee, 7 / 3)
+        powerStone(Jason, 3)
+    }
+
+    proposals(v4) {
+        proposal(8559) {
+            title("Paper Shredding")
+            ai("2.5")
+            author(ATMunn)
+            coauthors(G, Aris)
+            democratic()
+            sponsored()
+
+            text(
+                """
+Amend Rule 1742 "Contracts" by appending the following:
+
+  Any player CAN cause a contract to be terminated (syn. "shred")
+  without 2 objections. This SHOULD only be done to remove unused
+  contracts or contracts deemed detrimental to the game as a whole."""
+            )
+        }
+    }
+
+    voting {
+        votes(Falsifian) {
+            endorse(ATMunn) on 8559
+        }
+
+        votes(Jason) {
+            endorse(ATMunn) on 8559
+        }
+
+        votes(ATMunn) {
+            FOR on 8559
+        }
+
+        votes(Murphy) {
+            FOR on 8559
+        }
+    }
+}
