93fb913bfbf84516a0fab522b6c73cf1c94d58ca
Jason Cobb <jason.e.cobb@gmail.com>
2021-06-17 15:50:16 -0400

    Proposal 8560 (#75)
    * Import distribution
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2021-May/014925.html
    
    * Add voting strengths
    
    Referee's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2021-May/014926.html
    
    Jason wields Power Stone: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-May/046471.html
    
    * Add voting block
    
    * Initial votes by Jason
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-May/046462.html
    
    * Initial votes by Falsifian
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-May/046468.html
    
    * Initial votes by Cuddlebeam
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-May/046472.html
    
    * Initial votes by Murphy
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-May/046486.html
    
    * Add url

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8560.kt
@@ -0,0 +1,78 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.coauthors
+import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.receivers.url
+import org.agoranomic.assessor.dsl.votes.blotPenalty
+import org.agoranomic.assessor.dsl.votes.endorse
+import org.agoranomic.assessor.dsl.votes.powerStone
+import org.agoranomic.assessor.lib.vote.VoteKind.FOR
+
+@UseAssessment
+fun assessment8560() = assessment {
+    name("8560")
+    url("https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2021-June/014954.html")
+    quorum(3)
+
+    strengths {
+        default(3)
+        min(0)
+        max(15)
+
+        blotPenalty(RLee, 7 / 3)
+        powerStone(Jason, 3)
+    }
+
+    proposals(v4) {
+        proposal(8560) {
+            title("simplify indictments")
+            ai("1.7")
+            author(G)
+            coauthors(nix)
+            ordinary()
+            sponsored()
+
+            text(
+                """
+Repeal Rule 2619 (Indictment).
+
+Amend Rule 2557 (Sentencing Guidelines) by appending the following paragraph:
+
+  When the rules authorize an investigator to issue an Indictment
+  for a violation, e CAN do so by levying a fine on the perp, with
+  1.5 Agoran Consent, provided that levying the fine would be
+  otherwise LEGAL and EFFECTIVE.
+
+[
+Turns indictment into a simple "if we want to apply a punishment outside
+the 1...2xBase violation range (either higher or lower), we do so with 1.5
+Consent and call that an indictment."  The 1.5 level was chosen to match
+the AI of the current indictment proposals.
+
+The "otherwise LEGAL and EFFECTIVE" clause is so that this ability is
+still limited by Defendant's Rights.
+]"""
+            )
+        }
+    }
+
+    voting {
+        votes(Jason) {
+            FOR on 8560
+        }
+
+        votes(Falsifian) {
+            endorse(G) on 8560
+        }
+
+        votes(CuddleBeam) {
+            FOR on all
+        }
+
+        votes(Murphy) {
+            endorse(Aenet) on 8560
+        }
+    }
+}
