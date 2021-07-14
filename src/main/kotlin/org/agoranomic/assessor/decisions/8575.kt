5422f013899c7cf53265648098c0a30ed4159c34
Jason Cobb <jason.e.cobb@gmail.com>
2021-07-14 12:31:01 -0400

    Import distribution
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2021-July/015077.html

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8575.kt
@@ -0,0 +1,58 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.quorum
+
+// Due to a mishap, there are two Proposal 8575s. This should eventually be renumbered to have a unique number assigned
+// by the Promotor.
+@UseAssessment
+fun assessment8575() = assessment {
+    name("8575")
+    quorum(5)
+
+    proposals(v4) {
+        proposal(8575) {
+            title("Nicer Definitions")
+            ai("3.0")
+            author(RLee)
+            democratic()
+            sponsored()
+
+            text("""
+Amend rule 2152, "Mother May I", by replacing
+
+  "SHOULD NOT, DISCOURAGED, DEPRECATED: Before performing the
+  described action, the full implications of performing it should
+   be understood and carefully weighed."
+
+with
+
+
+  ""SHOULD NOT, DISCOURAGED, DEPRECATED: Before performing the
+  described action, the full implications of performing it ought to be
+  be understood and carefully weighed."
+
+
+and also by replacing
+
+  "SHOULD, ENCOURAGED, RECOMMENDED: Before failing to perform the
+   described action, the full implications of failing to perform
+   it should (in the ordinary-language sense) be understood and
+   carefully weighed."
+
+with
+
+  "SHOULD, ENCOURAGED, RECOMMENDED: Before failing to perform the
+  described action, the full implications of failing to perform
+  it ought to be understood and
+  carefully weighed."
+
+[Explanation: The word SHOULD is currently defined as meaning the word
+should. This is not technically recursive because the upper case terms are
+terms of art, but it looks awful. This proposal means that it becomes
+"ought to", which just looks better. I also get to remove a terribly clunky
+parenthetical.]""")
+        }
+    }
+}
