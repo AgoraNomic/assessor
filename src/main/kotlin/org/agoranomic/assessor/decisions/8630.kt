d1fd066a87598a3bd42dbd7ca8dd33858f817b66
Jason Cobb <jason.e.cobb@gmail.com>
2021-10-17 21:58:42 -0400

    Add voting strengths
    Latest Referee's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2021-October/015382.html
    
    Jason wields power stone: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-October/047709.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/8630.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8630.kt
@@ -2,6 +2,9 @@ package org.agoranomic.assessor.decisions
 
 import org.agoranomic.assessor.dsl.assessment
 import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.votes.festival
+import org.agoranomic.assessor.dsl.votes.onOrdinaryProposals
+import org.agoranomic.assessor.dsl.votes.powerStone
 import org.agoranomic.assessor.lib.proposal.DecisionAI
 import org.agoranomic.assessor.lib.proposal.ProposalAI
 
@@ -9,6 +12,34 @@ import org.agoranomic.assessor.lib.proposal.ProposalAI
 fun assessment8630() = assessment {
     name("8630")
     quorum(5)
+
+    strengths {
+        festival(
+            minStrength = 0,
+            maxStrength = 15,
+            festivePlayers = setOf(
+                ais523,
+                Aspen,
+                ATMunn,
+                CuddleBeam,
+                cuddlybanana,
+                Falsifian,
+                G,
+                Gaelan,
+                Jason,
+                Murphy,
+                nix,
+                omd,
+                PSS,
+                Telna,
+                Trigon,
+            )
+        )
+
+        onOrdinaryProposals {
+            powerStone(Jason, 3)
+        }
+    }
 
     proposals(v4) {
         proposal(8630) {
