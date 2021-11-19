ceb3fe39cf292f38b2a0f371f82d6a0122961df0
Jason Cobb <jason.e.cobb@gmail.com>
2021-11-19 14:29:28 -0500

    Proposal 8630 (#86)
    * Import distribution
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2021-October/015379.html
    
    * Jason wields Sabotage Stone, specifying referendum on 8630
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-October/047708.html
    
    * Add voting strengths
    
    Latest Referee's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2021-October/015382.html
    
    Jason wields power stone: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-October/047709.html
    
    * Add voting block
    
    * Initial votes by Shy Owl
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-October/047704.html
    
    * Initial votes by ATMunn
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-October/047706.html
    
    * Initial votes by G.
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-October/047710.html
    
    * ShyOwl changes vote on 8630
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-October/047711.html
    
    * Initial votes by Jason
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-October/047717.html
    
    * Shy Owl changes vote on 8630
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-October/047724.html
    
    * Initial votes by nix
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-October/047728.html
    
    * Shy Owl retracts vote on 8630
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-October/047736.html
    
    * Initial votes by ais523
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-October/047737.html
    
    * Initial votes by Gaelan
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-October/047739.html
    
    * Initial votes by Falsifian
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-October/047755.html
    
    * Initial votes by Telna
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-October/047762.html
    
    * Initial votes by Trigon
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-October/047773.html
    
    * Initial votes by Cuddlebeam, also causing cuddlybanana to vote
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-October/047783.html
    
    * Initial votes by Murphy
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2021-October/047816.html
    
    * Add url

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8630.kt
@@ -0,0 +1,118 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.receivers.url
+import org.agoranomic.assessor.dsl.votes.endorse
+import org.agoranomic.assessor.dsl.votes.festival
+import org.agoranomic.assessor.dsl.votes.onOrdinaryProposals
+import org.agoranomic.assessor.dsl.votes.powerStone
+import org.agoranomic.assessor.lib.proposal.DecisionAI
+import org.agoranomic.assessor.lib.proposal.ProposalAI
+import org.agoranomic.assessor.lib.vote.VoteKind.*
+
+@UseAssessment
+fun assessment8630() = assessment {
+    name("8630")
+    url("https://agoranomic.groups.io/g/main/topic/assessor_resolution_of/86405309")
+    quorum(5)
+
+    strengths {
+        festival(
+            minStrength = 0,
+            maxStrength = 15,
+            festivePlayers = setOf(
+                ais523,
+                Aspen,
+                ATMunn,
+                CuddleBeam,
+                cuddlybanana,
+                Falsifian,
+                G,
+                Gaelan,
+                Jason,
+                Murphy,
+                nix,
+                omd,
+                PSS,
+                Telna,
+                Trigon,
+            )
+        )
+
+        onOrdinaryProposals {
+            powerStone(Jason, 3)
+        }
+    }
+
+    proposals(v4) {
+        proposal(8630) {
+            title("A Very Merry Unvictory to Me! ")
+            proposalAI(ProposalAI("3.0".toBigDecimal()))
+            decisionAI(DecisionAI("3.0".toBigDecimal()))
+            author(CuddleBeam)
+            democratic()
+            sponsored()
+
+            text("""
+Ratify the following: {Cuddlebeam has not won the game by Proposal.}
+
+Then, if it is possible to do, revoke the Title of Champion (by Proposal)
+from Cuddlebeam.""")
+        }
+    }
+
+    voting {
+        votes(ShyOwl) {
+            // NO VOTE on 8630
+        }
+
+        votes(ATMunn) {
+            FOR on 8630
+        }
+
+        votes(G) {
+            AGAINST on 8630
+        }
+
+        votes(Jason) {
+            AGAINST on 8630
+        }
+
+        votes(nix) {
+            PRESENT on 8630
+        }
+
+        votes(ais523) {
+            endorse(CuddleBeam) on 8630
+        }
+
+        votes(Gaelan) {
+            AGAINST on 8630
+        }
+
+        votes(Falsifian) {
+            AGAINST on 8630
+        }
+
+        votes(Telna) {
+            FOR on 8630
+        }
+
+        votes(Trigon) {
+            AGAINST on 8630
+        }
+
+        votes(CuddleBeam) {
+            FOR on all
+        }
+
+        votes(cuddlybanana) {
+            FOR on all
+        }
+
+        votes(Murphy) {
+            AGAINST on 8630
+        }
+    }
+}
