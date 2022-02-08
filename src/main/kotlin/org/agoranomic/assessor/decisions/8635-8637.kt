cec94901c57bccf1b3ff997520a45b56f181c92f
Jason Cobb <jason.e.cobb@gmail.com>
2022-02-08 13:53:30 -0500

    Initial votes by ais523
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2022-January/048139.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/8635-8637.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8635-8637.kt
@@ -5,6 +5,9 @@ import org.agoranomic.assessor.dsl.receivers.ai
 import org.agoranomic.assessor.dsl.receivers.coauthors
 import org.agoranomic.assessor.dsl.receivers.quorum
 import org.agoranomic.assessor.dsl.votes.blotPenalty
+import org.agoranomic.assessor.lib.vote.VoteKind
+import org.agoranomic.assessor.lib.vote.VoteKind.AGAINST
+import org.agoranomic.assessor.lib.vote.VoteKind.PRESENT
 
 @UseAssessment
 fun assessment8635to8637() = assessment {
@@ -99,5 +102,10 @@ iscapped at 4 by R106].
     }
 
     voting {
+        votes(ais523) {
+            AGAINST on 8635
+            PRESENT on 8636
+            // TODO: resolve conditional vote on 8637: PRESENT if 8636 has been enacted, else AGAINST
+        }
     }
 }
