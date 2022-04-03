ee17f1ea317679631bb90a90235e386dfbb516d2
Jason Cobb <jason.e.cobb@gmail.com>
2022-04-02 21:46:39 -0400

    Initial votes by G.
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2022-March/048657.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/8655-8656.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8655-8656.kt
@@ -100,5 +100,9 @@ points" with "the associated amount of points, rounded down".""")
             FOR on 8655
             AGAINST on 8656
         }
+
+        votes(G) {
+            AGAINST on all
+        }
     }
 }
