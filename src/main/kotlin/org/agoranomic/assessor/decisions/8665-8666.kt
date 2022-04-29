56c6229f36a980c20c0fbb87375da690c712cde7
Jason Cobb <jason.e.cobb@gmail.com>
2022-04-28 20:47:27 -0400

    Import distribution
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2022-April/015842.html

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8665-8666.kt
@@ -0,0 +1,65 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.coauthors
+import org.agoranomic.assessor.dsl.receivers.quorum
+
+@UseAssessment
+fun assessment8665to8666() = assessment {
+    name("8665-8666")
+    quorum(5)
+
+    proposals(v4) {
+        proposal(8665) {
+            title("Promise Expirations 1.1")
+            ai("3.0")
+            author(nix)
+            coauthors(Jason, ais523)
+            democratic()
+            sponsored()
+
+            text("""
+Amend 2618 by replacing:
+
+  The Library is an entity and CAN own promises. Any player CAN take
+  a specified promise from the Library by announcement, provided e
+  cashes the promise in the same message. The creator of a promise
+  CAN take or revoke it from the Library by announcement, unless the
+  promise's text unambiguously designates it as irrevocable. Any
+  player CAN revoke a specified promise from the Library without
+  objection.
+
+with:
+
+  The Library is an entity and CAN own promises. Any player CAN take
+  a specified promise from the Library by announcement, provided e
+  cashes the promise in the same message.
+
+  The creator of a promise CAN take or revoke it from the Library by
+  announcement, unless the promise's text unambiguously designates
+  it as irrevocable. Any player CAN revoke a specified promise from
+  the Library without objection. If a promise specifies expiration
+  conditions then any player CAN by announcement destroy it while
+  the expiration conditions are unambiguously met.""")
+        }
+
+        proposal(8666) {
+            title("The Secretarybird")
+            ai("1.0")
+            author(Secretsnail9)
+            coauthors(nix)
+            ordinary()
+            sponsored()
+
+            text("""
+Amend Rule 2665 (The Birds) by appending the list item:
+{
+
+  - Secretarybird: The playmate gains 1 stamp with the type of a
+    specified active player.
+
+}""")
+        }
+    }
+}
