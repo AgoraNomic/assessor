9c80b2d09c551150e2e457c0b7338ec21a79955e
Jason Cobb <jason.e.cobb@gmail.com>
2022-05-14 21:17:54 -0400

    Add voting strengths
    Latest Referee's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2022-May/015932.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/8672-8674.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8672-8674.kt
@@ -3,11 +3,22 @@ package org.agoranomic.assessor.decisions
 import org.agoranomic.assessor.dsl.assessment
 import org.agoranomic.assessor.dsl.receivers.ai
 import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.votes.blotPenalty
 
 @UseAssessment
 fun assessment8672to8674() = assessment {
     name("8672-8674")
     quorum(5)
+
+    strengths {
+        default(3)
+        min(0)
+        max(15)
+
+        blotPenalty(nix, 5 / 3)
+        blotPenalty(cuddlybanana, 3 / 3)
+        blotPenalty(Jason, 3 / 3)
+    }
 
     proposals(v4) {
         proposal(8672) {
