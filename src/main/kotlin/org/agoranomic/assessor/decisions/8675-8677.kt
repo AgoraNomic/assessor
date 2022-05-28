f3f24d324b8a70301e288f73b4d440e25adacb2f
Jason Cobb <jason.e.cobb@gmail.com>
2022-05-28 12:59:38 -0400

    Proposals 8675-8677 (#100)
    * Import distribution
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2022-May/015949.html
    
    * Add voting strengths
    
    Latest Referee's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2022-May/015962.html
    
    Jason expunges one blot: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2022-May/049159.html
    
    * Add voting block
    
    * Initial votes by ais523
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2022-May/049128.html
    
    * Initial votes by juan
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2022-May/049129.html
    
    * Initial votes by Jason
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2022-May/049132.html
    
    * Initial votes by G.
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2022-May/049134.html
    
    * Initial votes by Madrid
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2022-May/049135.html
    
    * Initial votes by Secretsnail9
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2022-May/049136.html
    
    * Initial votes by Murphy
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2022-May/049166.html
    
    * Add url

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8675-8677.kt
@@ -0,0 +1,118 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.receivers.url
+import org.agoranomic.assessor.dsl.votes.blotPenalty
+import org.agoranomic.assessor.dsl.votes.endorse
+import org.agoranomic.assessor.lib.vote.VoteKind.*
+
+@UseAssessment
+fun assessment8675to8677() = assessment {
+    name("8675-8677")
+    url("https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2022-May/015979.html")
+    quorum(4)
+
+    strengths {
+        default(3)
+        min(0)
+        max(15)
+
+        blotPenalty(cuddlybanana, 3 / 3)
+    }
+
+    proposals(v4) {
+        proposal(8675) {
+            title("Let's make it clear")
+            ai("1.0")
+            author(Secretsnail9)
+            ordinary()
+            sponsored()
+
+            text("""
+Amend Rule 2621 (VP Wins) by replacing "If a player has at least 20 more
+Winsomes than any other player" with "If a player has at least 20 more
+Winsomes than all other players".""")
+        }
+
+        proposal(8676) {
+            title("Let's make it entirely clear")
+            ai("1.0")
+            author(Secretsnail9)
+            ordinary()
+            sponsored()
+
+            text("""
+Amend Rule 2621 (VP Wins) by replacing "If a player has at least 20 more
+Winsomes than any other player" with "If a player has at least 20 more
+Winsomes than each other player".""")
+        }
+
+        proposal(8677) {
+            title("Endgame")
+            ai("1.0")
+            author(G)
+            ordinary()
+            sponsored()
+
+            text("""
+Create the following power=1 rule, titled Buyout:
+
+     Any player who has not taken over the economy in the last 30
+     days CAN pay a fee of N Winsomes to create 500 times N coins
+     in eir possession, provided e does so unconditionally and
+     without disclaimers, acting as emself, in a message body
+     containing no other actions or other action attempts, and explicitly
+     specifies N in the message (i.e. without indirect references such
+     as "all").
+
+     One week after this rule first takes effect, the winds die down.
+
+     Immediately after Rule 2658 (The Winds Die Down) is repealed,
+     this rule is repealed.""")
+        }
+    }
+
+    voting {
+        votes(ais523) {
+            AGAINST on 8675
+            FOR on 8676
+            AGAINST on 8677
+        }
+
+        votes(juan) {
+            AGAINST on 8675
+            FOR on 8676
+            FOR on 8677
+        }
+
+        votes(Jason) {
+            AGAINST on 8675
+            PRESENT on 8676
+            endorse(ais523) on 8677
+        }
+
+        votes(G) {
+            AGAINST on 8675
+            FOR on 8676
+            endorse(Secretsnail9) on 8677 comment "Secretsnail9 is the Treasuror"
+        }
+
+        votes(Madrid) {
+            FOR on all
+        }
+
+        votes(Secretsnail9) {
+            FOR on 8675
+            FOR on 8676
+            AGAINST on 8677
+        }
+
+        votes(Murphy) {
+            AGAINST on 8675
+            FOR on 8676
+            FOR on 8677
+        }
+    }
+}
