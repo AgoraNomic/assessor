027f736d4d977db100e0e3243a695a1a4b6a46d9
Jason Cobb <jason.e.cobb@gmail.com>
2022-06-17 23:18:04 -0400

    Import distribution
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2022-June/016034.html

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8682.kt
@@ -0,0 +1,30 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.quorum
+
+@UseAssessment
+fun assessment8682() = assessment {
+    name("8682")
+    quorum(4)
+
+    proposals(v4) {
+        proposal(8682) {
+            title("Temptation")
+            ai("1.0")
+            author(Secretsnail9)
+            ordinary()
+            sponsored()
+
+            text("""
+Enact a new rule, with the title "Succumbing to Time" and the following
+text:
+
+  Once per month, a player CAN Succumb by announcement. When a player
+Succumbs, each of eir deadlines to perform an Officer's duty or judge a
+Call for Judgement is extended by 1 day if it would otherwise expire within
+the next week.""")
+        }
+    }
+}
