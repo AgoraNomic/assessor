b15a297778f6cbee77de2996593b3cbf063b736c
Jason Cobb <jason.e.cobb@gmail.com>
2022-07-02 17:51:03 -0400

    Import distribution
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2022-June/016076.html
    
    CFJ 3790 finds "Dream of Wandering" does not exist: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2022-June/049436.html

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8696-8699.kt
@@ -0,0 +1,81 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.coauthors
+import org.agoranomic.assessor.dsl.receivers.quorum
+
+@UseAssessment
+fun assessment8696to8699() = assessment {
+    name("8696-8699")
+    quorum(4)
+
+    proposals(v4) {
+        proposal(8696) {
+            title("I just win in this too")
+            ai("1.0")
+            author(Forest)
+            coauthors(Secretsnail9)
+            ordinary()
+
+            text("""
+Enact a new rule with title "Very fair rule 2.0" and the text:
+
+The player 4st CAN win the game by announcement.""")
+        }
+
+        proposal(8698) {
+            title("Everyone is secretsnail")
+            ai("1.0")
+            author(Forest)
+            ordinary()
+
+            text("""
+Enact a new rule with title "Fixing the very fair rule" and the text:
+If a rule called "Very fair rule" is in the ruleset, then:
+- Everyone has the role "The player secretsnail". Once a player wins, they
+lose this role.
+
+This rule explicitly takes precedence over the rule called "Very fair rule".
+After "Very fair rule" is repealed, or after it is not in any current
+proposals, repeal this rule the next day.
+By Agoran Consent, a player can repeal "Very fair rule".""")
+        }
+
+        proposal(8699) {
+            title("These birds got lazy")
+            ai("1.0")
+            author(Forest)
+            ordinary()
+
+            text("""
+Amend Rule 2665 "The Birds" by:
+- removing the text
+  "The playmate then gains 1 Votive."
+  and
+  "If the playmate is Beast Permitted, e gains
+   1 Winsome, 1 Blot-B-Gone, 1 Pendant, and 1 Votive."
+- Replacing the text
+ "Otherwise,
+   the playmate gains 1 blot, 1 Pendant, and 1 Votive."
+  with
+  "If the playmate is not Beast Permitted,
+   e gains 1 blot."
+- Replacing the text
+  "A specified player that is not the playmate gains a
+        specified Product."
+  with
+  "Nothing happens."
+- Replacing the text:
+  "The playmate gains 2 blots and 1 Blot-B-Gone."
+  with
+  "The playmate gains 2 blots."
+- Replacing the text
+  " A specified player (defaulting to the playmate if not
+        specified) gains the Grant associated with a specified Ministry."
+  with
+  "Nothing happens."""")
+        }
+
+    }
+}
