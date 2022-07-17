993eabfa83683f5f88a035f8634ffa3fba5ee8d1
Jason Cobb <jason.e.cobb@gmail.com>
2022-07-17 01:40:08 -0400

    Add voting strengths

--- src/main/kotlin/org/agoranomic/assessor/decisions/8700-8716.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8700-8716.kt
@@ -10,6 +10,12 @@ fun assessment8700to8716() = assessment {
     name("8700-8716")
     quorum(5)
 
+    strengths {
+        default(3)
+        min(0)
+        max(15)
+    }
+
     proposals(v4) {
         proposal(8700) {
             title("Well, it's not doing anything.")
