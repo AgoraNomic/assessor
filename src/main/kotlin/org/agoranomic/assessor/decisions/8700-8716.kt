56344fa7bfe9e346e813675a1e6091866e6e0883
Jason Cobb <jason.e.cobb@gmail.com>
2022-07-17 02:06:52 -0400

    Add url

--- src/main/kotlin/org/agoranomic/assessor/decisions/8700-8716.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8700-8716.kt
@@ -4,12 +4,14 @@ import org.agoranomic.assessor.dsl.assessment
 import org.agoranomic.assessor.dsl.receivers.ai
 import org.agoranomic.assessor.dsl.receivers.coauthors
 import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.receivers.url
 import org.agoranomic.assessor.dsl.votes.resolvedConditional
 import org.agoranomic.assessor.lib.vote.VoteKind.*
 
 @UseAssessment
 fun assessment8700to8716() = assessment {
     name("8700-8716")
+    url("https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2022-July/016147.html")
     quorum(5)
 
     strengths {
