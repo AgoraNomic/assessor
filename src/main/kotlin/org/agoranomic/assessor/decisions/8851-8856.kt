138ecebfaadaf180f4bbfabbbd4484792555e2ce
Jason Cobb <jason.e.cobb@gmail.com>
2022-09-11 14:08:40 -0400

    Import distribution
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2022-September/016312.html

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8851-8856.kt
@@ -0,0 +1,162 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.coauthors
+import org.agoranomic.assessor.dsl.receivers.quorum
+
+@UseAssessment
+fun assessment8851to8856() = assessment {
+    name("8851-8856")
+    quorum(5)
+
+    proposals(v4) {
+        proposal(8851) {
+            title("Voter Protection")
+            ai("3.0")
+            author(Forest)
+            democratic()
+
+            text(
+                """
+Set 4st's score to half its current value, rounded down.
+If 4st doesn't already have the patent title "Bully",
+grant em the patent title "Bully".
+Destroy half of 4st's coins, rounded down.
+Decrease 4st's Karma by 1.
+
+Create the following rule, with Title "Voter Protection"
+and power=3.0
+{
+Rules to the contrary notwithstanding, proposals cannot affect
+players' assets or players' scores based on the way they have voted.
+Rules to the contrary notwithstanding, rules that affect players' assets or
+players' scores based on the way a player has voted cannot affect the
+proposal in which that rule is enacted.
+Rules to the contrary notwithstanding, proposals cannot grant patent
+titles to players based on the way they have voted.
+}"""
+            )
+        }
+
+        proposal(8852) {
+            title("Fix infractions")
+            ai("2.0")
+            author(Forest)
+            coauthors(Jason)
+            ordinary()
+
+            text(
+                """
+Amend rule 2555 "Blots" by replacing the following sentence:
+"Destroying blots is secured with a Power Threshold of 1.7."
+with
+"Creating and destroying blots is secured with a Power Threshold of 1.7""""
+            )
+        }
+
+        proposal(8853) {
+            title("Unfortunately")
+            ai("3.0")
+            author(Jason)
+            democratic()
+
+            text(
+                """
+Amend Rule 869 by appending the following paragraphs:
+{
+Banned is a secured negative boolean person switch tracked by the
+Registrar. A person is unwelcome if e is Banned or if at least one part
+of em is unwelcome. Rules to the contrary notwithstanding, an unwelcome
+person CANNOT register or be registered, and e is immediately
+deregistered if e is ever a player. Designations of unwelcomeness are
+secured.
+
+Unwelcomeness is to be used only to make a social statement that the
+Agoran community no longer wishes to interact with certain persons; in
+particular, it is not to be used to acquire gameplay advantage. As such,
+an unwelcome person SHOULD NOT, under any circumstances, attempt to
+bypass unwelcomeness, and e SHOULD carefully consider the circumstances
+of eir becoming unwelcome before participating in public or discussion fora.
+}"""
+            )
+        }
+
+        proposal(8854) {
+            title("Attainder")
+            ai("3.0")
+            author(Jason)
+            democratic()
+
+            text(
+                """
+For the purposes of this proposal, "Madrid" is the person who, at the
+time of this proposal's submission, was a player and who was known as
+"Madrid" to the average Agoran.
+
+Madrid is hereby deregistered.
+Madrid's instance of the Banned switch is hereby flipped to true.
+
+[
+This is a bill of attainder. Its purpose is to deregister and, if
+possible, ban Madrid as a sanction for a series of incidents on the
+lists and on the unofficial Discord server. This is not in response to
+any single incident, but rather a pattern of behavior. This is
+additionally not in response to Madrid's distaste for Spivak pronouns
+itself, regardless of the timing of this submission.
+
+Madrid's actions and views have resulted in two deregistrations (one
+successful FAGE and one attempted FAGE that wasn't a FAGE due to a
+technicality), hostile exchanges of words on list, and an atmosphere on
+the Discord that discourages participation in discussion. No other
+single player's non-gameplay actions and expressions of views have
+resulting in similar responses from other players.
+
+This proposal is phrased in such a way that, if adopted, Madrid will be
+unconditionally deregisted and will be Banned if the associated proposal
+"Unfortunately" has been adopted. Even if "Unfortunately" has not been
+adopted, the adoption of this proposal represents the AI-3 consensus
+that Madrid is not welcome. If this occurs, and "Unfortunately" failed
+due to a technical reason rather than apparent rejection of the idea,
+the author will pursue further proposals to officialize the ban.
+]"""
+            )
+        }
+
+        proposal(8855) {
+            title("Extermination v1.1")
+            ai("1.0")
+            author(Jason)
+            ordinary()
+
+            text(
+                """
+Repeal each of the following rules, in ascending numerical order by ID:
+
+* Rule 2660 (Birds)
+
+* Rule 2661 (Permits)
+
+* Rule 2662 (Playing with Birds)
+
+* Rule 2663 (Bird Migration)
+
+* Rule 2664 (One with Nature)
+
+* Rule 2665 (The Birds)"""
+            )
+        }
+
+        proposal(8856) {
+            title("Backup Justice")
+            ai("2.0")
+            author(Forest)
+            ordinary()
+
+            text(
+                """
+Grant Madrid 200 blots."""
+            )
+        }
+    }
+}
