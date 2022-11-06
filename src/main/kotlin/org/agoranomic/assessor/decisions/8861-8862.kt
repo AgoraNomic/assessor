f1a09c110faaf8e1ce508e9af385d823ca99b12c
Jason Cobb <jason.e.cobb@gmail.com>
2022-11-06 15:04:50 -0500

    Add url

--- src/main/kotlin/org/agoranomic/assessor/decisions/8861-8862.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8861-8862.kt
@@ -3,12 +3,14 @@ package org.agoranomic.assessor.decisions
 import org.agoranomic.assessor.dsl.assessment
 import org.agoranomic.assessor.dsl.receivers.ai
 import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.receivers.url
 import org.agoranomic.assessor.dsl.votes.resolvedConditional
 import org.agoranomic.assessor.lib.vote.VoteKind.*
 
 @UseAssessment
 fun assessment8861to8862() = assessment {
     name("8861-8862")
+    url("https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2022-November/016501.html")
     quorum(3)
 
     strengths {
