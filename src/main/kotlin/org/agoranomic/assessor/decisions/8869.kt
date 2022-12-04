d1d7dd788dcf860867359381f73409e1a134c7fa
Jason Cobb <jason.e.cobb@gmail.com>
2022-12-04 14:45:56 -0500

    Add voting strengths
    Latest Referee's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2022-October/016456.html
    
    Latest Dreamor's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2022-November/016500.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/8869.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8869.kt
@@ -3,11 +3,24 @@ package org.agoranomic.assessor.decisions
 import org.agoranomic.assessor.dsl.assessment
 import org.agoranomic.assessor.dsl.receivers.ai
 import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.votes.onOrdinaryProposals
+import org.agoranomic.assessor.dsl.votes.powerDream
 
 @UseAssessment
 fun assessment8869() = assessment {
     name("8869")
     quorum(3)
+
+    strengths {
+        default(3)
+        min(0)
+        max(15)
+
+        onOrdinaryProposals {
+            powerDream(Janet, 2)
+            powerDream(snail, 2)
+        }
+    }
 
     proposals(v4) {
         proposal(8869) {
