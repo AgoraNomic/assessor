1bd63c7781147092c28258dbd404632c405bf65c
Jason Cobb <jason.e.cobb@gmail.com>
2022-12-11 02:15:20 -0500

    Proposal 8869 (#119)
    * Import distribution
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2022-November/016549.html
    
    * Add voting strengths
    
    Latest Referee's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2022-October/016456.html
    
    Latest Dreamor's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2022-November/016500.html
    
    * Add voting block
    
    * Initial votes by juan
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2022-November/050158.html
    
    * Initial votes by Shy Owl
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2022-November/050159.html
    
    * Initial votes by Janet
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2022-November/050160.html
    
    * Initial votes by nix
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2022-November/050170.html
    
    * nix changes vote on 8869
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2022-November/050171.html
    
    * Initial votes by Murphy
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2022-November/050172.html
    
    * Initial votes by ais523
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2022-November/050175.html
    
    * Add url

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8869.kt
@@ -0,0 +1,68 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.receivers.url
+import org.agoranomic.assessor.dsl.votes.onOrdinaryProposals
+import org.agoranomic.assessor.dsl.votes.powerDream
+import org.agoranomic.assessor.lib.vote.VoteKind.AGAINST
+import org.agoranomic.assessor.lib.vote.VoteKind.FOR
+
+@UseAssessment
+fun assessment8869() = assessment {
+    name("8869")
+    url("https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2022-December/016584.html")
+    quorum(3)
+
+    strengths {
+        default(3)
+        min(0)
+        max(15)
+
+        onOrdinaryProposals {
+            powerDream(Janet, 2)
+            powerDream(snail, 2)
+        }
+    }
+
+    proposals(v4) {
+        proposal(8869) {
+            title("Stamp Shenanigans")
+            ai("1.0")
+            author(ShyOwl)
+            ordinary()
+
+            text(
+                """
+Create one Peter Suber Stamp in possession of the Lost and Found Department."""
+            )
+        }
+    }
+
+    voting {
+        votes(juan) {
+            FOR on 8869
+        }
+
+        votes(ShyOwl) {
+            FOR on 8869
+        }
+
+        votes(Janet) {
+            AGAINST on 8869
+        }
+
+        votes(nix) {
+            AGAINST on 8869
+        }
+
+        votes(Murphy) {
+            FOR on 8869
+        }
+
+        votes(ais523) {
+            AGAINST on 8869
+        }
+    }
+}
