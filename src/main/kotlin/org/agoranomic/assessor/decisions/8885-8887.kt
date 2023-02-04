ca7edd37f5030b874fd83d7430301ce0113a6086
Janet Cobb <jason.e.cobb@gmail.com>
2023-02-04 16:56:33 -0500

    Import distribution
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2023-January/016695.html

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8885-8887.kt
@@ -0,0 +1,97 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.coauthors
+import org.agoranomic.assessor.dsl.receivers.quorum
+
+@UseAssessment
+fun assessment8885to8887() = assessment {
+    name("8885-8887")
+    quorum(3)
+
+    proposals(v4) {
+        proposal(8885) {
+            title("Nerfed Mason's Stone buff")
+            ai("2.0")
+            author(Janet)
+            ordinary()
+
+            text(
+                """
+Amend Rule 2645 ("The Stones") by replacing the list item beginning
+"Mason's Stone" with the following:
+
+{
+
+- Mason's Stone (Monthly, 0): If the Mason's Stone is owned by the
+Stonemason, its Mossiness is continuously set to 0. If the Mason's Stone
+is owned by Agora, it is transferred to the Stonemason. When wielded, the
+mossiest stone owned by Agora (or, if there is a tie, a specified stone tied
+for the same) is transferred to the wielder.
+
+}
+
+[Currently, I'm effectively locked out of owning any actually useful
+stone without setting a Dream, which isn't really fair. If I attempt to
+get rid of the Mason's stone, it can be forcibly transferred back to me
+or will eventually be automatically transferred back to me, resetting
+the 30 day time limit again. I can't pawn it off on an inactive player,
+since only active players can hold stones, and if I transfer it to an
+active player I reset their stone delay, likely angering them and
+probably just getting it transferred back. This change would allow me to
+actually participate in the part of the stone game that allows doing
+things with stones.]"""
+            )
+        }
+
+        proposal(8886) {
+            title("Mason's Stone repeal")
+            ai("2.0")
+            author(Janet)
+            ordinary()
+
+            text(
+                """
+Amend Rule 2645 ("The Stones") by removing the list item beginning
+"Mason's Stone (None, 0)".
+
+[Only has effect if none of the buff proposals passed.]"""
+            )
+        }
+
+        proposal(8887) {
+            title("Schrodinger's Horse")
+            ai("1.0")
+            author(Murphy)
+            coauthors(ais523)
+            ordinary()
+
+            text(
+                """
+Amend Rule 2672 (Weekly Race Action) by replacing this text:
+
+       * get a jersey for a specified horse, also specifying a horse to
+         be added to that horse's pull, by paying 1 hoof.
+
+with this text:
+
+       * get a jersey for a specified horse by paying 1 hoof, also
+         specifying a horse which is thereby added to that horse's pull,
+         by paying 1 hoof.
+
+Amend Rule 2674 (Horse Powers) by replacing this text:
+
+       Nacho's power: (WEEKLY POWER)  * get a jersey for a specified
+       horse, also specifying a horse to be added to another specified
+       horse's pull, by paying 1 hoof.
+
+with this text:
+
+       Nacho's power: (WEEKLY POWER)  * get a jersey for a specified
+       horse, also specifying a horse which is thereby added to another
+       specified horse's pull, by paying 1 hoof."""
+            )
+        }
+    }
+}
