72a21e95e99081c5c77198f62f5dc7451325664d
Janet Cobb <jason.e.cobb@gmail.com>
2023-02-04 20:49:29 -0500

    Add voting strengths
    Latest ADoP's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2023-January/016725.html
    
    Latest Referee's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2023-February/016738.html
    
    Latest Dream Keeper's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2023-January/016719.html
    
    snail wields Power Stone: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2023-January/050323.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/8888.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8888.kt
@@ -3,11 +3,48 @@ package org.agoranomic.assessor.decisions
 import org.agoranomic.assessor.dsl.assessment
 import org.agoranomic.assessor.dsl.receivers.ai
 import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.votes.complexityBonuses
+import org.agoranomic.assessor.dsl.votes.onOrdinaryProposals
+import org.agoranomic.assessor.dsl.votes.powerStone
 
 @UseAssessment
 fun assessment8888() = assessment {
     name("8888")
     quorum(3)
+
+    strengths {
+        default(3)
+        min(0)
+        max(15)
+
+        onOrdinaryProposals {
+            powerStone(snail, 3)
+
+            complexityBonuses {
+                maxBonus(3)
+
+                "ADoP"(1) heldBy Murphy
+                "Arbitor"(2) heldBy G
+                "Assessor"(3) heldBy Janet
+                "Collector"(1) heldBy nix
+                "Distributor"(0) heldBy omd
+                "Dream Keeper"(1) heldBy snail
+                "Herald"(2) heldBy nix
+                "Horsened"(1) heldBy snail
+                "Mad Engineer"(1) heldBy Janet
+                "Notary"(2) heldBy snail
+                "Prime Minister"(0) heldBy Murphy
+                "Promotor"(3) heldBy snail
+                "Referee"(2) heldBy null
+                "Registrar"(1) heldBy juan
+                "Rulekeepor"(3) heldBy Janet
+                "Speaker"(0) heldBy snail
+                "Stonemason"(1) heldBy Janet
+                "Tailor"(1) heldBy Murphy
+                "Webmastor"(1) heldBy null
+            }
+        }
+    }
 
     proposals(v4) {
         proposal(8888) {
