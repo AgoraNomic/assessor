c29e04ad1634e8beec7ea5006636f6fdcda9411c
Janet Cobb <jason.e.cobb@gmail.com>
2023-04-09 00:55:08 -0400

    Import distribution
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2023-March/016916.html

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8937-8939.kt
@@ -0,0 +1,90 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.coauthors
+import org.agoranomic.assessor.dsl.receivers.quorum
+
+@UseAssessment
+fun assessment8937to8939() = assessment {
+    name("8937-8939")
+    quorum(5)
+
+    proposals(v4) {
+        proposal(8937) {
+            title("Reification")
+            ai("1.0")
+            author(Forest)
+            ordinary()
+
+            text(
+                """
+Enact the following rule:
+{
+A device is an asset defined by the rules.
+Devices are tracked by the Mad Engineer's assistant.
+}
+
+Grant each player a Reified Device."""
+            )
+        }
+
+        proposal(8938) {
+            title("Aspen's Idea")
+            ai("1.0")
+            author(Forest)
+            coauthors(Aspen)
+            ordinary()
+
+            text(
+                """
+>From Discord, Aspen hath dothly proposeth that
+I yonder pass such a proposal as thus:
+{
+Light the rules on fire and officer's must wear
+polka-dotted hats every third Tuesday.
+}
+
+Glitter: I don't gain 5 radiance when
+this proposal passes.
+Aspen gains an additional 4 radiance
+when this proposal passes."""
+            )
+        }
+
+        proposal(8939) {
+            title("Simpler Charity Distribution Calculation")
+            ai("2.0")
+            author(nix)
+            ordinary()
+
+            text(
+                """
+[The current calculations are needlessly complex. These calculations are
+simpler and hopefully still fair.]
+
+Amend R2675 by replacing:
+
+       - Charity: A Charity Stamp is a stamp of the type that the L&FD
+         owns the most of (if there is a tie, the first the L&FD owned,
+         and if still tied, the first alphabetically). Immediately after
+         a wandering, for each Charity Dreamer, in reverse order of when
+         eir Dream was last flipped (and in reverse order of registration
+         if tied), one Charity Stamp is transferred from the L&FD to em.
+         If the number of Stamps the L&FD owned during the last wandering
+         is greater than 10, this process happens a second time. If it is
+         more than 20, this process happens a third time.
+
+with:
+
+         - Charity: Immediately after a wandering, one stamp (chosen by
+         most-to-least owned by the L&FD at time of transfer, tie-broken
+         alphabetically) is transferred to each Charity Dreamer (in order
+         from least-to-most stamps owned, tie-broken alphabetically) from
+         the L&FD. If the number of Stamps the L&FD owned during the last
+         wandering is greater than 10, this process happens a second
+         time. If it is more than 20, this process happens a third time."""
+            )
+        }
+    }
+}
