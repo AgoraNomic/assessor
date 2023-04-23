d0d18cdc48663a677cad1fc070abff6d8f7a54af
Janet Cobb <jason.e.cobb@gmail.com>
2023-04-23 17:44:28 -0400

    Add url

--- src/main/kotlin/org/agoranomic/assessor/decisions/8946-8951.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8946-8951.kt
@@ -4,12 +4,14 @@ import org.agoranomic.assessor.dsl.assessment
 import org.agoranomic.assessor.dsl.receivers.ai
 import org.agoranomic.assessor.dsl.receivers.coauthors
 import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.receivers.url
 import org.agoranomic.assessor.dsl.votes.*
 import org.agoranomic.assessor.lib.vote.VoteKind.*
 
 @UseAssessment
 fun assessment8946to8951() = assessment {
     name("8946-8951")
+    url("https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2023-April/016981.html")
     quorum(3)
 
     strengths {
