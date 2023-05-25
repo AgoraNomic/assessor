3a247fac309288be9cdbcb622cb2fe8acd712ba0
Janet Cobb <jason.e.cobb@gmail.com>
2023-05-25 00:39:08 -0400

    Initial votes by snail
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2023-May/051331.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/8972-8979.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8972-8979.kt
@@ -7,6 +7,8 @@ import org.agoranomic.assessor.dsl.votes.complexityBonuses
 import org.agoranomic.assessor.dsl.votes.onOrdinaryProposals
 import org.agoranomic.assessor.dsl.votes.powerDream
 import org.agoranomic.assessor.dsl.votes.powerStone
+import org.agoranomic.assessor.lib.vote.VoteKind.AGAINST
+import org.agoranomic.assessor.lib.vote.VoteKind.FOR
 
 @UseAssessment
 fun assessment8972to8979() = assessment {
@@ -339,5 +341,15 @@ Otherwise, this Proposal does nothing."""
     }
 
     voting {
+        votes(snail) {
+            FOR on 8972
+            FOR on 8973
+            AGAINST on 8974
+            FOR on 8975
+            FOR on 8976
+            FOR on 8977
+            FOR on 8978
+            FOR on 8979
+        }
     }
 }
