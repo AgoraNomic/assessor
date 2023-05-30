f8c19ae7cce12862f519619cd5d1d3ae267b037a
Janet Cobb <jason.e.cobb@gmail.com>
2023-05-29 22:17:16 -0400

    Proposals 8960-8964 (#140)
    * Import distribution
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2023-April/016996.html
    
    * Add voting strengths
    
    Latest ADoP's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2023-May/017026.html
    
    Latest Referee's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2023-May/017037.html
    
    Latest Dream Keeper's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2023-May/017007.html
    
    Janet wields Power Stone: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2023-April/051203.html
    
    * Add voting block
    
    * Initial votes by snail
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2023-April/051169.html
    
    * Initial votes by 4st
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2023-April/051170.html
    
    * Initial votes by nix
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2023-April/051181.html
    
    * Initial votes by ais523
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2023-April/051188.html
    
    * Initial votes by Murphy
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2023-April/051204.html
    
    * Initial votes by Janet
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2023-April/051205.html
    
    * snail changes vote on 8960
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2023-April/051207.html
    
    * nix changes vote on 8961
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2023-May/051237.html
    
    * Initial votes by G.
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2023-May/051239.html
    
    * Initial votes by Yachay Wayllukuq
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2023-May/051240.html
    
    * G. changes vote on 8962
    
    Soource: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2023-May/051241.html
    
    * G. changes vote on 8962
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2023-May/051243.html
    
    * Yachay Wayllukuq changes votes
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2023-May/051253.html
    
    * Resolve conditional votes
    
    * Add url

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/8960-8964.kt
@@ -0,0 +1,215 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.receivers.url
+import org.agoranomic.assessor.dsl.votes.*
+import org.agoranomic.assessor.lib.vote.VoteKind.*
+
+@UseAssessment
+fun assessment8960to8964() = assessment {
+    name("8960-8964")
+    url("https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2023-May/017039.html")
+    quorum(5)
+
+    strengths {
+        default(3)
+        min(0)
+        max(15)
+
+        onOrdinaryProposals {
+            powerDream(G, 2)
+            powerDream(Aspen, 2)
+            powerDream(Forest, 2)
+
+            powerStone(Janet, 3)
+
+            complexityBonuses {
+                maxBonus(3)
+
+                "ADoP"(1) heldBy Murphy
+                "Arbitor"(2) heldBy G
+                "Assessor"(3) heldBy Janet
+                "Collector"(1) heldBy snail
+                "Distributor"(0) heldBy omd
+                "Dream Keeper"(1) heldBy snail
+                "Herald"(2) heldBy nix
+                "Notary"(2) heldBy snail
+                "Prime Minister"(0) heldBy nix
+                "Promotor"(3) heldBy snail
+                "Referee"(2) heldBy Forest
+                "Registrar"(1) heldBy juan
+                "Rulekeepor"(3) heldBy Janet
+                "Speaker"(0) heldBy snail
+                "Stonemason"(1) heldBy Janet
+                "Tailor"(1) heldBy Murphy
+                "Webmastor"(1) heldBy Forest
+            }
+        }
+    }
+
+    proposals(v4) {
+        proposal(8960) {
+            title("Ritual disambiguation")
+            ai("1.0")
+            author(ais523)
+            ordinary()
+
+            text(
+                """
+In rule 2680, amend
+{{{
+      When a positive integral multiple of 41 is anointed as a ritual
+      number for the first time, the person who does so CAN, by
+      announcement, Raise the First Speaker in a powerful dance around
+      the Town Fountain. Doing so causes each player qualifying under
+      this Rule to gain 1 radiance.
+}}}
+to
+{{{
+      For each positive integral multiple of 41, when that number is
+      anointed as a ritual number for the first time, the person who
+      does so CAN, by announcement, Raise the First Speaker in a
+      powerful dance around the Town Fountain. Doing so causes each
+      player qualifying under this Rule to gain 1 radiance.
+}}}"""
+            )
+        }
+
+        proposal(8961) {
+            title("now you don't see it")
+            ai("1.0")
+            author(G)
+            ordinary()
+
+            text(
+                """
+Re-enact Rule 2056 (Invisibilitating) with the following text:
+
+      Invisibilitating is a Class 1 infraction.
+
+
+[
+Rule 2056 history (confirmed by checking archives):
+
+Enacted (Power=1) by Proposal 4513 "Invisibilitating" (Steve), 10 July 2003.
+Repealed by Proposal 4759 "Olive Repeals" (Manu, Sherlock), 15 May 2005.
+]"""
+            )
+        }
+
+        proposal(8962) {
+            title("Please behave, Prime Minister")
+            ai("1.0")
+            author(Yachay)
+            ordinary()
+
+            text(
+                """
+Remove the last sentence of rule 2463."""
+            )
+        }
+
+        proposal(8963) {
+            title("hats")
+            ai("1.0")
+            author(Forest)
+            ordinary()
+
+            text(
+                """
+Enact a new rule with the text
+"hats are an untracked player switch that can have any value ever.""""
+            )
+        }
+
+        proposal(8964) {
+            title("Minty Stone")
+            ai("2.0")
+            author(snail)
+            ordinary()
+
+            text(
+                """
+Amend Rule 2645 (The Stones) by replacing
+
+{{
+      - Jockey Stone (monthly, 3): When wielded, a specified Running
+        horse's Race Position is increased by 1.
+}}
+with
+{{
+      - Minty Stone (weekly, 4): When wielded, a specified Player gains a
+stamp of eir own type.
+}}"""
+            )
+        }
+    }
+
+    voting {
+        votes(snail) {
+            FOR on 8960
+            AGAINST on 8961
+            AGAINST on 8962
+            AGAINST on 8963
+            FOR on 8964
+        }
+
+        votes(Forest) {
+            // NO VOTE on 8960
+            FOR on 8961
+            FOR on 8962
+            FOR on 8963
+            AGAINST on 8964
+        }
+
+        votes(nix) {
+            FOR on 8960
+            FOR on 8961
+            AGAINST on 8962
+            AGAINST on 8963
+            FOR on 8964
+        }
+
+        votes(ais523) {
+            FOR on 8960
+            AGAINST on 8961
+            AGAINST on 8962
+            AGAINST on 8963
+            resolvedConditional(FOR, "snail voted unconditionally FOR on P8960") on 8964
+        }
+
+        votes(Murphy) {
+            PRESENT on 8960
+            FOR on 8961
+            AGAINST on 8962
+            FOR on 8963
+            FOR on 8964
+        }
+
+        votes(Janet) {
+            FOR on 8960
+            endorse(G) on 8961
+            AGAINST on 8962
+            AGAINST on 8963
+            endorse(nix) on 8964
+        }
+
+        votes(G) {
+            resolvedConditional(
+                FOR,
+                "Yachay Wayllukuq has changed all of eir votes on this distribution to unconditional votes"
+            ) on 8962
+            FOR on others
+        }
+
+        votes(Yachay) {
+            FOR on 8960
+            AGAINST on 8961
+            FOR on 8962
+            FOR on 8963
+            FOR on 8964
+        }
+    }
+}
