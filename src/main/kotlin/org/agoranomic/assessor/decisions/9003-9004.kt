299ab8ce75506a847a374a557b5c4409062d7eb7
Janet Cobb <git@randomcat.org>
2023-07-23 14:38:04 -0400

    Initial votes by Janet
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2023-July/051805.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/9003-9004.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/9003-9004.kt
@@ -7,8 +7,7 @@ import org.agoranomic.assessor.dsl.receivers.quorum
 import org.agoranomic.assessor.dsl.votes.complexityBonuses
 import org.agoranomic.assessor.dsl.votes.onOrdinaryProposals
 import org.agoranomic.assessor.dsl.votes.powerDream
-import org.agoranomic.assessor.lib.vote.VoteKind.FOR
-import org.agoranomic.assessor.lib.vote.VoteKind.PRESENT
+import org.agoranomic.assessor.lib.vote.VoteKind.*
 
 @UseAssessment
 fun assessment9003to9004() = assessment {
@@ -165,6 +164,11 @@ to the end of Rule UNDEFINED."""
 
         votes(Forest) {
             FOR on 9004
+        }
+
+        votes(Janet) {
+            FOR on 9003
+            AGAINST on 9004
         }
     }
 }
