ab908a9664969ae195a9b33e0093be5f07b5fed6
Janet Cobb <git@randomcat.org>
2023-07-23 14:07:50 -0400

    Proposal 9002 (#147)
    * Import distribution
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2023-June/017185.html
    
    * Add voting strengths
    
    Latest ADoP's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2023-July/017205.html
    
    CoE on complexity of Absurdor: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2023-July/051795.html
    
    Latest Referee's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2023-June/017140.html
    
    Latest Dream Keeper's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2023-June/017186.html
    
    Janet envisions dream of Gardens: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2023-June/051746.html
    
    * Add voting block
    
    * Initial votes by snail
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2023-June/051734.html
    
    * Initial votes by Murphy
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2023-June/051743.html
    
    * Initial votes by Janet
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2023-June/051744.html
    
    * Initial votes by juan
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2023-June/051749.html
    
    * Initial votes by 4st
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2023-June/051752.html
    
    * Add url

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/9002.kt
@@ -0,0 +1,90 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.receivers.url
+import org.agoranomic.assessor.dsl.votes.complexityBonuses
+import org.agoranomic.assessor.dsl.votes.onOrdinaryProposals
+import org.agoranomic.assessor.dsl.votes.powerDream
+import org.agoranomic.assessor.lib.vote.VoteKind.*
+
+@UseAssessment
+fun assessment9002() = assessment {
+    name("9002")
+    url("https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2023-July/017212.html")
+    quorum(5)
+
+    strengths {
+        default(3)
+        min(0)
+        max(15)
+
+        onOrdinaryProposals {
+            powerDream(G, 2)
+            powerDream(Aspen, 2)
+            powerDream(Forest, 2)
+
+            complexityBonuses {
+                maxBonus(3)
+
+                "Absurdor"(0) heldBy juan
+                "ADoP"(1) heldBy Murphy
+                "Arbitor"(2) heldBy G
+                "Assessor"(3) heldBy Janet
+                "Buttonmastor"(3) heldBy juan
+                "Collector"(1) heldBy snail
+                "Distributor"(0) heldBy omd
+                "Dream Keeper"(1) heldBy snail
+                "Herald"(2) heldBy Forest
+                "Notary"(2) heldBy snail
+                "Prime Minister"(0) heldBy Forest
+                "Promotor"(3) heldBy snail
+                "Referee"(2) heldBy null
+                "Registrar"(1) heldBy juan
+                "Rulekeepor"(3) heldBy Janet
+                "Speaker"(0) heldBy G
+                "Stonemason"(1) heldBy Janet
+                "Tailor"(1) heldBy Murphy
+                "Webmastor"(1) heldBy null
+            }
+        }
+    }
+
+    proposals(v4) {
+        proposal(9002) {
+            title("Active rocks")
+            ai("2.0")
+            author(Janet)
+            ordinary()
+
+            text(
+                """
+Amend Rule 2642 ("Gathering Stones") by replacing "Rockiness is a player
+switch" with "Rockiness is an active player switch"."""
+            )
+        }
+    }
+
+    voting {
+        votes(snail) {
+            FOR on 9002
+        }
+
+        votes(Murphy) {
+            FOR on 9002
+        }
+
+        votes(Janet) {
+            FOR on 9002
+        }
+
+        votes(juan) {
+            PRESENT on 9002
+        }
+
+        votes(Forest) {
+            AGAINST on 9002
+        }
+    }
+}
