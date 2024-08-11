3a9416a658475e2ec28f430ab4716af7f643e06d
Janet Cobb <git@randomcat.org>
2024-08-11 15:49:14 -0400

    Import distribution
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2024-July/017922.html

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/9148-9152.kt
@@ -0,0 +1,149 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.quorum
+
+@UseAssessment
+fun assessment9148to9152() = assessment {
+    name("9148-9152")
+    quorum(3)
+
+    proposals(v4) {
+        proposal(9148) {
+            title("Corpse Looting")
+            ai("1.0")
+            author(Forest)
+            ordinary()
+
+            text(
+                """
+Append the following to Rule 2695 ("The Veblen"):
+{
+When a shooter fires a round that eliminates a player who owns the Veblen,
+the shooter CAN, by announcement, loot the corpse. The Veblen is thereby
+transferred to the shooter.
+}"""
+            )
+        }
+
+        proposal(9149) {
+            title("Silly Bangs")
+            ai("1.0")
+            author(Forest)
+            ordinary()
+
+            text(
+                """
+Amend Rule 2696 ("Bang Actions") by replacing:
+{
+if the shooter is alive and has two bangs
+}
+with
+{
+if the shooter is alive and has at least two bangs
+}
+
+[it's not really exploitable, but rather just annoying: having your reveal
+fail bc you had more than 2 bangs for any reason, since you can simply
+transfer bangs away from you freely.]"""
+            )
+        }
+
+        proposal(9150) {
+            title("*sigh*")
+            ai("1.0")
+            author(Janet)
+            ordinary()
+
+            text(
+                """
+Amend the Rule entitled "The Veblen" to read, in whole:
+
+{
+
+The Veblen is a unique indestructible fixed asset tracked by the
+Abusrdor, with ownership entirely restricted to players and Agora. If
+the Veblen would otherwise be in abeyance, or if it is owned by the Lost
+and Found Department, it is immediately transferred to Agora.
+
+The Veblen cost is a secured singleton positive integer switch tracked
+by the Absurdor and with default value one.
+
+A player who does not own the Veblen CAN pay a fee of X spendies to
+purchase the Veblen, where X is an integer not less than the current
+Veblen cost. When e does so, the Veblen is transferred to em, then the
+Veblen cost is set to X+1.
+
+The owner of the Veblen SHOULD conspicuously show off eir ownership of
+it from time to time.
+
+}
+
+
+The Veblen is hereby transferred to the entity that owned the Veblen
+immediately before this proposal began taking effect (if e does not
+already own it).
+
+The Veblen cost is hereby flipped to what the Veblen Cost was
+immediately before this proposal began taking effect (if it does not
+already have that value).
+
+[Normalize tracking language. Normalize transfer language. Fix
+"ownership of the Veblen", if such a thing exists. Prohibit
+self-purchases.]"""
+            )
+        }
+
+        proposal(9151) {
+            title("Baa")
+            ai("2.0")
+            author(Mischief)
+            ordinary()
+
+            text(
+                """
+[Creates a stone that follows other stones around]
+
+Amend rule 2645 (The Stones) by adding, at the end, to the list of stones:
+
+       - Sheep Stone (monthly): This stone has no effect. Whenever
+         the Sheep Stone is not immune and another stone is transferred
+         from owner X to owner Y, where X is the current owner of the
+         Sheep Stone, then the Sheep Stone is immediately transferred
+         to owner Y as well. When this occurs, the stone cost of the
+         Sheep Stone is set to its default value."""
+            )
+        }
+
+        proposal(9152) {
+            title("Ugh")
+            ai("2.0")
+            author(Mischief)
+            ordinary()
+
+            text(
+                """
+[Creates a stone that's effectively worth -1 in the count.]
+
+Amend rule 2645 (The Stones) by adding, at the end, to the list of stones:
+
+       - Tacky Stone (monthly): Wielding the Tacky Stone reminds
+         everyone how much of an eyesore it is. If the Tacky Stone is
+         not immune, any player CAN pay a fee of X Spendies to transfer
+         the Tacky Stone to a specified entity that the rules permit to
+         own stones, where X is the current Stone Cost of the Tacky
+         Stone. When this occurs, the stone cost of the Tacky Stone is
+         set to its default value.
+
+Amend rule 2644 (Zen Gardening) by replacing "that owns 8 or more
+stones" with "with an impressive rock collection" and inserting as the
+first paragraph of the rule the following text (including an embedded list):
+
+       A player has an impressive rock collection if e:
+         1) owns 10 or more stones; or
+         2) owns 8 or more stones and does not own the Tacky Stone."""
+            )
+        }
+    }
+}
