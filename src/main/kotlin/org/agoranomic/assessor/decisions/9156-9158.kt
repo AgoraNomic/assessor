afda271ba6d190fd5f8429dccacc1b3167b93a97
Janet Cobb <git@randomcat.org>
2024-08-24 20:26:10 -0400

    Add voting strengths
    Latest Referee's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2024-August/017973.html
    
    Latest ADoP's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2024-August/017976.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/9156-9158.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/9156-9158.kt
@@ -4,11 +4,49 @@ import org.agoranomic.assessor.dsl.assessment
 import org.agoranomic.assessor.dsl.receivers.ai
 import org.agoranomic.assessor.dsl.receivers.coauthors
 import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.votes.complexityBonuses
+import org.agoranomic.assessor.dsl.votes.onOrdinaryProposals
 
 @UseAssessment
 fun assessment9156to9158() = assessment {
     name("9156-9158")
     quorum(3)
+
+    strengths {
+        default(3)
+        min(0)
+        max(15)
+
+        onOrdinaryProposals {
+            complexityBonuses {
+                maxBonus(3)
+
+                "Absurdor"(1) heldBy juan
+                "ADoP"(1) heldBy Murphy
+                "Arbitor"(2) heldBy Kate
+                "Archivist"(1) heldBy Gaelan
+                "Assessor"(3) heldBy Janet
+                "Collector"(2) heldBy Mischief
+                "Distributor"(0) heldBy omd
+                "Geologist"(1) heldBy Forest
+                "Herald"(2) heldBy Forest
+                "Illuminator"(1) heldBy Quadrantal
+                "Notary"(2) heldBy Forest
+                "Prime Minister"(0) heldBy null
+                "Promotor"(3) heldBy snail
+                "Referee"(2) heldBy ais523
+                "Registrar"(1) heldBy juan
+                "Rulekeepor"(3) heldBy Janet
+                "Simplifior"(1) heldBy juniper
+                "Speaker"(0) heldBy ais523
+                "Spendor"(1) heldBy nix
+                "Stonemason"(1) heldBy juniper
+                "Tailor"(1) heldBy Murphy
+                "Tracker of Hats"(1) heldBy Mischief
+                "Webmastor"(1) heldBy Quadrantal
+            }
+        }
+    }
 
     proposals(v4) {
         proposal(9156) {
