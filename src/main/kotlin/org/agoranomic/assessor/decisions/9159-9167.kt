8547d835b325cd3d9f8098b3b78f1a7a3d6ea1b2
Janet Cobb <git@randomcat.org>
2024-09-01 02:29:56 -0400

    Initial votes by Mischief
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2024-August/053562.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/9159-9167.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/9159-9167.kt
@@ -372,5 +372,17 @@ elimination when e eliminates a target with a non-zero Bounty Amount.
             PRESENT on 9166
             PRESENT on 9167
         }
+
+        votes(Mischief) {
+            FOR on 9159
+            PRESENT on 9160
+            FOR on 9161
+            FOR on 9162
+            FOR on 9163
+            FOR on 9164
+            FOR on 9165
+            AGAINST on 9166
+            PRESENT on 9167
+        }
     }
 }
