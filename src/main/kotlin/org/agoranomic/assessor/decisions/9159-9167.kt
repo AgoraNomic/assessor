aaf9e8c87429cd50fc6288b755318b7b1d18fa13
Janet Cobb <git@randomcat.org>
2024-09-01 01:55:01 -0400

    Import distribution
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2024-August/017978.html

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/9159-9167.kt
@@ -0,0 +1,218 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.quorum
+
+@UseAssessment
+fun assessment9159to9167() = assessment {
+    name("9159-9167")
+    quorum(6)
+
+    proposals(v4) {
+        proposal(9159) {
+            title("Simplification, ironically")
+            ai("1.0")
+            author(Janet)
+            ordinary()
+
+            text(
+                """
+Amend Rule 1681 ("The Logical Rulesets") by deleting the text from "The
+Readable Logical Ruleset (RLR)" (inclusive) to the end of the rule.
+
+Repeal Rule 2693 ("The Simplifior").
+
+
+[We have yet to see a report from this office and nobody has pointed
+this out or expressed interest in deputising, suggesting that the report
+is infeasible and/or not useful.]"""
+            )
+        }
+
+        proposal(9160) {
+            title("A reasonable limit")
+            ai("1.0")
+            author(snail)
+            ordinary()
+
+            text(
+                """
+Enact a new rule with title "Proposal Limits" and the following text:
+
+{
+
+A person SHALL NOT submit a proposal while e has already submitted five or
+more proposals currently in the proposal pool that week; doing so is the
+Class N Infraction of Preposterous Propositioning, where N is the number of
+times e has committed the infraction this week.
+
+}
+
+[Deterrence for excess proposals, which is currently easily abusable.]"""
+            )
+        }
+
+        proposal(9161) {
+            title("Fix office reports")
+            ai("1.0")
+            author(ais523)
+            ordinary()
+
+            text(
+                """
+In rule 2143, replace:
+{{{
+For each person:
+
+1. If any task is defined by the rules as part of that person's
+   weekly duties, then e SHALL perform it at least once each week.
+   If any information is defined by the rules as part of that
+   person's weekly report, then e SHALL maintain all such
+   information, and the publication of all such information is
+   part of eir weekly duties.
+
+2. If any task is defined by the rules as part of that person's
+   monthly duties, then e SHALL perform it at least once each
+   month. If any information is defined by the rules as part of
+   that person's monthly report, then e SHALL maintain all such
+   information, and the publication of all such information is
+   part of eir monthly duties.
+}}}
+with
+{{{
+For each person:
+
+1. If any task is defined by the rules as part of that person's
+   weekly duties, then e SHALL perform it at least once each week.
+
+2. If any task is defined by the rules as part of that person's
+   monthly duties, then e SHALL perform it at least once each month.
+
+For each office, if any information is defined by the rules as part of
+that office's weekly or monthly report, then its officeholder SHALL
+maintain all such information, and eir weekly or monthly duties
+respectively include publishing a document or series of documents,
+labelled as that office's report (or as that office's weekly or monthly
+report as appropriate), that includes all such information.
+}}}
+
+[The intent behind this proposal is to make it possible to
+unambiguously identify whether an office has missed a report, by
+requiring the report to be labelled as one, and thus makes it possible
+to deputise based on a missed report.]"""
+            )
+        }
+
+        proposal(9162) {
+            title("Shameless Bribery 2A")
+            ai("1.0")
+            author(Mischief)
+            ordinary()
+
+            text(
+                """
+All players who cast valid unconditional ballots FOR the referendum
+adopting this proposal earn a Black Ribbon"""
+            )
+        }
+
+        proposal(9163) {
+            title("Shameless Bribery 2B")
+            ai("1.0")
+            author(Mischief)
+            ordinary()
+
+            text(
+                """
+All players who cast valid unconditional ballots FOR the referendum
+adopting this proposal earn a Black Ribbon"""
+            )
+        }
+
+        proposal(9164) {
+            title("Shameless Bribery 2C")
+            ai("1.0")
+            author(Mischief)
+            ordinary()
+
+            text(
+                """
+All players who cast valid unconditional ballots FOR the referendum
+adopting this proposal earn a Black Ribbon"""
+            )
+        }
+
+        proposal(9165) {
+            title("Remove the Sabotage Stone")
+            ai("1.0")
+            author(Mischief)
+            ordinary()
+
+            text(
+                """
+[It has an obvious -- but annoying to everybody -- workaround.]
+
+The owner of the Sabotage Stone gains a number of Spendies equal to its
+current Stone Cost.
+
+Amend rule 2645 (The Stones) by deleting:
+
+       - Sabotage Stone (weekly): When wielded, the adoption index of a
+         specified AI-majority Agoran decision is increased by 1."""
+            )
+        }
+
+        proposal(9166) {
+            title("Untracked hats")
+            ai("1.0")
+            author(Janet)
+            ordinary()
+
+            text(
+                """
+Amend Rule 2594 ("Hats") by replacing "Hats are a secured player switch"
+with "Hats are a secured untracked player switch" and by replacing the
+paragraph beginning "A player CAN change eir hat" with the following
+paragraph:
+
+{
+
+A player CAN change eir hat to a valid value by public designation.
+
+}
+
+[Don't require reporting on hats in any event, and don't require
+explicit "notification" to any person (in any event, it's unclear what
+it means to "notify" oneself).]"""
+            )
+        }
+
+        proposal(9167) {
+            title("The Bounty Board")
+            ai("1.0")
+            author(snail)
+            ordinary()
+
+            text(
+                """
+Enact a new Rule with title "Bounties" and the following text:
+{
+Bounty Amount is an untracked player switch. Its possible values are all
+integers from 0 to 10 inclusive, where 0 is the default.
+
+Twice each month, each player CAN, by announcement, put a bounty on a
+specified player. Doing so increases the specified player's Bounty Amount
+by 1.
+
+Immediately after an alive player is eliminated, the shooter gains a number
+of spendies equal to the target's Bounty Amount, and then the target's
+bounty amount is flipped to 0.
+
+The shooter SHOULD publish the target's Bounty Amount at the time of the
+elimination when e eliminates a target with a non-zero Bounty Amount.
+}"""
+            )
+        }
+    }
+}
