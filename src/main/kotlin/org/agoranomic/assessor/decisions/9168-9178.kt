921625d8bd38e7dd6661507cd15f125cfff12fd1
Janet Cobb <git@randomcat.org>
2024-09-15 15:51:38 -0400

    Initial votes by Janet
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2024-September/053659.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/9168-9178.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/9168-9178.kt
@@ -473,5 +473,19 @@ bang." with "after which each alive player is granted 2 bangs."
             PRESENT on 9177
             PRESENT on 9178
         }
+
+        votes(Janet) {
+            FOR on 9168
+            AGAINST on 9169
+            AGAINST on 9170
+            FOR on 9171
+            FOR on 9172
+            // NO VOTE on 9173
+            FOR on 9174
+            AGAINST on 9175
+            FOR on 9176
+            FOR on 9177
+            PRESENT on 9178
+        }
     }
 }
