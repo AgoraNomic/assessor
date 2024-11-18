21ef46102f1d2b4408055e3b4621c7652c95f476
Janet Cobb <git@randomcat.org>
2024-11-18 18:31:46 -0500

    Add voting strengths
    Latest Referee's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2024-November/018138.html
    
    Latest ADoP's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2024-November/018139.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/9193-9194.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/9193-9194.kt
@@ -3,11 +3,46 @@ package org.agoranomic.assessor.decisions
 import org.agoranomic.assessor.dsl.assessment
 import org.agoranomic.assessor.dsl.receivers.ai
 import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.votes.complexityBonuses
+import org.agoranomic.assessor.dsl.votes.onOrdinaryProposals
 
 @UseAssessment
 fun assessment9193to9194() = assessment {
     name("9193-9194")
     quorum(3)
+
+    strengths {
+        default(3)
+        min(0)
+        max(15)
+
+        onOrdinaryProposals {
+            complexityBonuses {
+                maxBonus(3)
+
+                "Absurdor"(1) heldBy juan
+                "ADoP"(2) heldBy Murphy
+                "Arbitor"(2) heldBy Kate
+                "Archivist"(1) heldBy Gaelan
+                "Assessor"(3) heldBy Janet
+                "Collector"(2) heldBy Mischief
+                "Distributor"(0) heldBy omd
+                "Geologist"(1) heldBy Forest
+                "Herald"(2) heldBy Forest
+                "Illuminator"(1) heldBy Mischief
+                "Notary"(2) heldBy Forest
+                "Prime Minister"(0) heldBy Mischief
+                "Promotor"(3) heldBy snail
+                "Referee"(2) heldBy ais523
+                "Registrar"(1) heldBy juan
+                "Rulekeepor"(3) heldBy Janet
+                "Speaker"(0) heldBy kiako
+                "Spendor"(1) heldBy oliver
+                "Tailor"(1) heldBy Murphy
+                "Webmastor"(1) heldBy Quadrantal
+            }
+        }
+    }
 
     proposals(v4) {
         proposal(9193) {
