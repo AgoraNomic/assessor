8068a3fce0e3cf87073b2e4dd91557777758b80c
Janet Cobb <git@randomcat.org>
2024-12-16 17:28:31 -0500

    Initial votes by Kate
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2024-November/053908.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/9195-9196.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/9195-9196.kt
@@ -124,5 +124,10 @@ containing "Spendies are tracked by the Spendor":
             FOR on 9195
             // NO VOTE on 9196
         }
+
+        votes(Kate) {
+            FOR on 9195
+            FOR on 9196
+        }
     }
 }
