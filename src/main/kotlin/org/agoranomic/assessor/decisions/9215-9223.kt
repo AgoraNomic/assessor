f07353ea7af59ad9655857d94821e6aeadc192bc
Janet Cobb <git@randomcat.org>
2025-05-02 22:23:30 -0400

    Add url

--- src/main/kotlin/org/agoranomic/assessor/decisions/9215-9223.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/9215-9223.kt
@@ -4,6 +4,7 @@ import org.agoranomic.assessor.dsl.assessment
 import org.agoranomic.assessor.dsl.receivers.ai
 import org.agoranomic.assessor.dsl.receivers.coauthors
 import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.receivers.url
 import org.agoranomic.assessor.dsl.votes.complexityBonuses
 import org.agoranomic.assessor.dsl.votes.endorse
 import org.agoranomic.assessor.dsl.votes.onOrdinaryProposals
@@ -13,6 +14,7 @@ import org.agoranomic.assessor.lib.vote.VoteKind.*
 @UseAssessment
 fun assessment9215to9223() = assessment {
     name("9215-9223")
+    url("https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2025-March/018346.html")
     quorum(5)
 
     strengths {
