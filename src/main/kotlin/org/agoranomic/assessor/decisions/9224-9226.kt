c972b46fdcb2e73eda0757df19f52c763f79efc1
Janet Cobb <git@randomcat.org>
2025-05-02 22:31:59 -0400

    Initial votes by kambri
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2025-April/054276.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/9224-9226.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/9224-9226.kt
@@ -192,5 +192,11 @@ to be a regulation of that same tournament for the purposes of Rule 2464.
             AGAINST on 9225
             PRESENT on 9226
         }
+
+        votes(kambri) {
+            FOR on 9224
+            PRESENT on 9225
+            FOR on 9226
+        }
     }
 }
