aa38c72b980e6721a458fbb8ee9378a40dcfa6b8
Janet Cobb <git@randomcat.org>
2025-06-22 13:21:27 -0400

    Initial votes by Mischief
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2025-May/054296.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/9227-9229.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/9227-9229.kt
@@ -114,5 +114,11 @@ with this text:
             FOR on 9228
             PRESENT on 9229
         }
+
+        votes(Mischief) {
+            FOR on 9227
+            PRESENT on 9228
+            PRESENT on 9229
+        }
     }
 }
