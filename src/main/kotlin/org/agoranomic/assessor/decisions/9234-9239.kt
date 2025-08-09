eb2b8a27c55b8a4d46691e55e99d0529593521c2
Janet Cobb <git@randomcat.org>
2025-08-09 01:15:37 -0400

    Import distribution
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2025-July/018580.html

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/9234-9239.kt
@@ -0,0 +1,185 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.coauthors
+import org.agoranomic.assessor.dsl.receivers.quorum
+
+@UseAssessment
+fun assessment9234to9239() = assessment {
+    name("9234-9239")
+    quorum(6)
+
+    proposals(v4) {
+        proposal(9234) {
+            title("Prix Fixe")
+            ai("1")
+            author(Mischief)
+            ordinary()
+
+            text(
+                """
+[The variable cost nature of this fee means that from time to time, one
+failed purchase causes subsequent purchases to fail. To simplify things,
+this switches it to a flat fee since a player's Spendie holdings already
+limit how many times e can do this. It is calibrated so that the three
+purchases per month afforded by 20 Spendies have the same cost as today.]
+Amend rule 2659 (Stamps) by replacing:
+
+       Any player CAN pay a fee of 5 + (X) Spendies to transfer a
+       specified stamp from the L&FD to emself. X is equal to the number
+       of times e has already done so in the current month.
+
+with:
+
+       Any player CAN pay a fee of 6 Spendies to transfer a specified
+       stamp from the L&FD to emself."""
+            )
+        }
+
+        proposal(9235) {
+            title("Agoricultural Enabling Act")
+            ai("1")
+            author(Murphy)
+            ordinary()
+
+            text(
+                """
+Amend the rule titled "The Land Managor" by replacing "Land Manager"
+with "Land Managor".Amend the the rule titled "Genetic Modification" by
+replacing the first
+paragraph with:
+
+       Once per Crop Season, a player CAN pay a fee of 500 Pyrite to
+       create a new type of Crop, specifying its name. The Land Managor
+       CAN once by announcement, and SHALL in a timely fashion, randomly
+       select its Crop Season, Seed Cost (50 to 200), Growth Duration (3
+       to 24), and Sell Price (100 to 400). Rules to the contrary
+       notwithstanding, it CANNOT be planted before this announcement."""
+            )
+        }
+
+        proposal(9236) {
+            title("Landlocked")
+            ai("1")
+            author(Murphy)
+            ordinary()
+
+            text(
+                """
+Create a rule titled "Hectares" with this text:
+
+       Hectare is a type of fixed asset, tracked by the Land Managor.
+
+       A player CAN pay a fee of 500 Pyrite to gain 1 Hectare.
+
+Amend Rule 2499 (Welcome Packages) by adding this item to the list:
+
+       * 16 Hectares.
+
+Each player gains 16 Hectares.
+
+Amend the rule titled "The Growth Cycle" by replacing the first
+paragraph with:
+
+       A player CAN plant an in-season Crop by paying a fee in Pyrite
+       equal to its Seed Cost, provided that eir number of Hectares is
+       greater than the number of Crops e planted earlier in the current
+       Crop Season and has not yet harvested."""
+            )
+        }
+
+        proposal(9237) {
+            title("Pyrite Fungibility")
+            ai("1")
+            author(oliver)
+            coauthors(Mischief)
+            ordinary()
+
+            text(
+                """
+If Proposal 9230 (Agoriculture) has not been adopted, this proposal has
+no effect.
+
+Amend the rule titled "Pyrite" by replacing the text:
+
+     Pyrite is a type of fixed asset
+
+with:
+
+     Pyrite is a type of fixed fungible asset
+
+[Comment: This is presumably the intended use of Pyrite as a currency.
+Also, it saves the Land Managor the work of tracking each Pyrite
+individually, especially since there can be thousands of them.]"""
+            )
+        }
+
+        proposal(9238) {
+            title("Make currency current again")
+            ai("3")
+            author(Murphy)
+            coauthors(Janet, Mischief)
+            democratic()
+
+            text(
+                """
+Amend Rule 2578 (Fungibility) by replacing this text:
+
+       A fungible asset is one where
+
+with this text:
+
+       A fungible asset (syn. "currency") is an asset where
+
+Amend Rule 2555 (Blots) by replacing "fungible asset" with "currency".
+
+Amend Rule 2579 (Fee-based Actions) by replacing "fungible asset" with
+"currency".
+
+Amend Rule 2690 (Spendies) by replacing "fungible liquid asset" with
+"currency".
+
+[Liquid is already the default, per Rule 2577.]
+
+Amend Rule 2700 (Candles) by replacing "Candles are fixed fungible
+assets" with "Candles are a fixed currency".
+
+Amend the rule titled "Pyrite" by replacing the first paragraph with:
+
+       Pyrite is a type of fixed currency, tracked by the Land Managor.
+
+Amend the rule titled "Hectares" by replacing "fixed asset" with
+"fixed currency".
+
+[Non-currency assets: Promises, only fungible if creator and text are
+  also identical; Stamps, only fungible if type is also identical; the
+  Veblen.]"""
+            )
+        }
+
+        proposal(9239) {
+            title("Gray timing")
+            ai("3")
+            author(Murphy)
+            democratic()
+
+            text(
+                """
+Amend Rule 2438 (Ribbons) by replacing this text:      Gray (A): Once
+per month the Tailor CAN, by announcement, cause a
+       specified player to earn a Gray Ribbon. E is ENCOURAGED to do so
+       in the same message in which e publishes eir monthly report.
+
+with this text:
+
+       Gray (A): Once per month the Tailor CAN, by announcement, cause a
+       specified player to earn a Gray Ribbon. E is ENCOURAGED to do so
+       before publishing eir monthly report.
+
+["In the same message" doesn't play well with my recordkeeping software,
+  and I don't think anyone has mentioned this clause any time recently.]"""
+            )
+        }
+    }
+}
