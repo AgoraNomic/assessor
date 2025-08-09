82ab7352c74343ce266eded9628e20bbdd69df47
Janet Cobb <git@randomcat.org>
2025-08-09 01:18:56 -0400

    Initial votes by Cosmo
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2025-July/054492.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/9234-9239.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/9234-9239.kt
@@ -229,5 +229,14 @@ with this text:
             FOR on 9238
             endorse(Murphy) on 9239 comment "${Murphy.name} is the Tailor"
         }
+
+        votes(Cosmo) {
+            FOR on 9234
+            // TODO: resolve conditional vote on 9235: FOR if 9230 resolved as ADOPTED, else AGAINST.
+            // TODO: resolve conditional vote on 9236: FOR if 9230 resolved as ADOPTED, else AGAINST.
+            // TODO: resolve conditional vote on 9237: FOR if 9230 resolved as ADOPTED, else AGAINST.
+            FOR on 9238
+            endorse(Murphy) on 9239 comment "${Murphy.name} is the Tailor"
+        }
     }
 }
