0b975087354821297faa70a758844ed046acc027
Janet Cobb <git@randomcat.org>
2025-08-09 01:06:27 -0400

    Proposal 9233 (#195)
    * Import distribution
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2025-July/018575.html
    
    * Add voting strengths
    
    Latest ADoP's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2025-July/018588.html
    
    Latest Referee's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2025-July/018590.html
    
    * Add voting block
    
    * Initial votes by juan
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2025-July/054462.html
    
    * Initial votes by Trigon
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2025-July/054463.html
    
    * Initial votes by Kate
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2025-July/054464.html
    
    * Initial votes by Mischief
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2025-July/054482.html
    
    * Initial votes by Janet
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2025-July/054483.html
    
    * Initial votes by Murphy
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2025-July/054487.html
    
    * Initial votes by ais523
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2025-July/054489.html
    
    * Initial votes by Cosmo
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2025-July/054490.html
    
    * Add url

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/9233.kt
@@ -0,0 +1,128 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.receivers.url
+import org.agoranomic.assessor.dsl.votes.complexityBonuses
+import org.agoranomic.assessor.dsl.votes.endorse
+import org.agoranomic.assessor.dsl.votes.onOrdinaryProposals
+import org.agoranomic.assessor.lib.vote.VoteKind.AGAINST
+import org.agoranomic.assessor.lib.vote.VoteKind.FOR
+
+@UseAssessment
+fun assessment9233() = assessment {
+    name("9233")
+    url("https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2025-July/018591.html")
+    quorum(6)
+
+    strengths {
+        default(3)
+        min(0)
+        max(15)
+
+        onOrdinaryProposals {
+            complexityBonuses {
+                maxBonus(3)
+
+                "Absurdor"(1) heldBy juan
+                "ADoP"(2) heldBy Murphy
+                "Arbitor"(2) heldBy Kate
+                "Archivist"(1) heldBy kiako
+                "Assessor"(3) heldBy Janet
+                "Collector"(2) heldBy Mischief
+                "Distributor"(0) heldBy omd
+                "Herald"(2) heldBy snail
+                "Illuminator"(1) heldBy Mischief
+                "Notary"(2) heldBy Mischief
+                "Prime Minister"(0) heldBy Mischief
+                "Promotor"(3) heldBy Cosmo
+                "Referee"(2) heldBy ais523
+                "Registrar"(1) heldBy juan
+                "Rulekeepor"(3) heldBy Janet
+                "Speaker"(0) heldBy Kate
+                "Spendor"(1) heldBy Murphy
+                "Tailor"(1) heldBy Murphy
+                "Webmastor"(1) heldBy kiako
+            }
+        }
+    }
+
+    proposals(v4) {
+        proposal(9233) {
+            title("Significant but not essential")
+            ai("3.0")
+            author(Murphy)
+            democratic()
+
+            text(
+                """
+
+Amend Rule 107 (Initiating Agoran Decisions) by appending this text
+after the last list item:
+
+       The person publishing the notice SHALL also include any additional
+       information defined by the rules as significant parameters.
+
+Amend Rule 1607 (Distribution) by replacing this text:
+
+       A referendum is the Agoran decision to determine whether to adopt
+       a proposal (its associated proposal). For this decision, the vote
+       collector is the Assessor, the adoption index is initially the
+       adoption index of the proposal, and the text, author, coauthors,
+       and class of the proposal are essential parameters. Initiating a
+       referendum is known as distribution, and removes the proposal from
+       the Proposal Pool.
+
+with this text:
+
+       A referendum is the Agoran decision to determine whether to adopt
+       a proposal (its associated proposal). For this decision, the vote
+       collector is the Assessor, the adoption index is initially the
+       adoption index of the proposal, the text of the proposal is an
+       essential parameter, and the author, coauthors, and class of the
+       proposal are significant parameters. Initiating a referendum is
+       known as distribution, and removes the proposal from the Proposal
+       Pool.
+
+[Downgrades e.g. forgetting to include class from "invalidates the
+  distribution until/unless ratification papers over it" to "Assessor
+  may get some Blots".]"""
+            )
+        }
+    }
+
+    voting {
+        votes(juan) {
+            endorse(Janet) on 9233 comment "${Janet.name} is the Assessor"
+        }
+
+        votes(Trigon) {
+            endorse(Janet) on 9233 comment "${Janet.name} is the Assessor"
+        }
+
+        votes(Kate) {
+            endorse(Cosmo) on 9233 comment "${Cosmo.name} is the Promotor"
+        }
+
+        votes(Mischief) {
+            FOR on 9233
+        }
+
+        votes(Janet) {
+            AGAINST on 9233
+        }
+
+        votes(Murphy) {
+            FOR on 9233
+        }
+
+        votes(ais523) {
+            AGAINST on 9233
+        }
+
+        votes(Cosmo) {
+            FOR on 9233
+        }
+    }
+}
