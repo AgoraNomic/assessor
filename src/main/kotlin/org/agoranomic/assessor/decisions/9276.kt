618b1b6864631d401cddbb30e37d3357bb71c100
Janet Cobb <git@randomcat.org>
2026-01-06 17:03:46 +0000

    Proposal 9276 (#210)
    * Import distribution
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2025-December/055000.html
    
    * Add voting block
    
    * Initial votes by Cosmo
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2025-December/055001.html
    
    * Initial votes by ais523
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2025-December/055014.html
    
    * Initial votes by Murphy
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2025-December/055020.html
    
    * Initial votes by Janet
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2025-December/055033.html
    
    * Initial votes by Trigon
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2025-December/055036.html
    
    * Add url

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/9276.kt
@@ -0,0 +1,103 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.coauthors
+import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.receivers.url
+import org.agoranomic.assessor.dsl.votes.complexityBonuses
+import org.agoranomic.assessor.dsl.votes.onOrdinaryProposals
+import org.agoranomic.assessor.lib.vote.VoteKind.FOR
+import org.agoranomic.assessor.lib.vote.VoteKind.PRESENT
+
+@UseAssessment
+fun assessment9276() = assessment {
+    name("9276")
+    url("https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2025-December/018900.html")
+    quorum(7)
+
+    strengths {
+        default(3)
+        min(0)
+        max(15)
+
+        onOrdinaryProposals {
+            complexityBonuses {
+                maxBonus(3)
+
+                "Absurdor"(1) heldBy juan
+                "ADoP"(2) heldBy Murphy
+                "Arbitor"(2) heldBy Kate
+                "Archivist"(1) heldBy kiako
+                "Assessor"(3) heldBy Janet
+                "Collar"(1) heldBy Mischief
+                "Collector"(2) heldBy Mischief
+                "Distributor"(0) heldBy omd
+                "Herald"(2) heldBy snail
+                "Illuminator"(1) heldBy null
+                "Land Managor"(1) heldBy Murphy
+                "Notary"(2) heldBy null
+                "Numerator"(1) heldBy Trigon
+                "Prime Minister"(0) heldBy null
+                "Promotor"(3) heldBy Cosmo
+                "Referee"(2) heldBy ais523
+                "Registrar"(1) heldBy juan
+                "Rulekeepor"(3) heldBy Janet
+                "Speaker"(0) heldBy kiako
+                "Spendor"(1) heldBy Murphy
+                "Tailor"(1) heldBy Murphy
+                "Webmastor"(1) heldBy kiako
+            }
+        }
+    }
+
+
+    proposals(v4) {
+        proposal(9276) {
+            title("CFJ Number Card Fix")
+            ai("1.0")
+            author(ais523)
+            coauthors(Janet)
+            ordinary()
+
+            text(
+                """
+If exactly one rule contains the text
+{{{
+Whenever a player scores a number as described in this rule, the
+Numerator CAN once, and SHALL in a timely manner, grant its author
+}}}
+then amend that rule by replacing that text with
+{{{
+Whenever a player scores a number as described in this rule, the
+Numerator CAN once, and SHALL in a timely manner, grant that player
+}}}
+[This is a fix-forward for text in a rule proposed in a proposal that
+has not yet been adopted, written so as to not care about what number
+the rule eventually ends up being given.]"""
+            )
+        }
+    }
+
+    voting {
+        votes(Cosmo) {
+            FOR on 9276
+        }
+
+        votes(ais523) {
+            FOR on 9276
+        }
+
+        votes(Murphy) {
+            PRESENT on 9276
+        }
+
+        votes(Janet) {
+            FOR on 9276
+        }
+
+        votes(Trigon) {
+            FOR on 9276
+        }
+    }
+}
