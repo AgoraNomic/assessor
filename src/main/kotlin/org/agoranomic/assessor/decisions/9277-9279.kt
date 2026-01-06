86c8fb295c48a47b2729cf6e3c94f0220f55564d
Janet Cobb <git@randomcat.org>
2026-01-06 17:36:11 +0000

    Initial votes by Janet
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2025-December/055032.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/9277-9279.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/9277-9279.kt
@@ -5,6 +5,7 @@ import org.agoranomic.assessor.dsl.receivers.ai
 import org.agoranomic.assessor.dsl.receivers.coauthors
 import org.agoranomic.assessor.dsl.receivers.quorum
 import org.agoranomic.assessor.dsl.votes.complexityBonuses
+import org.agoranomic.assessor.dsl.votes.endorse
 import org.agoranomic.assessor.dsl.votes.endorseOfficer
 import org.agoranomic.assessor.dsl.votes.onOrdinaryProposals
 import org.agoranomic.assessor.lib.vote.VoteKind.*
@@ -147,6 +148,12 @@ was discharged at the time of eir original appointment.]"""
             AGAINST on 9277
             AGAINST on 9278
             AGAINST on 9279
+        }
+
+        votes(Janet) {
+            AGAINST on 9277
+            AGAINST on 9278
+            endorse(Mischief) on 9279
         }
     }
 }
