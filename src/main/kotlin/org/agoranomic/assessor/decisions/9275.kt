02a4675b867a18a2e38e24a6c193e01707f88746
Janet Cobb <git@randomcat.org>
2026-01-06 17:03:33 +0000

    Proposal 9275 (#209)
    * Import distribution
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2025-December/018853.html
    
    * Add voting strengths
    
    Latest ADoP's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2025-December/018879.html
    
    Latest Referee's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2025-December/018875.html
    
    * Add voting block
    
    * Initial votes by Cosmo
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2025-December/054988.html
    
    * Initial votes by Mischief
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2025-December/054989.html
    
    * Initial votes by juan
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2025-December/054990.html
    
    * Initial votes by Janet
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2025-December/054991.html
    
    * Initial votes by Murphy
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2025-December/054994.html
    
    * Add url

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/9275.kt
@@ -0,0 +1,94 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.receivers.url
+import org.agoranomic.assessor.dsl.votes.complexityBonuses
+import org.agoranomic.assessor.dsl.votes.endorseOfficer
+import org.agoranomic.assessor.dsl.votes.onOrdinaryProposals
+import org.agoranomic.assessor.lib.vote.VoteKind.FOR
+
+@UseAssessment
+fun assessment9275() = assessment {
+    name("9275")
+    url("https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2025-December/018897.html")
+    quorum(7)
+
+    strengths {
+        default(3)
+        min(0)
+        max(15)
+
+        onOrdinaryProposals {
+            complexityBonuses {
+                maxBonus(3)
+
+                "Absurdor"(1) heldBy juan
+                "ADoP"(2) heldBy Murphy
+                "Arbitor"(2) heldBy Kate
+                "Archivist"(1) heldBy kiako
+                "Assessor"(3) heldBy Janet
+                "Collar"(1) heldBy Mischief
+                "Collector"(2) heldBy Mischief
+                "Distributor"(0) heldBy omd
+                "Herald"(2) heldBy snail
+                "Illuminator"(1) heldBy null
+                "Land Managor"(1) heldBy Murphy
+                "Notary"(2) heldBy null
+                "Numerator"(1) heldBy Trigon
+                "Prime Minister"(0) heldBy null
+                "Promotor"(3) heldBy Cosmo
+                "Referee"(2) heldBy ais523
+                "Registrar"(1) heldBy juan
+                "Rulekeepor"(3) heldBy Janet
+                "Speaker"(0) heldBy kiako
+                "Spendor"(1) heldBy Murphy
+                "Tailor"(1) heldBy Murphy
+                "Webmastor"(1) heldBy kiako
+            }
+        }
+    }
+
+    proposals(v4) {
+        proposal(9275) {
+            title("Determinate determinacy")
+            ai("3.0")
+            author(Janet)
+            democratic()
+
+            text(
+                """
+Amend Rule 2518 ("Determinacy") by replacing the text "If a value CANNOT
+be reasonably determined" with the text "If a value cannot be reasonably
+determined".
+
+[We almost certainly mean "cannot" here in the natural language sense:
+determining something (in this sense) is not a regulated action; it
+requires thought and potentially a judicial process.]"""
+            )
+        }
+    }
+
+    voting {
+        votes(Cosmo) {
+            endorseOfficer("Rulekeepor", Janet) on 9275
+        }
+
+        votes(Mischief) {
+            FOR on 9275
+        }
+
+        votes(juan) {
+            FOR on 9275
+        }
+
+        votes(Janet) {
+            FOR on 9275
+        }
+
+        votes(Murphy) {
+            FOR on 9275
+        }
+    }
+}
