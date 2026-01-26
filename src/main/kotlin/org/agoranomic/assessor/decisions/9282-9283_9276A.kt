c2dd70c750c08702395d1b4670930f6cba2f042a
Janet Cobb <git@randomcat.org>
2026-01-26 00:48:53 -0500

    Proposals 9282-9283, 9276A (#213)
    * Import distribution
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2026-January/018924.html
    
    * Add voting strengths
    
    Latest ADoP's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2026-January/018937.html
    
    Latest Referee's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2026-January/018933.html
    
    * Add voting block
    
    * Initial votes by Cosmo
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2026-January/055068.html
    
    * Initial votes by Trigon
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2026-January/055069.html
    
    * Initial votes by Janet
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2026-January/055070.html
    
    * Initial votes by ais523
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2026-January/055071.html
    
    * Initial votes by Mischief
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2026-January/055073.html
    
    * Initial votes by Murphy
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2026-January/055078.html
    
    * Add url

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/9282-9283_9276A.kt
@@ -0,0 +1,175 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.coauthors
+import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.receivers.url
+import org.agoranomic.assessor.dsl.votes.complexityBonuses
+import org.agoranomic.assessor.dsl.votes.endorse
+import org.agoranomic.assessor.dsl.votes.endorseOfficer
+import org.agoranomic.assessor.dsl.votes.onOrdinaryProposals
+import org.agoranomic.assessor.lib.vote.VoteKind.*
+
+@UseAssessment
+fun assessment9282to9283_9276A() = assessment {
+    name("9282-9283, 9276A")
+    url("https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2026-January/018946.html")
+    quorum(3)
+
+    strengths {
+        default(3)
+        min(0)
+        max(15)
+
+        onOrdinaryProposals {
+            complexityBonuses {
+                maxBonus(3)
+
+                "Absurdor"(1) heldBy juan
+                "ADoP"(2) heldBy Murphy
+                "Arbitor"(2) heldBy Kate
+                "Archivist"(1) heldBy kiako
+                "Assessor"(3) heldBy Janet
+                "Collar"(1) heldBy Mischief
+                "Collector"(2) heldBy Mischief
+                "Distributor"(0) heldBy omd
+                "Herald"(2) heldBy snail
+                "Illuminator"(1) heldBy null
+                "Land Managor"(1) heldBy Murphy
+                "Notary"(2) heldBy null
+                "Numerator"(1) heldBy Trigon
+                "Prime Minister"(0) heldBy null
+                "Promotor"(3) heldBy Cosmo
+                "Referee"(2) heldBy ais523
+                "Registrar"(1) heldBy juan
+                "Rulekeepor"(3) heldBy Janet
+                "Speaker"(0) heldBy kiako
+                "Spendor"(1) heldBy Murphy
+                "Tailor"(1) heldBy Murphy
+                "Webmastor"(1) heldBy kiako
+            }
+        }
+    }
+
+    proposals(v4) {
+        proposal(9276) {
+            title("CFJ Number Card Fix")
+            ai("1.0")
+            author(ais523)
+            coauthors(Janet)
+            ordinary()
+
+            text(
+                """
+If exactly one rule contains the text
+{{{
+Whenever a player scores a number as described in this rule, the
+Numerator CAN once, and SHALL in a timely manner, grant its author
+}}}
+then amend that rule by replacing that text with
+{{{
+Whenever a player scores a number as described in this rule, the
+Numerator CAN once, and SHALL in a timely manner, grant that player
+}}}
+[This is a fix-forward for text in a rule proposed in a proposal that
+has not yet been adopted, written so as to not care about what number
+the rule eventually ends up being given.]"""
+            )
+        }
+
+        proposal(9282) {
+            title("Initiation specification")
+            ai("3.0")
+            author(Janet)
+            coauthors(ais523)
+            democratic()
+
+            text(
+                """
+Amend Rule 107 by:
+
+* Replacing "The publication of such a valid notice initiates the voting
+period for the decision." with "The initiation of an Agoran decision
+also initiates the voting period for that decision.".
+
+* Replacing the text ", and that such a decision was initiated" with the
+text ", that such a decision was initiated, and that that decision's
+voting period was initiated".
+
+
+[Per ais523's judgement in CFJ 4118. We don't need to state the
+requirement to "publish[] a valid notice" twice for initiating Agoran
+decisions, and we should always ratify that the voting period was
+initiated anyway.]"""
+            )
+        }
+
+        proposal(9283) {
+            title("Welcome Package Tweak")
+            ai("1.0")
+            author(Mischief)
+            ordinary()
+
+            text(
+                """
+Amend rule 2499 (Welcome Packages) by replacing:
+
+       * One stamp of eir own type.
+       * 10 spendies, if e has not been granted any spendies since e last
+         registered.
+
+with:
+
+       * 15 spendies, if e has not been granted any spendies since e last
+         registered.
+
+[A frequent pattern is: a new person joins, gets a welcome package, does
+essentially nothing further, then is deregistered. This results in a
+proliferation of stamp types. The revised package allows a new player
+the ability to purchase multiple stamps of eir own type, if e wants to
+and does so.
+
+Also, the welcome package as it stands now grants even players with too
+many stamps to use the normal rule 2659 mechanism (6 spendies for X
+stamps) yet another stamp of eir type.]"""
+            )
+        }
+    }
+
+    voting {
+        votes(Cosmo) {
+            FOR on all
+        }
+
+        votes(Trigon) {
+            FOR on 9276
+            PRESENT on 9282
+            AGAINST on 9283
+        }
+
+        votes(Janet) {
+            FOR on 9276
+            endorse(ais523) on 9282
+            FOR on 9283
+        }
+
+        votes(ais523) {
+            FOR on 9276
+            FOR on 9282
+            PRESENT on 9283
+        }
+
+        votes(Mischief) {
+            PRESENT on 9276
+            endorseOfficer("Rulekeepor", Janet) on 9282
+            FOR on 9283
+        }
+
+        votes(Murphy) {
+            FOR on 9276
+            FOR on 9282
+            endorseOfficer("Collector", Mischief) on 9283
+        }
+    }
+}
