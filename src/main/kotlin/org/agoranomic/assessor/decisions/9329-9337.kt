26c4d2e72452e824dd5f7a20c8fcdd534b03460d
Janet Cobb <git@randomcat.org>
2026-05-10 23:02:48 -0400

    Initial votes by ais523
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2026-May/019221.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/9329-9337.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/9329-9337.kt
@@ -454,5 +454,17 @@ its body as of the beginning of this year."""
             FOR on 9334
             PRESENT on others
         }
+
+        votes(ais523) {
+            FOR on 9329
+            AGAINST on 9330
+            FOR on 9331
+            FOR on 9332
+            FOR on 9333
+            FOR on 9334
+            FOR on 9335
+            AGAINST on 9336
+            AGAINST on 9337
+        }
     }
 }
