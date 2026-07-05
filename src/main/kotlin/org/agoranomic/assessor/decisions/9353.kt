11b5473da8eff718a8332ba3626f7210d8cf399e
Janet Cobb <git@randomcat.org>
2026-07-05 00:23:40 -0400

    Proposal 9353 (#228)
    * Import distribution
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2026-June/019334.html
    
    * Add voting strengths
    
    Latest ADoP's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2026-June/019353.html
    
    Latest Referee's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2026-June/019354.html
    
    * Add voting block
    
    * Initial votes by ais523
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2026-June/055691.html
    
    * Initial votes by Cosmo
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2026-June/055693.html
    
    * Initial votes by pizza723
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2026-June/055694.html
    
    * Initial votes by Murphy
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2026-June/055698.html
    
    * Initial votes by msh210
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2026-June/055702.html
    
    * Initial votes by Galle
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2026-June/055703.html
    
    * Initial votes by 4st
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2026-June/055705.html
    
    * Initial votes by juan
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2026-June/055707.html
    
    * Initial votes by Janet
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2026-June/055709.html
    
    * Initial votes by Mischief
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2026-June/055711.html
    
    * Add url

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/9353.kt
@@ -0,0 +1,118 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.receivers.url
+import org.agoranomic.assessor.dsl.votes.complexityBonuses
+import org.agoranomic.assessor.dsl.votes.endorseOfficer
+import org.agoranomic.assessor.dsl.votes.onOrdinaryProposals
+import org.agoranomic.assessor.lib.vote.VoteKind.FOR
+import org.agoranomic.assessor.lib.vote.VoteKind.PRESENT
+
+@UseAssessment
+fun assessment9353() = assessment {
+    name("9353")
+    url("https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2026-June/019369.html")
+    quorum(6)
+
+    strengths {
+        default(3)
+        min(0)
+        max(15)
+
+        onOrdinaryProposals {
+            complexityBonuses {
+                maxBonus(3)
+
+                "Absurdor"(1) heldBy juan
+                "ADoP"(2) heldBy Murphy
+                "Arbitor"(2) heldBy Kate
+                "Archivist"(1) heldBy kiako
+                "Assessor"(3) heldBy Janet
+                "Collar"(1) heldBy Mischief
+                "Collector"(2) heldBy Mischief
+                "Distributor"(0) heldBy omd
+                "Executor"(1) heldBy Mischief
+                "Herald"(2) heldBy snail
+                "Illuminator"(1) heldBy Cosmo
+                "Land Managor"(1) heldBy Murphy
+                "Notary"(2) heldBy null
+                "Numerator"(1) heldBy Trigon
+                "Prime Minister"(0) heldBy Kate
+                "Promotor"(3) heldBy Cosmo
+                "Referee"(2) heldBy ais523
+                "Registrar"(1) heldBy juan
+                "Rulekeepor"(3) heldBy Janet
+                "Speaker"(0) heldBy Janet
+                "Spendor"(1) heldBy Murphy
+                "Tailor"(1) heldBy Murphy
+                "Webmastor"(1) heldBy kiako
+            }
+        }
+    }
+
+    proposals(v4) {
+        proposal(9353) {
+            title("Costlier crafting")
+            ai("1.0")
+            author(ais523)
+            ordinary()
+
+            text(
+                """
+In rule 2714, change "2 spendies" to "6 spendies".
+
+[Right now, there isn't very much incentive to trade number cards
+because you can generally get what you need by crafting, and this is
+making it too easy to Outshine The Sun because common number cards are
+common enough to make that fairly easily possible.
+
+By making crafting much more expensive, it encourages trading for the
+rare cards rather than creating new rare cards.]"""
+            )
+        }
+    }
+
+    voting {
+        votes(ais523) {
+            FOR on 9353
+        }
+
+        votes(Cosmo) {
+            FOR on 9353
+        }
+
+        votes(pizza723) {
+            FOR on 9353
+        }
+
+        votes(Murphy) {
+            endorseOfficer("Numerator", Trigon) on 9353
+        }
+
+        votes(msh210) {
+            FOR on 9353
+        }
+
+        votes(Galle) {
+            FOR on 9353
+        }
+
+        votes(Forest) {
+            FOR on 9353
+        }
+
+        votes(juan) {
+            PRESENT on 9353
+        }
+
+        votes(Janet) {
+            FOR on 9353
+        }
+
+        votes(Mischief) {
+            FOR on 9353
+        }
+    }
+}
