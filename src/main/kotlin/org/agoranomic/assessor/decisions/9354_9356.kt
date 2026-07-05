519674243fed5e6b256f74ade8ba352d0c89eae3
Janet Cobb <git@randomcat.org>
2026-07-05 01:14:37 -0400

    Initial votes by Galle
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2026-June/055794.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/9354_9356.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/9354_9356.kt
@@ -5,7 +5,9 @@ import org.agoranomic.assessor.dsl.receivers.ai
 import org.agoranomic.assessor.dsl.receivers.coauthors
 import org.agoranomic.assessor.dsl.receivers.quorum
 import org.agoranomic.assessor.dsl.votes.complexityBonuses
+import org.agoranomic.assessor.dsl.votes.endorseOfficer
 import org.agoranomic.assessor.dsl.votes.onOrdinaryProposals
+import org.agoranomic.assessor.lib.vote.VoteKind.FOR
 
 @UseAssessment
 fun assessment9354_9356() = assessment {
@@ -137,5 +139,10 @@ I don't see any reason to stop em.)"""
     }
 
     voting {
+        votes(Galle) {
+            FOR on 9354
+            endorseOfficer("Absurdor", juan) on 9356
+            // AGAINST on 9357
+        }
     }
 }
