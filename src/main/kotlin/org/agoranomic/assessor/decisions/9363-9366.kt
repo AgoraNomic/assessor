4f800f0b831fba99602daa59f79557948dad75a4
Janet Cobb <git@randomcat.org>
2026-08-09 00:23:04 -0400

    Initial votes by juniper
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2026-July/055946.html

--- src/main/kotlin/org/agoranomic/assessor/decisions/9363-9366.kt
+++ src/main/kotlin/org/agoranomic/assessor/decisions/9363-9366.kt
@@ -167,5 +167,12 @@ there is no such type to select, then e SHALL instead announce that fact.
             FOR on 9365
             FOR on 9366
         }
+
+        votes(juniper) {
+            PRESENT on 9363
+            AGAINST on 9364
+            AGAINST on 9365
+            FOR on 9366
+        }
     }
 }
