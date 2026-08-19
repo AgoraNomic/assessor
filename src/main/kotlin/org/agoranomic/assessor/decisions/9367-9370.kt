976fcb825aa25969e3bb5ace66aaba91c0faebf9
Janet Cobb <git@randomcat.org>
2026-08-18 23:31:52 -0400

    Proposals 9367-9370 (#233)
    * Import distribution
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2026-July/019474.html
    
    * Add voting strengths
    
    Latest ADoP's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2026-August/019484.html
    
    Latest Referee's report: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2026-August/019488.html
    
    * Add voting block
    
    * Initial votes by Cosmo
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2026-July/055966.html
    
    * Initial votes by pizza723
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2026-July/055969.html
    
    * Initial votes by Galle
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2026-July/055971.html
    
    * Initial votes by Mischief
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2026-July/055974.html
    
    * Initial votes by msh210
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2026-July/055983.html
    
    * Initial votes by Murphy
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2026-July/055989.html
    
    * Initial votes by juan
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2026-July/055991.html
    
    * Initial votes by 4st
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2026-July/055994.html
    
    * Initial votes by snail
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2026-July/055995.html
    
    * Proposal 9368 was never distributed
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2026-July/055998.html
    
    * Initial votes by Janet
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2026-July/055999.html
    
    * Initial votes by ais523
    
    Source: https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-business/2026-July/056000.html
    
    * Add url

---
+++ src/main/kotlin/org/agoranomic/assessor/decisions/9367-9370.kt
@@ -0,0 +1,243 @@
+package org.agoranomic.assessor.decisions
+
+import org.agoranomic.assessor.dsl.assessment
+import org.agoranomic.assessor.dsl.receivers.ai
+import org.agoranomic.assessor.dsl.receivers.coauthors
+import org.agoranomic.assessor.dsl.receivers.quorum
+import org.agoranomic.assessor.dsl.receivers.url
+import org.agoranomic.assessor.dsl.votes.complexityBonuses
+import org.agoranomic.assessor.dsl.votes.endorse
+import org.agoranomic.assessor.dsl.votes.endorseOfficer
+import org.agoranomic.assessor.dsl.votes.onOrdinaryProposals
+import org.agoranomic.assessor.lib.vote.VoteKind.*
+
+@UseAssessment
+fun assessment9367to9370() = assessment {
+    name("9367-9370")
+    url("https://mailman.agoranomic.org/cgi-bin/mailman/private/agora-official/2026-August/019497.html")
+    quorum(7)
+
+    strengths {
+        default(3)
+        min(0)
+        max(15)
+
+        onOrdinaryProposals {
+            complexityBonuses {
+                maxBonus(3)
+
+                "Absurdor"(1) heldBy juan
+                "ADoP"(2) heldBy Murphy
+                "Arbitor"(2) heldBy Kate
+                "Archivist"(1) heldBy kiako
+                "Assessor"(3) heldBy Janet
+                "Collar"(1) heldBy Mischief
+                "Collector"(2) heldBy Mischief
+                "Distributor"(0) heldBy omd
+                "Executor"(1) heldBy Mischief
+                "Herald"(2) heldBy snail
+                "Illuminator"(1) heldBy Cosmo
+                "Land Managor"(1) heldBy Murphy
+                "Notary"(2) heldBy pizza723
+                "Numerator"(1) heldBy Trigon
+                "Prime Minister"(0) heldBy Kate
+                "Promotor"(3) heldBy Cosmo
+                "Referee"(2) heldBy ais523
+                "Registrar"(1) heldBy juan
+                "Rulekeepor"(3) heldBy Janet
+                "Speaker"(0) heldBy ais523
+                "Spendor"(1) heldBy Murphy
+                "Tailor"(1) heldBy Murphy
+                "Webmastor"(1) heldBy kiako
+            }
+        }
+    }
+
+    proposals(v4) {
+        proposal(9367) {
+            title("Divestment")
+            ai("3.0")
+            author(Kate)
+            democratic()
+
+            text(
+                """
+Set the Indigo Ribbon Ownership of the author of this proposal to False.
+
+[Many, many years ago, I acquired an Indigo Ribbon through a scam,
+  without realising this was considered Poor Form, and later used it in a
+  Win by Renaissance. Since then I have desperately been trying to
+  account for it by achieving degrees and declining to award myself the
+  ribbon, but people keep "helpfully" awarding it to me anyway to allow
+  for my "forgetfulness".
+
+  This just happened again and I do not want to go through yet another
+  round of this. Can we fix it by proposal, please?]"""
+            )
+        }
+
+        /*
+        proposal(9368) {
+            title("Greater Genetic Mutation v3")
+            ai("1.0")
+            author(Galle)
+            ordinary()
+
+            text(
+                """
+Amend Rule 2710 ("Genetic Modification") by replacing the fourth
+paragraph with the following text:
+---
+Once per Crop Season, the Land Managor CAN and SHALL by announcement set
+a randomly selected attribute (Seed Cost, Growth Duration, Opportunity,
+Resilience, or Sell Price) of a randomly selected type of Crop that was
+in-season during the previous season to a randomly selected valid value.
+If there is no such type to select, then e SHALL instead announce that fact."""
+            )
+        }
+         */
+
+        proposal(9369) {
+            title("Oops")
+            ai("3.0")
+            author(snail)
+            coauthors(Mischief)
+            democratic()
+
+            text(
+                """
+Amend Rule 2713 (Scoring Numbers) by replacing the text:
+
+{
+
+       Whenever a player scores a number as described in this rule, the
+       Numerator CAN once, and SHALL in a timely fashion, grant that
+       player (and, if the number was scored due to the adoption of a
+       proposal, also a randomly chosen other player who voted FOR on a
+       referendum to adopt that proposal) a number card for each digit in
+       that number, whose type is that digit.
+
+
+}
+
+with the text:
+
+{
+
+       Whenever a player scores a number as described in this rule, the
+       Numerator CAN once by announcement, and SHALL in a timely fashion,
+grant that
+       player (and, if the number was scored due to the adoption of a
+       proposal, also a randomly chosen other player who voted FOR on a
+       referendum to adopt that proposal) a number card for each digit in
+       that number, whose type is that digit.
+
+}
+
+The following statement is hereby ratified:
+{
+Each attempt to grant a player a number card that occured prior to the
+adoption of this proposal took effect as if the text "CAN once," in Rule
+2713 instead read "CAN once by announcement,".
+}
+
+[This should work for each version of Rule 2713 that has existed. I'm doing
+this instead of changing the past text of the rule because doing that
+retroactively would have caused proposals that amended that specific text
+to fail.]
+
+[I also considered expunging the Numerator's duties to avoid shenanigans,
+but that might do more harm than good, and the ratification should already
+cover that issue.]"""
+            )
+        }
+
+        proposal(9370) {
+            title("Probably shouldn't leave this lying around")
+            ai("2.0")
+            author(Galle)
+            ordinary()
+
+            text(
+                """
+Repeal the rule entitled "Purchased Re-enactment"."""
+            )
+        }
+    }
+
+    voting {
+        votes(Cosmo) {
+            PRESENT on 9367
+            // FOR on 9368
+            endorseOfficer("Numerator", Trigon) on 9369
+            FOR on 9370
+        }
+
+        votes(pizza723) {
+            PRESENT on 9367
+            // PRESENT on 9368
+            FOR on 9369
+            FOR on 9370
+        }
+
+        votes(Galle) {
+            FOR on 9367
+            // FOR on 9368
+            FOR on 9369
+            FOR on 9370
+        }
+
+        votes(Mischief) {
+            FOR on 9367
+            // endorseOfficer("Land Managor", Murphy) on 9368
+            FOR on 9369
+            FOR on 9370
+        }
+
+        votes(msh210) {
+            // FOR on 9368
+            FOR on 9369
+            FOR on 9370
+        }
+
+        votes(Murphy) {
+            endorse(Kate) on 9367
+            // FOR on 9368
+            endorseOfficer("Numerator", Trigon) on 9369
+            FOR on 9370
+        }
+
+        votes(juan) {
+            endorse(Kate) on 9367
+            // PRESENT on 9368
+            FOR on 9369
+            FOR on 9370
+        }
+
+        votes(Forest) {
+            AGAINST on 9367
+            AGAINST on 9370
+        }
+
+        votes(snail) {
+            FOR on 9367
+            // FOR on 9368
+            FOR on 9369
+            FOR on 9370
+        }
+
+        votes(Janet) {
+            FOR on 9367
+            // AGAINST on 9368
+            endorse(ais523) on 9369
+            endorse(ais523) on 9370
+        }
+
+        votes(ais523) {
+            FOR on 9367
+            // FOR on 9368
+            FOR on 9369
+            FOR on 9370
+        }
+    }
+}
